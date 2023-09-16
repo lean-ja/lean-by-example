b4fed8cd29da8ff5a7aaeaa966fdff9d885541b8
Seasawher <tellagoodstory@icloud.com>
2023-09-16 20:59:04 +0900

    mathlib のバージョンを固定する

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package examples {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "master"
+  "https://github.com/leanprover-community/mathlib4.git" @ "96806534a3a47c26cec8985be643f13c2a1b9c92"
 
 lean_lib Examples {
   -- add lib config here
