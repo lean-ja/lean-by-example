f2291214d776ad00d903549a4230a30a12374598
Seasawher <tellagoodstory@icloud.com>
2023-09-16 17:14:30 +0900

    left, right に例を追加

--- Examples/LeftRight.lean
+++ Examples/LeftRight.lean
@@ -1,5 +1,14 @@
+import Mathlib.Tactic
+
+-- ANCHOR: first
+example (hP: P) : P ∨ Q := by
+  left
+  assumption
+-- ANCHOR_END: first
+
+
 -- ANCHOR: without_math
 example (hP: P) : P ∨ Q := by
   apply Or.inl
-  exact hP
+  assumption
 -- ANCHOR_END: without_math
