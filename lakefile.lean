422baa7207c0a08769d159dbc879bd5d0716d940
Seasawher <tellagoodstory@icloud.com>
2023-09-16 17:09:54 +0900

    mathlib4 への依存関係を追加

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,9 @@ package examples {
   -- add configuration options here
 }
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
+
 lean_lib Examples {
   -- add lib config here
 }
