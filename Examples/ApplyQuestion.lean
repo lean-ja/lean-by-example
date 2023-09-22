107a3a710e923ad4c83e12a801df1eeb0f7b7d37
Seasawher <tellagoodstory@icloud.com>
2023-09-22 18:38:28 +0900

    誤字を修正

--- Examples/ApplyQuestion.lean
+++ Examples/ApplyQuestion.lean
@@ -5,7 +5,7 @@ import Mathlib.Algebra.Hom.Group.Defs
 
 
 -- ANCHOR: first
--- 群順同型は積を保つ
+-- 群準同型は積を保つ
 example [Group G] [Group H] (f : G →* H) (a b : G) :
     f (a * b) = f a * f b := by
     -- `exact MonoidHom.map_mul f a b` を提案してくれる
