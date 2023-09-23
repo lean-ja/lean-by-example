39f8da269b93d5f6654783830906ba33e8c30bcd
Seasawher <tellagoodstory@icloud.com>
2023-09-23 21:12:09 +0900

    CI を作成するテスト

--- lakefile.lean
+++ lakefile.lean
@@ -2,12 +2,15 @@ import Lake
 open Lake DSL
 
 package examples {
-  -- add configuration options here
+  -- https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/.60autoImplicit.20true.60.20is.20evil/near/355145527
+  moreLeanArgs := #["-DrelaxedAutoImplicit=false"]
+  moreServerArgs := #["-DrelaxedAutoImplicit=false"]
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "96806534a3a47c26cec8985be643f13c2a1b9c92"
 
+@[default_target]
 lean_lib Examples {
   -- add lib config here
 }
