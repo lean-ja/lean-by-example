4996cd8669e78c90ac862abe586bbbb5dc839a70
Seasawher <tellagoodstory@icloud.com>
2023-10-17 23:19:05 +0900

    says の説明を修正
    simp? や aesop? に対する説明が不十分だった

--- Examples/Says.lean
+++ Examples/Says.lean
@@ -1,3 +1,5 @@
+import Aesop
+import Mathlib.Init.Function
 import Mathlib.Tactic.LibrarySearch
 import Mathlib.Tactic.Says
 
@@ -8,3 +10,20 @@ example (hPQ : P → Q) (hQR : Q → R) (hRS : R → S) (hP : P) : S := by
   -- `exact?` は実行されない
   exact? says exact hRS (hQR (hPQ hP))
 -- ANCHOR_END: first
+
+
+-- 以下 `X` `Y` `Z`を集合とする
+variable {X Y Z : Type}
+
+open Function
+
+-- ANCHOR: aesop
+-- 合成 `g ∘ f` が単射なら，`f` も単射
+example {f : X → Y} {g : Y → Z} (hgfinj : Injective (g ∘ f)) : Injective f := by
+  rw [Injective]
+  aesop? says {
+    intro a₁ a₂ a
+    apply hgfinj
+    simp_all only [comp_apply]
+  }
+-- ANCHOR_END: aesop
