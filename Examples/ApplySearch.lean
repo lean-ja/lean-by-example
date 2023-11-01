3ff388d694da31929a7fe00508151ea872024a45
Seasawher <tellagoodstory@icloud.com>
2023-11-01 20:26:06 +0900

    apply? のコード例を，lean playground でそのまま実行できる形式に修正する

--- Examples/ApplySearch.lean
+++ Examples/ApplySearch.lean
@@ -1,33 +1,28 @@
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Hom.Group.Defs
-import Mathlib.Tactic.Explode
-import Mathlib.Tactic.LibrarySearch
+import Mathlib.Tactic.LibrarySearch -- apply? を使うのに必要
 
-
--- ANCHOR: first
--- 群準同型は積を保つ
+/-- 群準同型は積を保つ -/
 example [Group G] [Group H] (f : G →* H) (a b : G) :
     f (a * b) = f a * f b := by
-    -- `exact MonoidHom.map_mul f a b` を提案してくれる
-    apply?
--- ANCHOR_END: first
+  -- `exact MonoidHom.map_mul f a b` を提案してくれる
+  apply?
 
+/-!
+  ## 補足
 
--- ANCHOR: sorry
-theorem T (x y : Nat) (_: x ≤ y) : 2 ^ x ≤ 2 ^ y := by
-    apply?
-
-    -- `apply?` しただけで `done` が通り，示せているように見える
-    done
-
-/-
-T : ∀ (x y : ℕ), x ≤ y → 2 ^ x ≤ 2 ^ y
-
-0│       │ x       ├ ℕ
-1│       │ y       ├ ℕ
-2│       │ h       ├ x ≤ y
-3│       │ sorryAx │ 2 ^ x ≤ 2 ^ y
-4│0,1,2,3│ ∀I      │ ∀ (x y : ℕ), x ≤ y → 2 ^ x ≤ 2 ^ y
+  `apply?` はあくまで証明を書くときに補助として使うものです．
+  `sorry` と同じように，清書した証明に残してはいけません．
+  `sorry` と同じと言いましたが，実際 `apply?` は `sorryAx` を裏で使用します．
 -/
-#explode T
--- ANCHOR_END: sorry
+
+theorem T (x y : Nat) (_: x ≤ y) : 2 ^ x ≤ 2 ^ y := by
+  apply?
+
+  -- `apply?` しただけで `done` が通り，示せているように見える
+  done
+
+-- 以下に示すように，裏で `sorryAx` が使われている
+
+/-- info: 'T' depends on axioms: [sorryAx] -/
+#guard_msgs in #print axioms T
--- src/apply_search.md
+++ src/apply_search.md
@@ -2,22 +2,10 @@
 
 needs: `import Mathlib.Tactic.LibrarySearch`
 
-`apply?` は，ゴールを閉じるのに必要な命題をライブラリから検索してきて，提案してくれるタクティクです．
-
-```lean
-{{#include ../Examples/ApplySearch.lean:first}}
-```
+`apply?` は，カレントゴールを `apply` や [refine](./refine.md) で変形することができないか，ライブラリから検索して提案してくれるタクティクです．
 
 複数の候補が提案されたときは，どれを選ぶとゴールが何に変わるのか表示されるので，その中から好ましいものを選ぶと良いでしょう．
 
-## 補足
-
-`apply?` はあくまで証明を書くときに補助として使うものです．`sorry` と同じように，清書した証明に残してはいけません．
-
-`sorry` と同じと言いましたが，実際 `apply?` は sorryAx [^sorry] を裏で使用します．これは，`#explode` で証明の中身を出力させれば分かります．`sorry` を使っているという旨の警告も出ます．
-
 ```lean
-{{#include ../Examples/ApplySearch.lean:sorry}}
+{{#include ../Examples/ApplySearch.lean}}
 ```
-
-[^sorry] `sorry` が裏で使用している公理のこと
