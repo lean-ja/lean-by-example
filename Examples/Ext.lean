f37c92e0f022e074b991d931c6a2788c200e3490
Seasawher <tellagoodstory@icloud.com>
2023-11-01 22:00:36 +0900

    ext のコード例をそのまま実行できる形式に修正

--- Examples/Ext.lean
+++ Examples/Ext.lean
@@ -1,19 +1,43 @@
 import Aesop
+import Mathlib.Data.SetLike.Basic -- `ext` タクティクで集合の等号を展開するために必要
+import Mathlib.Tactic.Says
 import Std.Tactic.Ext
 
--- `ext` タクティクで集合の等号を展開するために必要
-import Mathlib.Data.SetLike.Basic
+set_option says.no_verify_in_CI true
 
-
--- ANCHOR: first
 variable {α : Type}
 
 -- `s` と `t` は `α` の部分集合
 variable (s t : Set α)
 
+/-! ## ext -/
+
 example : s ∩ t = t ∩ s := by
-  -- `x ∈ α` を取る．` x ∈ s ∩ t ↔ x ∈ t ∩ s` を証明すればよい
+  -- `x ∈ α` を取る．
   ext x
 
-  aesop
--- ANCHOR_END: first
+  -- ` x ∈ s ∩ t ↔ x ∈ t ∩ s` を証明すればよい
+  show x ∈ s ∩ t ↔ x ∈ t ∩ s
+
+  aesop? says
+    simp_all only [Set.mem_inter_iff]
+    apply Iff.intro
+    · intro a
+      simp_all only [and_self]
+    · intro a
+      simp_all only [and_self]
+
+/-!
+  ## A ⊆ B
+
+  `A ⊆ B` を示すために元を取る操作は `intro` でできる．
+-/
+
+example : (s ∩ t) ⊆ t := by
+  -- `ext` は使えない
+  try ext x
+
+  intro x hx
+
+  simp? says
+    simp_all only [Set.mem_inter_iff]
--- src/ext.md
+++ src/ext.md
@@ -14,7 +14,5 @@ named after: 外延性(extensionality)
 `@[ext]` で登録されたルールを使用するため，集合の等式 `A = B` を示すときは `Mathlib.Data.SetLike.Basic` も必要です．
 
 ```lean
-{{#include ../Examples/Ext.lean:first}}
+{{#include ../Examples/Ext.lean}}
 ```
-
-なお `A ⊂ B` を示すために元を取るのは `intro x` で可能です．
