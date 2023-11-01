277a0a5270458fb2e42887796724dae782ca3b2e
Seasawher <tellagoodstory@icloud.com>
2023-11-01 22:04:49 +0900

    funext のコード例をそのまま実行できる形式に修正

--- Examples/Funext.lean
+++ Examples/Funext.lean
@@ -1,17 +1,27 @@
-import Mathlib.Tactic.Ring
+import Mathlib.Tactic.Ring -- `ring` を使用するのに必要
 
--- ANCHOR: first
 def f := fun (x : Nat) ↦ x + x
 
 def g := fun (x : Nat) ↦ 2 * x
+
+/-! ## funext -/
 
 example : f = g := by
   -- 引数 `x` を取る
   funext x
 
-  -- `f x` と `g x` を展開する
-  dsimp [f, g]
+  -- `f` と `g` を定義に展開する
+  unfold f g
 
   -- `x + x` と `2 * x` が等しいことを証明する
   ring
--- ANCHOR_END: first
+
+/-! ## ext との関連 -/
+
+example : f = g := by
+  -- `ext` で書き換えることができる
+  ext x
+
+  unfold f g
+
+  ring
--- src/funext.md
+++ src/funext.md
@@ -3,5 +3,5 @@
 関数 `f` と `g` が等しいことを示す際に，引数 `x` をとって `f x = g x` を示そうとすることがありますが，`funext` はそれを行うタクティクです.
 
 ```lean
-{{#include ../Examples/Funext.lean:first}}
+{{#include ../Examples/Funext.lean}}
 ```
