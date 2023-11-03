6aeb0bebe0547c13e8ec346c9bb139726c0ce240
Seasawher <tellagoodstory@icloud.com>
2023-11-03 19:34:04 +0900

    `induction` のコード例をそのまま実行できる形式に修正

--- Examples/Induction.lean
+++ Examples/Induction.lean
@@ -1,18 +1,31 @@
+import Mathlib.Algebra.Algebra.Basic -- `simp` で使う
+import Mathlib.Init.Data.Nat.Lemmas -- 強い帰納法のために必要
+import Mathlib.Order.Basic -- `simp` で使う
 import Mathlib.Tactic.Cases -- `induction'` のために必要
-import Mathlib.Tactic.Positivity
+import Mathlib.Tactic.Says -- `says` のために必要
 
-namespace Examples
+/-! ## 帰納法をいつ使うか -/
 
--- ANCHOR: nat
-inductive Nat
-  | zero : Nat
-  | succ (n : Nat) : Nat
--- ANCHOR_END: nat
+namespace Examples -- 名前被りを避けるため
+
+  -- 自然数の定義
+  inductive Nat
+    /-- `zero` は自然数 -/
+    | zero : Nat
+
+    /-- `n` が自然数なら `succ n` も自然数 -/
+    | succ (n : Nat) : Nat
+
+  -- 自然数はこのように帰納的に定義されているので，
+  -- 自然数に対して何かを示そうとすると帰納法を使うことが自然な選択といえる
+  -- 一般に，`inductive` などで帰納的に定義されたものを扱うとき
+  -- 帰納法が有用な可能性は高い
 
 end Examples
 
--- ANCHOR: first
--- 階乗関数
+/-! ## induction -/
+
+/-- 階乗関数 -/
 def fac : Nat → Nat
   | 0 => 1
   | n + 1 => (n + 1) * fac n
@@ -20,29 +33,56 @@ def fac : Nat → Nat
 example (n : Nat) : 0 < fac n := by
   -- `n` についての帰納法で示す
   induction n with
+
   | zero =>
-    -- `fac` の定義から従う
-    simp [fac]
+    simp only [fac]
+
   | succ n ih =>
-    -- `fac` の定義から従う
-    simp [fac]
-    positivity
--- ANCHOR_END: first
+    simp? [fac] says
+      simp only [
+        fac,
+        gt_iff_lt,
+        add_pos_iff,
+        or_true,
+        zero_lt_mul_left
+      ]
+    assumption
 
+/-!
+  ## inudction'
 
--- ANCHOR: dash
+  `induction'` というタクティクもあります．
+  これは Lean3 における `induction` タクティクとより構文が似ていて，
+  高度な帰納法が可能です．
+-/
+
 example (n : Nat) : 0 < fac n := by
   -- `ih` は帰納法の仮定
   -- `k` は `ih` に登場する変数
   induction' n with k ih
-  · simp [fac]
-  · simp [fac]
-    positivity
--- ANCHOR_END: dash
+
+  case zero =>
+    simp only [fac]
+
+  case succ =>
+    simp? [fac] says
+      simp only [
+        fac,
+        gt_iff_lt,
+        add_pos_iff,
+        or_true,
+        zero_lt_mul_left
+      ]
+    assumption
+
+/-!
+  ### 〇〇の～についての帰納法
+
+  `induction'` では「リストの長さに対する帰納法」もできます．
+-/
 
 variable (α : Type)
 
--- ANCHOR: induction_on_length
 example (l : List α) (P : List α → Prop) : P l := by
   -- リストの長さに対する帰納法
   induction' h : l.length generalizing l
@@ -63,9 +103,20 @@ example (l : List α) (P : List α → Prop) : P l := by
 
     show P l
     sorry
--- ANCHOR_END: induction_on_length
 
--- ANCHOR: strong
+/-!
+  ### 強い帰納法
+
+  時には，より強い帰納法が必要なこともあります．強い帰納法とは，たとえば
+
+  * `P(0)` を示す
+  * `(∀ k < n, P (k)) → P (n)` を示す
+  * したがって `∀ n, P (n)` である
+
+  という形式で表されるような帰納法のことです．
+  これは超限帰納法の特別な場合です．
+-/
+
 variable (P : Nat → Prop)
 
 example (n : Nat) : P n := by
@@ -78,4 +129,3 @@ example (n : Nat) : P n := by
   match n with
   | 0 => sorry
   | k + 1 => sorry
--- ANCHOR_END: strong
--- src/induction.md
+++ src/induction.md
@@ -2,53 +2,6 @@
 
 `induction` は，帰納法のためのタクティクです．
 
-自然数を例に説明します．Lean では自然数は次のように帰納的に定義されています.
-
 ```lean
-{{#include ../Examples/Induction.lean:nat}}
-```
-
-`succ` は後者関数と呼ばれる関数で，`n + 1 := succ n` です．
-
-`n : Nat` についてゴール `P n ⊢ Q n` があったとします．このとき `induction n` を行うと，コンストラクタ `zero` と `succ` のそれぞれに対して，対応するゴールを生成します．つまり
-
-* `P 0 ⊢ Q 0`
-* `(P (succ a)) (P a → Q a) ⊢ Q (succ a)`
-
-の２つのゴールです．
-
-```lean
-{{#include ../Examples/Induction.lean:first}}
-```
-
-## induction'
-
-needs: `import Mathlib.Tactic.Cases`
-
-`induction'` というタクティクもあります．こちらは箇条書きによる，より簡潔な書き方が可能です．
-
-```lean
-{{#include ../Examples/Induction.lean:dash}}
-```
-
-### 一般の～についての帰納法
-
-また，`induction'` では「リストの長さに対する帰納法」のようなより複雑な帰納法を行うことができます．
-
-```lean
-{{#include ../Examples/Induction.lean:induction_on_length}}
-```
-
-### 強い帰納法
-
-時には，より強い帰納法が必要なこともあります．強い帰納法とは，たとえば
-
-* `P(0)` を示す
-* `(∀ k < n, P (k)) → P (n)` を示す
-* したがって `∀ n, P (n)` である
-
-という形式で表されるような帰納法のことです．これは超限帰納法の特別な場合です．これを使用するには，`Nat.strong_induction_on` を使って次のようにします．
-
-```lean
-{{#include ../Examples/Induction.lean:strong}}
+{{#include ../Examples/Induction.lean}}
 ```
