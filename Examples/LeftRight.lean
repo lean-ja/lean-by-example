2f0ec63185c6c55d02e4deff7f4a0585d8373d90
Seasawher <tellagoodstory@icloud.com>
2023-11-03 11:19:49 +0900

    `left` `right` のコード例をそのまま実行できる形式に修正

--- Examples/LeftRight.lean
+++ Examples/LeftRight.lean
@@ -1,14 +1,35 @@
 import Mathlib.Tactic.LeftRight
 
+variable (P Q : Prop)
 
--- ANCHOR: first
+/-! ## left, right -/
+
 example (hP: P) : P ∨ Q := by
   left
-  assumption
--- ANCHOR_END: first
 
--- ANCHOR: without_math
+  -- ゴールが変わる
+  show P
+
+  exact hP
+
+/-! ## left, right を使わない方法 -/
+
+-- 以下に示すように，`Or.inl` は `a` から `a ∨ b` を得る関数です．
+
+/-
+Or.inl {a b : Prop} (h : a) : a ∨ b
+-/
+#check Or.inl
+
+-- `Or.inr` についても同様．
+
+/-
+Or.inr {a b : Prop} (h : b) : a ∨ b
+-/
+#check Or.inr
+
+-- これを使うことで `left` と `right` を使わずに証明できます．
+
 example (hP: P) : P ∨ Q := by
   apply Or.inl
-  assumption
--- ANCHOR_END: without_math
+  exact hP
--- src/left_right.md
+++ src/left_right.md
@@ -1,15 +1,7 @@
 # left, right
 
-needs: `import Mathlib.Tactic.LeftRight`
-
 ゴールが `⊢ P ∨ Q` であるとき，`left` はゴールを `⊢ P` に，`right` はゴールを `⊢ Q` に変えます．
 
 ```lean
-{{#include ../Examples/LeftRight.lean:first}}
-```
-
-`left`, `right` を使わずに Mathlib4 なしで同じことをするには，`Or.inl` または `Or.inr` を使用します．
-
-```lean
-{{#include ../Examples/LeftRight.lean:without_math}}
+{{#include ../Examples/LeftRight.lean}}
 ```
