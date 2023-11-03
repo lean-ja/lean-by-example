b34479cfb776e815ad8d902a6ab026bb04feef57
Seasawher <tellagoodstory@icloud.com>
2023-11-03 19:04:12 +0900

    `says` のコード例をそのまま実行できる形式に修正

--- Examples/Says.lean
+++ Examples/Says.lean
@@ -1,30 +1,45 @@
-import Aesop
-import Mathlib.Init.Function
-import Mathlib.Tactic.LibrarySearch
-import Mathlib.Tactic.Says
+import Aesop -- `aesop` のために必要
+import Mathlib.Init.Function -- `Injection` のために必要
+import Mathlib.Tactic.LibrarySearch -- `exact?` を使うために必要
+import Mathlib.Tactic.Says -- `says` を使うために必要
 
+-- `says` のチェックをCIで無効にする
 set_option says.no_verify_in_CI true
+
+/-!
+  ## says
+
+  検索タクティク `X` があり，その提案内容が `Try this: Y` だったとき，
+  `X says` とすると `says` は `Try this: Y` の代わりに
+  `Try this: X says Y` という提案を infoview 上で出します．
+  それをクリックすると，`X says` の内容が `X says Y` で置換されます．
+  そして，`X says Y` が実行されるときには `X` は飛ばされます．
+-/
 
 variable (P Q R S: Prop)
 
--- ANCHOR: first
+-- `exact?` に対して使用する例
 example (hPQ : P → Q) (hQR : Q → R) (hRS : R → S) (hP : P) : S := by
   -- `exact?` は実行されない
-  exact? says exact hRS (hQR (hPQ hP))
--- ANCHOR_END: first
+  exact? says
+    exact hRS (hQR (hPQ hP))
 
+/-!
+  ## aesop に使用した例
+
+  `says` は `Try this: ...` と何かを提案するタクティクに対して有用です．
+  `simp?`, `aesop?`, `exact?`, `apply?` などが該当します．
+-/
 
 -- 以下 `X` `Y` `Z`を集合とする
 variable {X Y Z : Type}
 
 open Function
 
--- ANCHOR: aesop
--- 合成 `g ∘ f` が単射なら，`f` も単射
+/-- 合成 `g ∘ f` が単射なら，`f` も単射 -/
 example {f : X → Y} {g : Y → Z} (hgfinj : Injective (g ∘ f)) : Injective f := by
   rw [Injective]
   aesop? says
     intro a₁ a₂ a
     apply hgfinj
     simp_all only [comp_apply]
--- ANCHOR_END: aesop
