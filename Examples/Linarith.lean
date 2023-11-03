30c1460cde94c5b26d7283ef4a5a7e73344f64fa
Seasawher <tellagoodstory@icloud.com>
2023-11-03 13:27:52 +0900

    `linarith` のコード例をそのまま実行できる形式に修正
    * さらに内部で使用しているアルゴリズムにも言及

--- Examples/Linarith.lean
+++ Examples/Linarith.lean
@@ -1,16 +1,18 @@
-import Mathlib.Tactic.Linarith
-
-import Std.Data.Rat
+import Mathlib.Tactic.Linarith -- `linarith` のために必要
+import Std.Data.Rat.Basic -- `ℚ` のために必要
 
 variable (x y z : ℚ)
 
--- ANCHOR: first
-example (h1 : 2 * x < 3 * y) (h2 : -4 * x + 2 * z < 0): 12 * y - 4 * z ≥ 0 := by
+/-! ## linarith -/
+
+example (h1: x = 2 * y) (h2 : - x + 2 * y = 1) : False := by
   linarith
--- ANCHOR_END: first
 
+example (h1 : 2 * x < 3 * y) (h2 : -4 * x + 2 * z < 0) :
+    12 * y - 4 * z ≥ 0 := by
+  linarith
 
--- ANCHOR: id
+-- 利用できる命題を増やすことで，通るようになることがある
 example : id x ≤ x := by
   -- `linarith` で示すことはできない
   try linarith
@@ -19,10 +21,3 @@ example : id x ≤ x := by
 
   -- `id x = x` だと教えてあげると `linarith` で示せる
   linarith
--- ANCHOR_END: id
-
-
--- ANCHOR: general
-example (h1: x = 2 * y) (h2 : - x + 2 * y = 1) : False := by
-  linarith
--- ANCHOR_END: general
--- src/linarith.md
+++ src/linarith.md
@@ -1,25 +1,7 @@
 # linarith
 
-needs: `import Mathlib.Tactic.Linarith`
-
-named after: 線形算術(linear arithmetic)
-
-`linarith` は線形な(不)等式を導くことができます．
+`linarith` は線形算術(linear arithmetic)を行うタクティクです．Fourier-Motzkin elimination を用いて，線形な(不)等式系から矛盾を導こうとします．一般に，ゴールが `False` でないときにはゴールの否定を仮定に加えることで，ゴールを閉じようとします．
 
 ```lean
-{{#include ../Examples/Linarith.lean:first}}
-```
-
-`linarith` はローカルコンテキストを読み取ってくれるので，`linarith` が通らないとき補題を追加してあげると通るようになることがあります．
-
-```lean
-{{#include ../Examples/Linarith.lean:id}}
-```
-
-## 補足
-
-もう少し詳細に書くと，`linarith` は「ロールコンテキストにある線形な(不)等式系に矛盾があるか調べる」タクティクなので，次のような使い方もできます．
-
-```lean
-{{#include ../Examples/Linarith.lean:general}}
+{{#include ../Examples/Linarith.lean}}
 ```
