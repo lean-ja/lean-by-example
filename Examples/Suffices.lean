b1b6cbc0ec57d78c388952252c07eac7d56eddc1
Seasawher <tellagoodstory@icloud.com>
2023-11-03 19:51:55 +0900

    `suffices` のコード例をそのまま実行できる形式に修正

--- Examples/Suffices.lean
+++ Examples/Suffices.lean
@@ -1,10 +1,8 @@
-import Mathlib.Data.Nat.Order.Lemmas
+import Mathlib.Data.Nat.Order.Lemmas -- 除算 `∣` を使うため
 
--- ANCHOR: first
 example : 13 ∣ (2 ^ 70 + 3 ^ 70) := by
   -- 余りがゼロであることを示せば十分
   suffices (2 ^ 70 + 3 ^ 70) % 13 = 0 from by
     exact Iff.mpr (Nat.dvd_iff_div_mul_eq (2 ^ 70 + 3 ^ 70) 13) rfl
 
   rfl
--- ANCHOR_END: first
