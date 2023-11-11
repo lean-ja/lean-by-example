b722864b552bf244c3bd2d05846da59a621a6def
Seasawher <tellagoodstory@icloud.com>
2023-11-11 20:05:50 +0900

    `contradiction` に例を追加
    通りそうで通らない例を追加する

--- Examples/Contradiction.lean
+++ Examples/Contradiction.lean
@@ -1,3 +1,7 @@
+import Mathlib.Algebra.Algebra.Basic -- `not_lt` などが使えるようにする
+
+/- ## contradiction -/
+
 -- `False`
 example (h : False) : P := by contradiction
 
@@ -9,3 +13,20 @@ example (x : Nat) (h : x ≠ x) : P := by contradiction
 
 -- 矛盾する仮定
 example (hP : P) (hnP : ¬ P) : Q := by contradiction
+
+/- ## contradiction が通りそうで通らない例 -/
+
+variable (n m : ℕ)
+
+example (hl : n ≤ m) (hg : m < n) : False := by
+  -- 明らかに矛盾に見えるが， 通らない
+  try contradiction
+
+  -- 通らない理由は， `n ≤ m` が `¬ m < n` を意味することを
+  -- `contradiction` は知らないから．
+
+  -- 次のようにして教えてあげると…
+  have := hl.not_lt
+
+  -- `contradiction` が通るようになる
+  contradiction
