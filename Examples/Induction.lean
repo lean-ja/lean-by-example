52f2fee925a4bd9e0670e8b2336a9b37b5644894
Seasawher <tellagoodstory@icloud.com>
2023-12-07 20:20:52 +0900

    超限帰納法の例で，`sorry` を使用しない

--- Examples/Induction.lean
+++ Examples/Induction.lean
@@ -100,15 +100,34 @@ example (l : List α) (P : List α → Prop) : P l := by
   これは超限帰納法の特別な場合です．
 -/
 
-variable (P : Nat → Prop)
+/-- フィボナッチ数列の通常の定義をそのまま Lean の関数として書いたもの -/
+def fibonacci : Nat → Nat
+| 0 => 0
+| 1 => 1
+| n + 2 => fibonacci n + fibonacci (n + 1)
 
-example (n : Nat) : P n := by
+/-- フィボナッチ数列の線形時間の実装 -/
+def fib (n : Nat) : Nat :=
+  (loop n).1
+where
+  loop : Nat → Nat × Nat
+    | 0 => (0, 1)
+    | n + 1 =>
+      let p := loop n
+      (p.2, p.1 + p.2)
+
+/-- `fib` が `fibonacci` と同じ漸化式を満たすことを証明する -/
+@[simp]
+theorem fib_add (n : Nat) : fib n + fib (n + 1) = fib (n + 2) := by rfl
+
+/-- `fibonacci` と `fib` は同じ結果を返す -/
+example : fibonacci = fib := by
+  -- 関数が等しいことを示すので，引数 `n` が与えられたとする
+  ext n
+
   -- `n` についての強い帰納法で示す
   induction' n using Nat.strong_induction_on with n ih
-
-  -- 仮定が追加される
-  guard_hyp ih : ∀ (m : ℕ), m < n → P m
-
   match n with
-  | 0 => sorry
-  | k + 1 => sorry
+  | 0 => rfl
+  | 1 => rfl
+  | n + 2 => simp_all [fibonacci]
