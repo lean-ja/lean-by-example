04d0bb375200140782724f3a1328610f4811c913
Seasawher <tellagoodstory@icloud.com>
2024-01-07 00:15:59 +0900

    import 文のチェックを, リモートから取得することで行うように変更

--- lakefile.lean
+++ lakefile.lean
@@ -7,14 +7,12 @@ package examples where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
+require «import-all» from git
+  "https://github.com/Seasawher/import-all" @ "main"
+
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "ac94cca1865a06e5401a58746ed299936dacb312"
 
 @[default_target]
 lean_lib Examples where
   -- add lib config here
-
-@[default_target]
-lean_exe importAll where
-  root := `scripts.importAll
-  supportInterpreter := true
