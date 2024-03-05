00748416e838a333b34739d4eec2ae190b232b3d
Seasawher <tellagoodstory@icloud.com>
2024-03-05 11:14:30 +0900

    simp のページの更新
    simp_arith を紹介する #74
    simp の設定事項についても書く #78

--- Examples/Tactic/Simp.lean
+++ Examples/Tactic/Simp.lean
@@ -1,54 +1,78 @@
 /- # simp
 
-`simp` は，ターゲットを決められた規則に基づいて自動で簡約（simplify）するタクティクです．カスタマイズすることが可能で，簡約に使ってほしい命題を登録することができます．-/
+`simp` は，ターゲットを決められた規則に基づいて自動で簡約（simplify）するタクティクです．`@[simp]` と付けることにより簡約に使ってほしい命題を登録することができます．-/
+import Mathlib.Tactic.Says -- `says` を使うために必要
+import Mathlib.Tactic.Tauto -- `tauto` を使うため
 
-universe u v
+variable {P Q R : Prop}
 
--- 圏の公理
-class Category (C : Type u) where
-  -- 射
-  Hom : C → C → Type v
+@[simp]
+theorem or_and : (P ∨ Q ∨ R) ∧ R ↔ R := by
+  constructor
 
-  -- 射の合成
-  comp : ∀ {a b c : C}, Hom a b → Hom b c → Hom a c
+  -- 左から右を示す
+  case mp =>
+    intro ⟨_, hR⟩
+    assumption
 
-  -- 恒等射. `id a` が `a` 上の恒等射を意味する
-  id : ∀ (a : C), Hom a a
+  -- 右から左を示す
+  case mpr =>
+    -- R が成り立つと仮定する
+    intro hR
 
-  -- 恒等射の性質
-  id_comp : ∀ {a b : C} (f : Hom a b), comp (id a) f = f
-  comp_id : ∀ {a b : C} (f : Hom a b), comp f (id b) = f
+    -- R は成り立つので，後は P ∨ Q ∨ R が成り立つことを示せばよい
+    refine ⟨?_, hR⟩
 
-  -- 射の結合律
-  assoc : ∀ {a b c d : C} (f : Hom a b) (g : Hom b c) (h : Hom c d),
-    comp (comp f g) h = comp f (comp g h)
+    -- R が成り立つことから明らか．
+    tauto
 
--- `f : Hom a b`と`g : Hom b c`の合成を`f ≫ g`と書く
-infixr:80 " ≫ " => Category.comp
+-- 一度登録した命題は `simp` で示せるようになる．
+example : (P ∨ Q ∨ R) ∧ R ↔ R := by simp
 
--- `Category.hoge` ではなく `hoge` で呼び出せるようにする
-open Category
+/-! 既知の `h : P` という命題を使って簡約させたいときは，明示的に `simp [h]` と指定することで可能です．複数個指定することもできます．`simp only [h₁, ... , hₖ]` とすると `h₁, ... , hₖ` だけを使用して簡約を行います．-/
 
--- 公理の等式が `simp` で使えるようにする
-attribute [simp] id_comp comp_id assoc
+example (h : R) : (P ∨ Q ∨ R) ∧ R := by
+  simp only [or_and]
+  assumption
 
--- 変数の定義
-variable {C : Type u} [Category.{u, v} C] {a b c d e : C}
+/-! 何も指定しなければゴールを簡約しますが，ローカルコンテキストにある `h : P` を簡約させたければ `simp at h` と指定することで可能です．全て簡約させたい場合は `simp at *` とします．-/
 
-example (f : Hom a b) (g : Hom b c) (h : Hom c d) (i : Hom d e) :
-    (f ≫ (id b ≫ g)) ≫ (h ≫ i) = f ≫ (g ≫ ((id c ≫ h) ≫ i)) := by
-  -- 上で `simp` で使えるようにした等式を使って自動で簡約する
-  simp
+variable {n m : Nat}
 
-/-! 既知の `h : P` という証明を使って簡約させたいときは，明示的に `simp [h]` と指定することで可能です．
+example (h : n + 0 + 0 = m) : n = m := by
+  simp at h
+  assumption
 
-何も指定しなければゴールを簡約しますが，ローカルコンテキストにある `h : P` を簡約させたければ `simp at h` と指定することで可能です．
 
-## simp?
+/-! ## simpa
+`simpa` は，`simp` を実行した後 `assumption` を実行するという一連の流れを一つのタクティクにしたものです．`simpa at h` 構文は存在せず，`simpa using h` と書くことに注意してください．-/
 
-`simp` は自動的に証明を行ってくれますが，何が使われたのか知りたいときもあります．`simp?` は簡約に何が使われたのかを示してくれるので，`rw` などを用いて明示的に書き直すことができます．
+example (h : R) : (P ∨ Q ∨ R) ∧ R := by
+  simpa only [or_and]
 
-## simp_all
+example (h : n + 0 + 0 = m) : n = m := by
+  simpa using h
+
+/-! ## simp?
+
+`simp` は自動的に証明を行ってくれますが，何が使われたのか知りたいときもあります．`simp?` は簡約に何が使われたのかを示してくれるので，`simp only` などを用いて明示的に書き直すことができます．-/
+
+example : (P ∨ Q ∨ R) ∧ R ↔ R := by
+  simp? says
+    simp only [or_and]
+
+/-! ## simp_arith
+`simp` の設定で `arith` を有効にすると，算術的な簡約もできるようになります．
+これはよく使用されるので，`simp_arith` という省略形が用意されています．
+-/
+
+example {x y : Nat} : 0 < 1 + x ∧ x + y + 2 ≥ y + 1 := by
+  simp (config := { arith := true, decide := true })
+
+example {x y : Nat} : 0 < 1 + x ∧ x + y + 2 ≥ y + 1 := by
+  simp_arith
+
+/-! ## simp_all
 
 `simp_all` は `simp [*] at *` の強化版で，ローカルコンテキストとゴールをこれ以上簡約できなくなるまですべて簡約します．
 
