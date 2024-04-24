65c3db623bb93265a8289e2cbea4105de5fc63bc
Seasawher <tellagoodstory@icloud.com>
2024-04-25 03:46:46 +0900

    check_failure の説明を少し詳しくする

--- Examples/Command/CheckFailure.lean
+++ Examples/Command/CheckFailure.lean
@@ -2,6 +2,11 @@
 `#check_failure` は，エラーが起こった時に成功し，エラーが起こらなければ失敗するコマンドです．エラーが起こるコードを意図的に構成したいときに便利です．
 -/
 
+-- 自然数と文字列を足すことはできない
 #check_failure 1 + "hello"
 
-#check_failure (rfl : 1 = 2)
+-- `1 = 2` を `rfl` で証明することはできない
+#check_failure (by rfl : 1 = 2)
+
+-- `1 + 4 = 5` は `contradiction` では示せない
+#check_failure (by contradiction : 1 + 4 = 5)
