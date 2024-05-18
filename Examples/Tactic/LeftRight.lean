5998a77c06237eb8e166843db7dc323cb2da73c6
Seasawher <tellagoodstory@icloud.com>
2024-05-19 01:15:12 +0900

    check の出力をテストする

--- Examples/Tactic/LeftRight.lean
+++ Examples/Tactic/LeftRight.lean
@@ -16,11 +16,9 @@ example (hP: P) : P ∨ Q := by
 以下に示すように，`Or.inl` は `a` から `a ∨ b` を得る関数です．また `Or.inr` は `b` から `a ∨ b` を得る関数です．これを使うことで `left` や `right` を使わずに証明できます．
 -/
 
--- Or.inl {a b : Prop} (h : a) : a ∨ b
-#check Or.inl
+#check (Or.inl : P → P ∨ Q)
 
--- Or.inr {a b : Prop} (h : b) : a ∨ b
-#check Or.inr
+#check (Or.inr : Q → P ∨ Q)
 
 example (hP: P) : P ∨ Q := by
   apply Or.inl
