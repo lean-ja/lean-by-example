01a898051911705bacbded9765e2f048851f6375
Seasawher <tellagoodstory@icloud.com>
2024-05-19 01:50:49 +0900

    eval コマンドの出力をテストする

--- Examples/Command/Eval.lean
+++ Examples/Command/Eval.lean
@@ -2,13 +2,12 @@ import Mathlib.Tactic --#
 
 namespace eval --#
 
-/-!
-  # eval
-  `#eval` コマンドは，式の値を評価します．
+/-! # eval
+`#eval` コマンドは，式の値を評価します．
 -/
 
--- 2
-#eval 1 + 1
+/-- info: 2 -/
+#guard_msgs in #eval 1 + 1
 
 -- 実行している Lean のバージョンが表示される
 #eval Lean.versionString
@@ -16,11 +15,11 @@ namespace eval --#
 def w := "world"
 
 -- 文字列が代入されて "hello, world" と表示される
-#eval s!"hello, {w}"
+/-- info: "hello, world" -/
+#guard_msgs in #eval s!"hello, {w}"
 
-/-!
-  ## 関数の値の評価
-  定義した関数が特定の値に対してどのように振る舞うか，その場で調べることができます．
+/-! ## 関数の値の評価
+定義した関数が特定の値に対してどのように振る舞うか，その場で調べることができます．
 -/
 
 -- 階乗関数
@@ -28,40 +27,42 @@ def fac : ℕ → ℕ
 | 0 => 1
 | n + 1 => (n + 1) * fac n
 
--- 120
-#eval fac 5
+/-- info: 120 -/
+#guard_msgs in #eval fac 5
 
-/-!
-  ## IO アクションの評価
-  `#eval` により一部の IO アクションを実行することもできます．
+/-! ## IO アクションの評価
+`#eval` により一部の IO アクションを実行することもできます．
 -/
 
 def main : IO Unit :=
   IO.println "Hello, world!"
 
--- Hello, world!
-#eval main
+/-- info: Hello, world! -/
+#guard_msgs in #eval main
 
-/-!
-  ## 例外処理の慣例
-  Lean および Mathlib では，「関数ではなく定理に制約を付ける」ことが慣例です．
-  関数の定義域を制限するアプローチだと，
-  関数を呼び出すたびに，引数が定義域に含まれているか確認する必要が生じて，
-  面倒になってしまうためです．
+/-! ## 例外処理の慣例
+Lean および Mathlib では，「関数ではなく定理に制約を付ける」ことが慣例です．
+関数の定義域を制限するアプローチだと，
+関数を呼び出すたびに，引数が定義域に含まれているか確認する必要が生じて，
+面倒になってしまうためです．
 -/
 
 -- たとえば，`1 / 0 = 0` となる．
 -- ご存じのようにゼロで割るのは除算として定義できないのだが，
 -- 除算の定義域を制限する代わりに，`1 / 0 = 0` という出力を割り当てている．
-#eval 1 / 0
+/-- info: 0 -/
+#guard_msgs in #eval 1 / 0
 
 -- もちろん，これは `0` であり `1` ではない．
-#eval (1 / 0) * 0
+/-- info: 0 -/
+#guard_msgs in #eval (1 / 0) * 0
 
 -- 他にも，自然数の引き算は，マイナスの代わりに `0` を返す．
-#eval 1 - 2
+/-- info: 0 -/
+#guard_msgs in #eval 1 - 2
 
 -- `1` ではなくて `2` になる．
-#eval (1 - 2) + 2
+/-- info: 2 -/
+#guard_msgs in #eval (1 - 2) + 2
 
 end eval --#
