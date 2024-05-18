c4d3c4fae271d4f7204b224f45b7193f2c32252b
Seasawher <tellagoodstory@icloud.com>
2024-05-19 01:50:37 +0900

    check コマンドの出力をテストする

--- Examples/Command/Check.lean
+++ Examples/Command/Check.lean
@@ -40,37 +40,30 @@
 /-! ## 型の型
 「すべての」項には型があるので，型も型を持ちます．
 -/
+-- 宇宙変数を宣言する
+universe u
 
 #check (Prop : Type)
 
 -- Lean では `List` は Type を受け取って，Type を返す関数.
--- List.{u} (α : Type u) : Type u
-#check List
+#check (List : (α : Type u) → Type u)
 
 -- 配列も同じ．型を型に変換する関数．
--- Array.{u} (α : Type u) : Type u
-#check Array
+#check (Array : (α : Type u) → Type u)
 
 -- `Type` 自身も型を持っており…
--- Type : Type 1
-#check Type
+#check (Type : Type 1)
 
--- Type 1 : Type 2
-#check Type 1
-
--- 宇宙変数を宣言する
-universe u
+#check (Type 1 : Type 2)
 
 -- この調子で限りなく続く
--- Type u : Type (u + 1)
-#check Type u
+#check (Type u : Type (u + 1))
 
 /-! ## 命題と証明
 Lean では命題やその証明にも型があります．命題の型は `Prop` で，命題の項が証明になっています．逆に言えば，Lean ではある命題 `P : Prop` を証明するということは，`P` という型を持つ項 `proof : P` を構成することと同じです．
 -/
 
--- Prop
-#check 1 + 1 = 2
+#check (1 + 1 = 2 : Prop)
 
 -- `1 + 1 = 2` の証明 `two_eq_add_one` を構成
 theorem two_eq_add_one : 2 = 1 + 1 := by rfl
@@ -96,9 +89,9 @@ theorem tautology : True -> True := fun a ↦ a
 -- 暗黙の変数を明示的にするために `@` を付けた.
 -- `tautology : True → True` と出力されるので，
 -- `tautology` は `True → True` という関数であることがわかる
-#check @tautology
+#check (@tautology : True → True)
 
 -- 実際に `trivial : True` を「代入」すると
 -- `tautology trivial : True` となり，
 -- 確かに `True` 型の項が得られていることがわかる．
-#check tautology trivial
+#check (tautology trivial : True)
