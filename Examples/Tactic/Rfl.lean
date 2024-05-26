66f950aedb4c8c34f975725e8c8b2676b04aa49e
Seasawher <tellagoodstory@icloud.com>
2024-05-26 23:00:07 +0900

    rfl の記述の誤りを修正
    Mathlib を import しなくても，@[refl] タグで反射性を登録することが可能になったようなので，補足情報を削除

--- Examples/Tactic/Rfl.lean
+++ Examples/Tactic/Rfl.lean
@@ -24,20 +24,6 @@ attribute [refl] MyEq.refl
 -- `rfl` で示せるようになる
 example (n : ℕ) : MyEq n n := by rfl
 
-/-! ## ≤ と rfl
-
-適宜ライブラリが必要ですが，不等式 `≤` の反射性も登録されているので `rfl` で示すことができます．
--/
+/-! 適宜ライブラリが必要ですが，不等式 `≤` の反射性も登録されているので `rfl` で示すことができます．-/
 
 example (n : ℕ) : n ≤ n := by rfl
-
-/-! ## 補足
-
-実は `Mathlib.Tactic.Relation.Rfl` を import するかどうかにより，内部で呼び出されるタクティクが変わります．
-
-* `Mathlib.Tactic.Relation.Rfl` ありなら [Lean.MVarId.rfl](https://leanprover-community.github.io/mathlib4_docs//Mathlib/Tactic/Relation/Rfl.html#Lean.MVarId.rfl) が，
-* なしなら [Lean.MVarId.refl](https://leanprover-community.github.io/mathlib4_docs//Lean/Meta/Tactic/Refl.html#Lean.MVarId.refl) が
-
-それぞれ参照されます．後者は `@[refl]` が付けられた一般の関係の反射性にアクセスできず，等号 `=` の反射性しか使うことができません．
-
-後者の場合 `rfl` は，単に定義から等しいものが等しいことを示すタクティクになります．-/
