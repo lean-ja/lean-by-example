6fe5583e86022b832c62c8e57864fb2b481cc3ae
Seasawher <tellagoodstory@icloud.com>
2024-06-03 03:34:14 +0900

    `#instances` の説明を改善
    * 依存関係をわかりやすくした
    * コード例に落とした

--- Examples/Contents/Instances.lean
+++ Examples/Contents/Instances.lean
@@ -1,8 +1,25 @@
 /- # \#instances
 `#instances` は，与えられた型クラスのインスタンスの完全なリストを出力するコマンドです．
 -/
+import Batteries.Tactic.Instances -- `#instances` コマンドを使うために必要
 
-import Mathlib.Data.Rat.Defs
-import Mathlib.Data.Real.Basic
+-- 内容が何もない例示のためだけの型クラス
+class Hoge (α : Type) where
+  hoge : Unit
 
-#instances Inv
+-- `Nat` を `Hoge` のインスタンスにする
+instance : Hoge Nat where
+  hoge := ()
+
+-- `Bool` を `Hoge` のインスタンスにする
+instance : Hoge Bool where
+  hoge := ()
+
+-- 今登録した2つのインスタンスが表示される
+/--
+info: 2 instances:
+
+instHogeBool : Hoge Bool
+instHogeNat : Hoge Nat
+-/
+#guard_msgs in #instances Hoge
