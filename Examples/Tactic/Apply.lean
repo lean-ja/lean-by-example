d392c1bd9805e0bd8245e966e88ef7a9ce13dc2e
Seasawher <tellagoodstory@icloud.com>
2024-06-20 01:26:25 +0900

    fix: `apply` の説明を加筆修正する

--- Examples/Tactic/Apply.lean
+++ Examples/Tactic/Apply.lean
@@ -1,12 +1,11 @@
 /- # apply
 
-`apply` は含意 `→` をゴールに適用するタクティクです．
-ゴールが `⊢ Q` で，ローカルコンテキストに `h: P → Q` があるときに，`apply h` を実行するとゴールが `⊢ P` に書き換わります．
+`apply` は含意 `→` をゴールに適用するタクティクです．ゴールが `⊢ Q` で，ローカルコンテキストに `h : P → Q` があるときに，`apply h` を実行するとゴールが `⊢ P` に書き換わります．
 -/
 variable (P Q : Prop)
 
 /-- `P → Q` かつ `P` ならば `Q` -/
-example (h: P → Q) (hP: P) : Q := by
+example (h : P → Q) (hP : P) : Q := by
   apply h
 
   -- ゴールが `P` に変わっている
@@ -14,30 +13,31 @@ example (h: P → Q) (hP: P) : Q := by
 
   exact hP
 
-/-! 注意点として，`h: P → Q` は `P` の証明を受け取って `Q` の証明を返す関数でもあるので，上記の例は `apply` を使わずに `exact h hP` で閉じることもできます．-/
+/-! 注意点として，`h : P → Q` は `P` の証明を受け取って `Q` の証明を返す関数でもあるので，上記の例は `apply` を使わずに `exact h hP` で閉じることもできます．-/
 
-example (h: P → Q) (hP: P) : Q := by
+example (h : P → Q) (hP : P) : Q := by
   exact h hP
 
-/-!
-## 否定 ¬ について
-また，Lean では否定 `¬ P` は `P → False` として実装されているため，ゴールが `⊢ False` であるときに `hn: ¬P` に対して `apply hn` とするとゴールが `⊢ P` に書き換わります． -/
+/- さらに [`exact`](./Exact.md) の代わりに `apply` を使うこともできます．-/
 
-example (hn: ¬ P) (hP: P) : False := by
-  apply hn
-  show P
-
-  exact hP
-
-/-!
-## exact との関連
-[`exact`](./Exact.md) の代わりに `apply` を使うことができます．
--/
-
-example (h: P → Q) (hP: P) : Q := by
+example (h : P → Q) (hP : P) : Q := by
   apply h hP
 
-/-! ## よくあるエラー
+/- また，Lean では否定 `¬ P` は `P → False` として実装されているため，ゴールが `⊢ False` であるときに `hn : ¬P` に対して `apply hn` とするとゴールが `⊢ P` に書き換わります． -/
 
-`apply` には引数が必須なのですが，省略しても近くにエラーが出ません．一般に，構文的に間違った証明を書いた場合には，エラーがわかりやすい場所に出てくれる保証はありません．
+-- P の否定は，「P を仮定すると矛盾する」ということに等しい
+example : (¬ P) = (P → False) := by rfl
+
+example (hn : ¬ P) (hP : P) : False := by
+  apply hn
+  show P
+  exact hP
+
+/- ## 補足
+一般に，`apply` は関数適用を逆算するタクティクです．手元に関数 `f : A → B` があって型 `B` の型を作りたいとき，`A` の項を構成すれば `f` に適用することで `B` の項が得られる…といった推論を行います．
 -/
+
+-- 関数をタクティクを使用して構成する例
+def apply {α β : Type} (f : α → β) (a : α) : β := by
+  apply f
+  exact a
