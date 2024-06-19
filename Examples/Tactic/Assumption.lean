d20d9d41842e1ccd50de460f0f0b5608c0f477bb
Seasawher <tellagoodstory@icloud.com>
2024-06-20 01:28:55 +0900

    fix: `assumption` タクティクの説明を修正する

--- Examples/Tactic/Assumption.lean
+++ Examples/Tactic/Assumption.lean
@@ -7,9 +7,7 @@ variable (P Q : Prop)
 example (hP: P) (_: Q) : P := by
   assumption
 
-/-! ## exact との関連
-
-`assumption` による証明は，どの仮定を使うか明示すれば [`exact`](./Exact.md) で書き直すことができます．-/
+/- `assumption` による証明は，どの仮定を使うか明示すれば [`exact`](./Exact.md) で書き直すことができます．`assumption` を使用することにより，仮定の名前の変更に対してロバストになるほか，意図がわかりやすくなるというメリットがあります．-/
 
 example (hP: P) (_: Q) : P := by
   exact hP
