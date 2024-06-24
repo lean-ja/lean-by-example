413cfc5a279f56bb9a291c278b4388464aaf5518
Seasawher <tellagoodstory@icloud.com>
2024-06-24 21:03:14 +0900

    無名コンストラクタを紹介する Fixes #362

--- Examples/Command/Structure.lean
+++ Examples/Command/Structure.lean
@@ -38,6 +38,38 @@ structure Prod (α : Type) (β : Type) where
 -- コンストラクタ
 #check Prod.gen
 
+/- ## 無名コンストラクタ
+無名コンストラクタは，構造体 `T` に対して `T` 型の項を構成する方法のひとつです．これは以下に示すように，`⟨x1, x2, ...⟩` という構文により使うことができます．
+-/
+set_option linter.unusedVariables false --#
+
+/-- 2つのフィールドを持つ構造体 -/
+structure Hoge where
+  foo : Nat
+  bar : Nat
+
+def hoge : Hoge := ⟨1, 2⟩
+
+/- コンストラクタが入れ子になっていても平坦化することができます．例えば，以下の2つの定義は同じものを定義します．-/
+
+def foo : Nat × (Int × String) := ⟨1, ⟨2, "foo"⟩⟩
+
+def foo' : Nat × (Int × String) := ⟨1, 2, "foo"⟩
+
+#guard foo = foo'
+
+/- 一般の帰納型に対しては使用できません．-/
+
+inductive Sample where
+  | fst (foo bar : Nat) : Sample
+  | snd (foo bar : String) : Sample
+
+-- 「コンストラクタが一つしかない型でなければ使用できない」というエラーになる
+#check_failure (⟨"foo", "bar"⟩ : Sample)
+
+-- コンストラクタを指定しても使用できない
+#check_failure (Sample.snd ⟨"foo", "bar"⟩ : Sample)
+
 /- ## 項を定義する
 構造体の項を定義したい場合，複数の方法があります．波括弧記法が好まれますが，フィールド名が明らかな状況であれば無名コンストラクタを使用することもあります．-/
 
