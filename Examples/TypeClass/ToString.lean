5c988b0fd0affb07f5770f451ae603ccc114cd4a
Seasawher <tellagoodstory@icloud.com>
2024-07-06 20:55:14 +0900

    feat: 旧Lean by Exampleから型クラスの記事を移設する Fixes #369

--- Examples/TypeClass/ToString.lean
+++ Examples/TypeClass/ToString.lean
@@ -12,27 +12,53 @@ def origin : Point Int := ⟨0, 0⟩
 
 -- 文字列補完により文字列に変換しようとしても，
 -- 最初はどう変換したらいいのかわからないのでエラーになる
-#check_failure s!"{origin}"
+/--
+error: failed to synthesize
+  ToString (Point Int)
+use `set_option diagnostics true` to get diagnostic information
+-/
+#guard_msgs (error) in #check s!"{origin}"
 
 /-- `ToString` のインスタンスを登録する -/
 instance : ToString (Point Int) where
   toString p := s!"Point: ({p.x}, {p.y})"
 
 -- これで文字列に変換できる
-#eval s!"{origin}"
+/-- info: "Point: (0, 0)" -/
+#guard_msgs in #eval s!"{origin}"
 
-/- なお，`Repr` のインスタンスがないが，`ToString` のインスタンスはある状態で `#eval` しようとすると，`Repr` の代わりに `ToString` のインスタンスが使用されます．`Repr` のインスタンスを与えればそちらが優先して使用されます．-/
+/- ## `Repr` と `ToString`
+
+[`Repr`](./Repr.md) のインスタンスはないが，`ToString` のインスタンスはあるという状態で `#eval` しようとすると，`Repr` の代わりに `ToString` のインスタンスが使用されます．`Repr` のインスタンスを与えればそちらが優先して使用されます．-/
 
 /-- info: Point: (0, 0) -/
-#guard_msgs in --#
-#eval origin
+#guard_msgs in #eval origin
 
 -- `Repr` のインスタンスを自動生成して登録する
 -- 以降は，`#eval` 時には `Repr` のインスタンスが使用される
 deriving instance Repr for Point
 
 /-- info: { x := 0, y := 0 } -/
-#guard_msgs in --#
-#eval origin
+#guard_msgs in #eval origin
+
+/- 逆に，`Repr` のインスタンスはあるが，`ToString` のインスタンスがないとき，`ToString` の代わりに `Repr` が呼び出されることはありません．エラーになります．-/
+
+structure Person where
+  name : String
+  age : Nat
+deriving Repr
+
+def david := Person.mk "David" 42
+
+-- #eval はできる
+#eval david
+
+-- `ToString` のインスタンスがないのでエラーになる
+/--
+error: failed to synthesize
+  ToString Person
+use `set_option diagnostics true` to get diagnostic information
+-/
+#guard_msgs (error) in #check s!"{david}"
 
 end ToString --#
