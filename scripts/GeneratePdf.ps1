dc09f78efe974b43a1daefa60e9d405532f08122
Seasawher <tellagoodstory@icloud.com>
2024-07-24 01:20:51 +0900

    PDF を生成するコマンドを書く
    mdbook が通常はPDFを生成しないように、`book.toml` を動的に書き換える

---
+++ scripts/GeneratePdf.ps1
@@ -0,0 +1,12 @@
+# book.toml を動的に書き換える
+Get-Content -Path "book-pdf.toml" | Add-Content -Path "book.toml"
+
+# mdbook build を実行するが、
+# このとき mdbook-pdf というバックエンドが使われてPDFの生成が行われる
+mdbook build
+
+# book.pdf という名前でカレントディレクトリに置く
+Copy-Item -Path "book/pdf/output.pdf" -Destination "book.pdf"
+
+# 書き換えられた book.toml を元に戻す
+git checkout -- book.toml
