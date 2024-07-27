86b7036397eddf677e7b3d4128c2f34c5163c1e9
Seasawher <tellagoodstory@icloud.com>
2024-07-27 09:17:53 +0900

    class inductive を紹介する
    resolve #494

--- Examples/Term/TypeClass/Decidable.lean
+++ Examples/Term/TypeClass/Decidable.lean
@@ -44,7 +44,7 @@ instance (n : Nat) : Decidable (Even n) := by
 example : Even 4 := by decide
 
 /- ## class inductive
-`Decidable` 型クラスの定義は少し特殊です。コンストラクタが複数あり、構造体ではなく帰納型の構造をしています。これは `Decidable` が `class inductive` というコマンドで定義されているためです。-/
+`Decidable` 型クラスの定義は少し特殊です。コンストラクタが複数あり、構造体ではなく帰納型の構造をしています。これは `Decidable` が [`class inductive`](../../Command/Declarative/Class.md#ClassInductive) というコマンドで定義されているためです。-/
 
 /--
 info: inductive Decidable : Prop → Type
