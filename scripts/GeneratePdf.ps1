a3e44dbeb5d746c39f4a9a02f4633a206fc2806c
Seasawher <tellagoodstory@icloud.com>
2024-07-28 15:43:37 +0900

    fix workflow file

--- scripts/GeneratePdf.ps1
+++ scripts/GeneratePdf.ps1
@@ -5,11 +5,11 @@ Get-Content -Path "book-pdf.toml" | Add-Content -Path "book.toml"
 # このとき mdbook-pdf というバックエンドが使われてPDFの生成が行われる
 mdbook build
 
-# book.pdf という名前でカレントディレクトリに置く
-Copy-Item -Path "book/pdf/output.pdf" -Destination "book.pdf"
-
-# 書き換えられた book.toml を元に戻す
-git checkout -- book.toml
+# 古いPDFがもしあれば削除する
+Remove-Item -Path "book/pdf/LeanByExample.pdf" -Force -ErrorAction SilentlyContinue
 
 # ファイル名を変更する
 Rename-Item -Path "book/pdf/output.pdf" -NewName "LeanByExample.pdf"
+
+# 書き換えられた book.toml を元に戻す
+git checkout -- book.toml
