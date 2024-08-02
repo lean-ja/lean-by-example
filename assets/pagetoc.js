000bdb2a947ea5c7ca28a01af4b9a68857a80c91
Seasawher <tellagoodstory@icloud.com>
2024-08-03 06:23:06 +0900

    pagetoc がPCでのみ走るようにする

--- assets/pagetoc.js
+++ assets/pagetoc.js
@@ -1,114 +1,137 @@
-function forEach(elems, fun) {
-  Array.prototype.forEach.call(elems, fun);
+"use strict";
+
+/** クライアントの環境がPCかどうか判定する */
+function isDesktop() {
+  const userAgent = navigator.userAgent;
+  const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return !mobileRegex.test(userAgent);
 }
 
-function getPagetoc(){
-  return document.getElementsByClassName("pagetoc")[0]
-}
-
-function getPagetocElems() {
-  return getPagetoc().children;
-}
-
-function getHeaders(){
-  return document.getElementsByClassName("header")
-}
-
-// Un-active everything when you click it
-function forPagetocElem(fun) {
-  forEach(getPagetocElems(), fun);
-}
-
-function getRect(element) {
-  return element.getBoundingClientRect();
-}
-
-function overflowTop(container, element) {
-  return getRect(container).top - getRect(element).top;
-}
-
-function overflowBottom(container, element) {
-  return getRect(container).bottom - getRect(element).bottom;
-}
-
-var activeHref = location.href;
-
-var updateFunction = function (elem = undefined) {
-  var id = elem;
-
-  if (!id && location.href != activeHref) {
-    activeHref = location.href;
-    forPagetocElem(function (el) {
-      if (el.href === activeHref) {
-        id = el;
-      }
-    });
+/** 本体の処理 */
+function pageToc() {
+  function forEach(elems, fun) {
+    Array.prototype.forEach.call(elems, fun);
   }
 
-  if (!id) {
-    var elements = getHeaders();
-    let margin = window.innerHeight / 3;
+  function getPagetoc(){
+    return document.getElementsByClassName("pagetoc")[0]
+  }
 
-    forEach(elements, function (el, i, arr) {
-      if (!id && getRect(el).top >= 0) {
-        if (getRect(el).top < margin) {
+  function getPagetocElems() {
+    return getPagetoc().children;
+  }
+
+  function getHeaders(){
+    return document.getElementsByClassName("header")
+  }
+
+  // Un-active everything when you click it
+  function forPagetocElem(fun) {
+    forEach(getPagetocElems(), fun);
+  }
+
+  function getRect(element) {
+    return element.getBoundingClientRect();
+  }
+
+  function overflowTop(container, element) {
+    return getRect(container).top - getRect(element).top;
+  }
+
+  function overflowBottom(container, element) {
+    return getRect(container).bottom - getRect(element).bottom;
+  }
+
+  let activeHref = location.href;
+
+  let updateFunction = function (elem = undefined) {
+    let id = elem;
+
+    if (!id && location.href != activeHref) {
+      activeHref = location.href;
+      forPagetocElem(function (el) {
+        if (el.href === activeHref) {
           id = el;
-        } else {
-          id = arr[Math.max(0, i - 1)];
+        }
+      });
+    }
+
+    if (!id) {
+      let elements = getHeaders();
+      let margin = window.innerHeight / 3;
+
+      forEach(elements, function (el, i, arr) {
+        if (!id && getRect(el).top >= 0) {
+          if (getRect(el).top < margin) {
+            id = el;
+          } else {
+            id = arr[Math.max(0, i - 1)];
+          }
+        }
+        // a very long last section
+        // its heading is over the screen
+        if (!id && i == arr.length - 1) {
+          id = el
+        }
+      });
+    }
+
+    forPagetocElem(function (el) {
+      el.classList.remove("active");
+    });
+
+    if (!id) return;
+
+    forPagetocElem(function (el) {
+      if (id.href.localeCompare(el.href) == 0) {
+        el.classList.add("active");
+        let pagetoc = getPagetoc();
+        if (overflowTop(pagetoc, el) > 0) {
+          pagetoc.scrollTop = el.offsetTop;
+        }
+        if (overflowBottom(pagetoc, el) < 0) {
+          pagetoc.scrollTop -= overflowBottom(pagetoc, el);
         }
       }
-      // a very long last section
-      // its heading is over the screen
-      if (!id && i == arr.length - 1) {
-        id = el
-      }
     });
-  }
+  };
 
-  forPagetocElem(function (el) {
-    el.classList.remove("active");
-  });
+  let elements = getHeaders();
 
-  if (!id) return;
-
-  forPagetocElem(function (el) {
-    if (id.href.localeCompare(el.href) == 0) {
-      el.classList.add("active");
+  if (elements.length > 2) {
+    // Populate sidebar on load
+    window.addEventListener("load", function () {
       let pagetoc = getPagetoc();
-      if (overflowTop(pagetoc, el) > 0) {
-        pagetoc.scrollTop = el.offsetTop;
-      }
-      if (overflowBottom(pagetoc, el) < 0) {
-        pagetoc.scrollTop -= overflowBottom(pagetoc, el);
-      }
-    }
-  });
-};
-
-let elements = getHeaders();
-
-if (elements.length > 2) {
-  // Populate sidebar on load
-  window.addEventListener("load", function () {
-    var pagetoc = getPagetoc();
-    var elements = getHeaders();
-    forEach(elements, function (el) {
-      var link = document.createElement("a");
-      link.appendChild(document.createTextNode(el.text));
-      link.href = el.hash;
-      link.classList.add("pagetoc-" + el.parentElement.tagName);
-      pagetoc.appendChild(link);
-      link.onclick = function () {
-        updateFunction(link);
-      };
+      let elements = getHeaders();
+      forEach(elements, function (el) {
+        let link = document.createElement("a");
+        link.appendChild(document.createTextNode(el.text));
+        link.href = el.hash;
+        link.classList.add("pagetoc-" + el.parentElement.tagName);
+        pagetoc.appendChild(link);
+        link.onclick = function () {
+          updateFunction(link);
+        };
+      });
+      updateFunction();
     });
-    updateFunction();
-  });
 
-  // Handle active elements on scroll
-  window.addEventListener("scroll", function () {
-    updateFunction();
-  });
-} else {
-  document.getElementsByClassName("sidetoc")[0].remove();
+    // Handle active elements on scroll
+    window.addEventListener("scroll", function () {
+      updateFunction();
+    });
+  } else {
+    document.getElementsByClassName("sidetoc")[0].remove();
+  }
 }
+
+// DOMが完全に読み込まれた後に実行
+document.addEventListener('DOMContentLoaded', function() {
+  if (isDesktop()) {
+      // PCの場合に実行するコード
+      pageToc();
+  } else {
+      // モバイルデバイスの場合のコード（必要に応じて）
+      console.info('pagetoc does not run on a mobile device.');
+  }
+});
