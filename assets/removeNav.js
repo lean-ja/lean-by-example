438f5572608dfb73aa380f1d43f268ac927ef1b2
Seasawher <tellagoodstory@icloud.com>
2024-08-03 04:55:22 +0900

    mdbook のナビゲーションボタンがチャプターリストと被っていた問題の修正

---
+++ assets/removeNav.js
@@ -0,0 +1,10 @@
+/* previous (next) chapter へ移動するボタンが chapter list と被ってしまい押しづらい。
+荒っぽい修正としてこのボタンを削除してしまうことで対応 */
+
+// nav-wide-wrapper クラスを持つ要素を取得する
+const navigationButtons = document.querySelector('nav.nav-wide-wrapper');
+
+// この要素が存在する場合、削除する
+if (navigationButtons) {
+  navigationButtons.remove();
+}
