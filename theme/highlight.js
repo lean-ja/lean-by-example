f2b41b472b46c7b4099258d6156f787174f85a20
Seasawher <tellagoodstory@icloud.com>
2024-08-15 14:38:42 +0900

    syntax highlight にキーワードを追加する

--- theme/highlight.js
+++ theme/highlight.js
@@ -1115,7 +1115,7 @@ var hljs=function(){"use strict";function e(n){Object.freeze(n)
         'import open export prelude renaming hiding ' +
         'calc match with do by let extends ' +
         'for in unless try catch finally mutual mut return continue break where rec ' +
-        'syntax macro_rules macro deriving ' +
+        'syntax macro_rules macro deriving opaque ' +
         'fun elab ' +
         '#check #check_failure #eval #reduce #print ' +
         '#guard #guard_msgs #synth ' +
@@ -1124,7 +1124,7 @@ var hljs=function(){"use strict";function e(n){Object.freeze(n)
         'Type Prop|10 Sort rw|10 rewrite rwa erw subst substs ' +
         'simp dsimp simpa simp_intros finish using generalizing ' +
         'unfold unfold1 dunfold unfold_projs unfold_coes ' +
-        'delta cc ac_rfl ' +
+        'delta cc ac_rfl simp_arith simp_all ' +
         'existsi|10 cases rcases intro intros introv by_cases ' +
         'refl rfl funext case focus propext exact exacts ' +
         'refine apply eapply fapply apply_with apply_instance ' +
@@ -1135,7 +1135,7 @@ var hljs=function(){"use strict";function e(n){Object.freeze(n)
         'repeat skip swap solve1 abstract all_goals any_goals done ' +
         'fail_if_success success_if_fail guard_target guard_hyp ' +
         'have replace at suffices show from ' +
-        'congr congr_n congr_arg norm_num ring ' +
+        'congr congr_n congr_arg norm_num ring omega ' +
         'exists ext fail native_decide decide change obtain ',
       literal:
         'true false',
