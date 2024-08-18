3b160e9337708519a8b6db770c743210c7593024
Seasawher <tellagoodstory@icloud.com>
2024-08-18 17:01:58 +0900

    演習問題と解答の同期が保たれない Fixes #641

--- lakefile.lean
+++ lakefile.lean
@@ -30,12 +30,16 @@ def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
     IO.eprint out.stderr
   return hasError
 
-script build do
+/-- mk_exercise を実行し、演習問題の解答に
+解答部分を sorry に置き換えるなどの処理を施して演習問題ファイルを生成する。-/
+script mk_exercise do
   if ← runCmd "lake" #["exe", "mk_exercise", "Examples/Solution", "Examples/Exercise"] then return 1
-  if ← runCmd "lake" #["exe", "mdgen", "Examples", "src"] then return 1
-  if ← runCmd "mdbook" #["build"] then return 1
   return 0
 
-script link_test do
-  if ← runCmd "lychee" #["--base", "book", "."] then return 1
+/-- mk_exercise と mdgen と mdbook を順に実行し、
+Lean ファイルから Markdown ファイルと HTML ファイルを生成する。-/
+script build do
+  if ← runCmd "lake" #["run", "mk_exercise"] then return 1
+  if ← runCmd "lake" #["exe", "mdgen", "Examples", "src"] then return 1
+  if ← runCmd "mdbook" #["build"] then return 1
   return 0
