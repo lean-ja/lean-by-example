194c4147b89e0c98dbd1ae86c3271a18a9dcb72e
Seasawher <tellagoodstory@icloud.com>
2024-08-24 16:53:31 +0900

    `lake run build` の実行時間を部分ごとに計測して表示する

--- lakefile.lean
+++ lakefile.lean
@@ -20,6 +20,7 @@ require mathlib from git
 lean_lib Examples where
   globs := #[.submodules `Examples]
 
+@[inline]
 def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
   let out ← IO.Process.output {
     cmd := cmd
@@ -36,10 +37,24 @@ script mk_exercise do
   if ← runCmd "lake" #["exe", "mk_exercise", "Examples/Solution", "Examples/Exercise"] then return 1
   return 0
 
+@[inline]
+macro "with_time" x:doElem : doElem => `(doElem| do
+  let start_time ← IO.monoMsNow;
+  $x;
+  let end_time ← IO.monoMsNow;
+  IO.println s!"{end_time - start_time}ms")
+
 /-- mk_exercise と mdgen と mdbook を順に実行し、
 Lean ファイルから Markdown ファイルと HTML ファイルを生成する。-/
 script build do
-  if ← runCmd "lake" #["run", "mk_exercise"] then return 1
-  if ← runCmd "lake" #["exe", "mdgen", "Examples", "src"] then return 1
-  if ← runCmd "mdbook" #["build"] then return 1
+  -- `lake run mk_exercise` を使用すると遅くなってしまうのでコピペしている
+  IO.print "Time to running mk_exercise: "
+  with_time if ← runCmd "lake" #["exe", "mk_exercise", "Examples/Solution", "Examples/Exercise"] then return 1
+
+  IO.print "Time to running mdgen: "
+  with_time if ← runCmd "lake" #["exe", "mdgen", "Examples", "src"] then return 1
+
+  IO.print "Time to running mdbook: "
+  with_time if ← runCmd "mdbook" #["build"] then return 1
+
   return 0
