42c0b68c0143a1a24d13ed8461bd6359deed09e6
GitHub Action <action@github.com>
2024-09-09 14:28:58 +0000

    generated by GitHub Action

--- Exercise/HeytingAndBooleanAlgebra.lean
+++ Exercise/HeytingAndBooleanAlgebra.lean
@@ -258,10 +258,6 @@ abbrev Three := Fin 3
 
 /- ### 問2.1 半順序集合であること -/
 
--- この二つの定理は使用して良い
-theorem min_def (a b : Nat) : min a b = if a ≤ b then a else b := by aesop
-theorem max_def (a b : Nat) : max a b = if a ≤ b then b else a := by aesop
-
 -- ここに `local add_aesop_rules` コマンドを追加して証明が通るようにしてください。
 -- いくつルールを追加しても構いません。
 
