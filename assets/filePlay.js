d7201f12ca8a6c5af591ff36474e2ae775754aa9
Seasawher <tellagoodstory@icloud.com>
2024-09-23 01:10:17 +0900

    Biome で js ファイルのフォーマットなどを行う Fixes #861

--- assets/filePlay.js
+++ assets/filePlay.js
@@ -2,34 +2,37 @@
  * mdbook の "Suggest an edit" ボタンを改造し、
  * lean4 web editor へのリンクにしてしまう
  */
-"use strict";
-
-(function filePlay() {
+function filePlay() {
   // 編集ボタンのアイコン部分の `i` 要素
-  const editButtonIcon = document.querySelector('#git-edit-button');
-  editButtonIcon.className = 'fa fa-play';
-  editButtonIcon.id = 'lean-play-button';
+  const editButtonIcon = document.querySelector("#git-edit-button");
+  editButtonIcon.className = "fa fa-play";
+  editButtonIcon.id = "lean-play-button";
 
   // 編集ボタンを表す `a` 要素
   const editButtonLink = editButtonIcon.parentElement;
-  editButtonLink.title = 'Run on Lean 4 playground';
+  editButtonLink.title = "Run on Lean 4 playground";
   editButtonLink.ariaLabel = editButtonLink.title;
 
   // 拡張子が `.md` になっているので `.lean` に修正する
-  editButtonLink.href = editButtonLink.href.replace(/\.md$/, '.lean');
+  editButtonLink.href = editButtonLink.href.replace(/\.md$/, ".lean");
 
   // Lean ファイルがあるのは `src` ではなく `Examples` ディレクトリ
-  editButtonLink.href = editButtonLink.href.replace('/src/', '/Examples/');
+  editButtonLink.href = editButtonLink.href.replace("/src/", "/Examples/");
 
   // 演習問題のファイルのみ、`Examples` ディレクトリではなくて `Exercise` ディレクトリにある
-  editButtonLink.href = editButtonLink.href.replace('/Examples/Exercise/', '/Exercise/');
+  editButtonLink.href = editButtonLink.href.replace(
+    "/Examples/Exercise/",
+    "/Exercise/",
+  );
 
   // URL を書き換える
   fetch(editButtonLink.href)
-    .then(response => response.text())
-    .then(body => {
+    .then((response) => response.text())
+    .then((body) => {
       const escaped_code = encodeURIComponent(body);
       const url = `https://live.lean-lang.org/#code=${escaped_code}`;
       editButtonLink.href = url;
     });
-})();
+}
+
+filePlay();
