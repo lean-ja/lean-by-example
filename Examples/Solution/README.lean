84c54c615dca3ba317c718849371bf7afa981409
Seasawher <tellagoodstory@icloud.com>
2024-09-24 23:52:09 +0900

    演習問題の説明を更新

--- Examples/Solution/README.lean
+++ Examples/Solution/README.lean
@@ -1,10 +1,19 @@
 /- # 演習問題
-このセクションでは、演習問題を扱います。
+このセクションでは、演習問題を扱います。ぜひ新しいパズルゲームだと思って挑戦してみてください。
 
-## はじめに
+## 🎮 遊び方
 
-* 言語仕様リファレンスでは伝えきれない、「数学の理論をどのように Lean で実装するか」や「自然言語で表現された仕様をどのように Lean で表現するか」といったことを演習問題を通じてお伝えしたいと考えています。
-* 問題はおおむね難易度が低い順に並べています。
-* 各ページの右上に <i class="fa fa-play"></i> というボタンがあるので、これをクリックしてください。Lean Playground が起動してブラウザ上で演習問題を解くことができます。
-* 解答は [`Examples/Solution`](https://github.com/lean-ja/lean-by-example/tree/main/Examples/Solution) ディレクトリにあります。
+問題は、前提となるライブラリや知識が少なく、難易度が低いと思われる順に並べてあります。興味があるものから挑戦してみましょう。
+
+解き始めるための最も簡単で手軽な方法は、Lean 4 Playground を使う方法です。各ページの右上に <i class="fa fa-play"></i> というボタンがあるので、これをクリックしてください。Lean 4 Playground のページに移動し、そのままブラウザ上で演習問題を解き始めることができます。
+
+Lean 4 Playground を使いたくない事情がある場合は、Codespace を開き、`Exercise` ディレクトリの中にある該当ファイルを開いてください。
+
+[![codespace badge](https://github.com/codespaces/badge.svg)](https://codespaces.new/lean-ja/lean-by-example)
+
+## ❓ どうしてもわからないとき
+
+時間をかけて考えてみてください。著者は簡単な問題をたくさん解くのではなく、おもしろい問題にじっくり取り組んだ方が学びは大きいと信じています。どの問題も、本書のレファレンス部分を参照しつつ試行錯誤をすれば自力で解けることを目指して作っています。
+
+しかし、自力で解いた後に解答を見るのは構いません。解答は [`Examples/Solution`](https://github.com/lean-ja/lean-by-example/tree/main/Examples/Solution) ディレクトリに置いてあります。
 -/
--- src/SUMMARY.md
+++ src/SUMMARY.md
@@ -170,8 +170,8 @@
 
 - [演習問題](./Exercise/README.md)
   - [床屋のパラドクス](./Exercise/BarberParadox.md)
-  - [「ならば」の定義を疑う](./Exercise/DoubtImplication.md)
   - [騎士と悪党のパズル](./Exercise/KnightAndKnave.md)
+  - [「ならば」の定義を疑う](./Exercise/DoubtImplication.md)
   - [Cantorの定理](./Exercise/CantorTheorem.md)
   - [Cantorの対関数の全単射性](./Exercise/CantorPairing.md)
   - [Heying代数とBool代数](./Exercise/HeytingAndBooleanAlgebra.md)
