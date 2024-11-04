8dc8eb51b48ac3b532a4d9447a3c88a06671d6f5
Seasawher <tellagoodstory@icloud.com>
2024-11-04 17:23:46 +0900

    Bool を紹介する Fixes #505

---
+++ LeanByExample/Reference/Type/Bool.lean
@@ -0,0 +1,19 @@
+/- # Bool
+
+`Bool` は真偽値を表す型です。`true` と `false` の２つの値を持ちます。
+-/
+
+#check (true : Bool)
+#check (false : Bool)
+
+/- `Bool` の値を得るためには、たとえば `==` を用いて値を比較します。-/
+
+inductive Foo where
+  | bar
+  | baz
+  deriving BEq
+
+example : Bool := Foo.bar == Foo.bar
+example : Bool := Foo.bar != Foo.baz
+
+/- 真偽を表すという点で [`Prop`](./Prop.md) と似ていますが、`Bool` の項は簡約すれば必ず `true` か `false` になるため計算可能であるという含みがあります。-/
