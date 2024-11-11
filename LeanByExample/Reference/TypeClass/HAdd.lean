359f5a83038b249531745b5546b6bd44f7cd4509
Seasawher <tellagoodstory@icloud.com>
2024-11-12 05:15:36 +0900

    `HMul` 型クラスを紹介する Fixes #1083

--- LeanByExample/Reference/TypeClass/HAdd.lean
+++ LeanByExample/Reference/TypeClass/HAdd.lean
@@ -1,6 +1,7 @@
 /- # HAdd
-`HAdd` は、`+` という二項演算子のための型クラスです。
+`HAdd` (heterogeneous addition)は、`+` という二項演算子のための型クラスです。
 `+` 記法が何を意味するかについては制約はありません。
+任意の型 `α, β, γ : Type` に対して足し算 `(· + ·) : α → β → γ` を定義することができます。
 -/
 
 structure Colour where
@@ -14,11 +15,12 @@ def sample : Colour := { red := 2, blue := 4, green := 8 }
 -- メタ変数の番号を表示しない
 set_option pp.mvars false
 
--- 最初は `+` 記号が定義されていないのでエラーになります。
+-- 最初は `+` 記号が定義されていないのでエラーになる
 /--
 error: failed to synthesize
   HAdd Colour Colour ?_
-Additional diagnostic information may be available using the `set_option diagnostics true` command.
+Additional diagnostic information may be available
+using the `set_option diagnostics true` command.
 -/
 #guard_msgs in #eval sample + sample
 
