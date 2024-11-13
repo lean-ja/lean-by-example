2719d4769c1704d12b264d660157967dd10020db
Seasawher <tellagoodstory@icloud.com>
2024-11-14 06:21:50 +0900

    単に言及するだけでなくリンクにする

--- LeanByExample/Reference/TypeClass/README.lean
+++ LeanByExample/Reference/TypeClass/README.lean
@@ -2,5 +2,5 @@
 
 型クラスとは、和や積、逆数を取る演算など、複数の型に対してそれぞれのやり方で実装されるような演算を定義するものです。
 
-型クラスは主に `class` コマンドで定義され、インスタンスを宣言するには `instance` コマンドを使用します。
+型クラスは主に [`class`](#{root}/Reference/Declarative/Class.md) コマンドで定義され、インスタンスを宣言するには [`instance`](#{root}/Reference/Declarative/Instance.md) コマンドを使用します。
 -/
