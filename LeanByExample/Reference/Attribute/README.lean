b10bb781ae4093ce98294acc9271afd4a4b5387b
Seasawher <tellagoodstory@icloud.com>
2024-11-14 06:48:21 +0900

    Mathlib #help の出力へのリンクが間違っていたので修正する

--- LeanByExample/Reference/Attribute/README.lean
+++ LeanByExample/Reference/Attribute/README.lean
@@ -2,5 +2,5 @@
 
 属性は、`attribute [foo] bar` または `@[foo]` という構文で使用することができます。
 
-Mathlib において利用可能な属性の一覧は [attribute list](https://seasawher.github.io/mathlib4-help/attributes.html) で確認できます。
+Mathlib において利用可能な属性の一覧は [attribute list](https://seasawher.github.io/mathlib4-help/attributes/) で確認できます。
 -/
