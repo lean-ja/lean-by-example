bcf294e83c3867d5cfa12850f125f8bb3152c3a8
Seasawher <tellagoodstory@icloud.com>
2024-11-25 08:56:16 +0900

    plausible のカスタマイズ方法を紹介する Fixes #1098

--- LeanByExample/Reference/Tactic/Plausible.lean
+++ LeanByExample/Reference/Tactic/Plausible.lean
@@ -1,6 +1,6 @@
 /- # plausible
 
-`plausible` は、証明しようとしているゴールが間違っていないかチェックし、反例を見つけるとエラーで警告するタクティクです。 -/
+`plausible` は、証明しようとしているゴールが間違っていないかランダムに例を生成してチェックし、反例を見つけるとエラーで警告するタクティクです。 -/
 import Plausible
 
 variable (a b : Nat)
@@ -13,9 +13,7 @@ variable (a b : Nat)
     sorry
 
 /-
-## 反例が見つからない時
-
-100 個のテストケースでテストしてOKならエラーにならないのですが、途中でギブアップした場合はエラーになります。-/
+100 個のテストケースでテストして反例が見つからなかった場合、ギブアップして [`sorry`](./Sorry.md) と同様にはたらきます。-/
 
 /--
 warning: Gave up after failing to generate values that fulfill the preconditions 100 times.
@@ -25,3 +23,20 @@ warning: declaration uses 'sorry'
 #guard_msgs (warning) in
   example (a : Nat) : a ≠ a → a ≤ 1 := by
     plausible
+
+/- ## 引数
+引数として、オプションを渡すことができます。
+
+### numInst
+
+`numInst` を設定すると、ギブアップするまでに行うテストの回数を指定することができます。
+-/
+
+/--
+warning: Gave up after failing to generate values that fulfill the preconditions 10 times.
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs (warning) in
+  example (a : Nat) : a ≠ a → a ≤ 1 := by
+    plausible (config := { numInst := 10 })
