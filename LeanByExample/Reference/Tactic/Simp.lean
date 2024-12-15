2ee1b8a82d4660162fa86dfeefb6df779bd46f7e
Seasawher <tellagoodstory@icloud.com>
2024-12-15 17:28:51 +0900

    `variable` を使用しない

--- LeanByExample/Reference/Tactic/Simp.lean
+++ LeanByExample/Reference/Tactic/Simp.lean
@@ -4,13 +4,10 @@
 import Mathlib.Tactic.Ring -- `ring` を使うため --#
 import Mathlib.Tactic.Says -- `says` を使うために必要 --#
 import Mathlib.Tactic.Tauto -- `tauto` を使うため
-namespace Simp --#
-
-variable {P Q R : Prop}
 
 set_option linter.flexible false in --#
 
-example : (P ∨ Q ∨ R) ∧ R ↔ R := by
+example {P Q R : Prop} : (P ∨ Q ∨ R) ∧ R ↔ R := by
   -- simp だけでは証明が終わらない
   simp
   show R → P ∨ Q ∨ R
@@ -20,7 +17,7 @@ example : (P ∨ Q ∨ R) ∧ R ↔ R := by
   tauto
 
 @[simp]
-theorem or_and : (P ∨ Q ∨ R) ∧ R ↔ R := by
+theorem or_and {P Q R : Prop} : (P ∨ Q ∨ R) ∧ R ↔ R := by
   constructor
 
   -- 左から右を示す
@@ -40,7 +37,7 @@ theorem or_and : (P ∨ Q ∨ R) ∧ R ↔ R := by
     tauto
 
 -- 一度登録した命題は `simp` で示せるようになる。
-example : (P ∨ Q ∨ R) ∧ R ↔ R := by simp
+example {P Q R : Prop} : (P ∨ Q ∨ R) ∧ R ↔ R := by simp
 
 /- なお、`[simp]` 属性を付与した命題は「左辺を右辺に」単純化するルールとして登録されます。
 左辺と右辺を間違えて登録すると、無限ループになって `simp` の動作が破壊されることがあります。`[simp]` 属性は慎重に登録してください。-/
@@ -70,16 +67,14 @@ end
 
 既知の `h : P` という命題を使って単純化させたいときは、明示的に `simp [h]` と指定することで可能です。複数個指定することもできます。また `simp only [h₁, ... , hₖ]` とすると `h₁, ... , hₖ` だけを使用して単純化を行います。-/
 
-example (h : R) : (P ∨ Q ∨ R) ∧ R := by
+example {P Q R : Prop} (h : R) : (P ∨ Q ∨ R) ∧ R := by
   simp only [or_and]
   assumption
 
 /- 単に名前を定義に展開したい場合は [`dsimp`](./Dsimp.md) を使用します。
 `simp` は何も指定しなければゴールを単純化しますが、ローカルコンテキストにある `h : P` を単純化させたければ `simp at h` と指定することで可能です。ゴールと `h` の両方を単純化したいときは `simp at h ⊢` とします。-/
 
-variable {n m : Nat}
-
-example (h : n + 0 + 0 = m) : n = m + (0 * n) := by
+example {n m : Nat} (h : n + 0 + 0 = m) : n = m + (0 * n) := by
   simp only [add_zero, zero_mul] at h ⊢
   assumption
 
@@ -88,17 +83,17 @@ example (h : n + 0 + 0 = m) : n = m + (0 * n) := by
 /- ## simpa
 `simpa` は、`simp` を実行した後 `assumption` を実行するという一連の流れを一つのタクティクにしたものです。`simpa at h` 構文は存在せず、`simpa using h` と書くことに注意してください。-/
 
-example (h : R) : (P ∨ Q ∨ R) ∧ R := by
+example {P Q R : Prop} (h : R) : (P ∨ Q ∨ R) ∧ R := by
   simpa only [or_and]
 
-example (h : n + 0 + 0 = m) : n = m := by
+example {n m : Nat} (h : n + 0 + 0 = m) : n = m := by
   simpa using h
 
 /- ## simp?
 
 `simp` は自動的に証明を行ってくれますが、何が使われたのか知りたいときもあります。`simp?` は単純化に何が使われたのかを示してくれるので、`simp only` などを用いて明示的に書き直すことができます。-/
 
-example : (P ∨ Q ∨ R) ∧ R ↔ R := by
+example {P Q R : Prop} : (P ∨ Q ∨ R) ∧ R ↔ R := by
   simp? says
     simp only [or_and]
 
@@ -133,5 +128,3 @@ example {P Q : Prop} (hP : P) (hQ : Q) : P ∧ (Q ∧ (P → Q)) := by
   fail_if_success simp at *
 
   simp_all
-
-end Simp --#
