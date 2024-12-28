319f9618dd19d24d7fc6acf4a59003d13eec36ba
Seasawher <tellagoodstory@icloud.com>
2024-12-28 18:44:03 +0900

    `suffices` のページの校正・加筆修正

--- LeanByExample/Tactic/Suffices.lean
+++ LeanByExample/Tactic/Suffices.lean
@@ -7,14 +7,34 @@
 * `suffices h : Q from` のブロック内で、仮定に `h : Q` が追加される。
 * `suffices h : Q from` 以降で、ゴールが `⊢ Q` に書き換えられる。
 
-[`apply`](./Apply.md) と似ていますが、`apply` と違って「十分条件になっていること」の証明が明らかでないときにも使うことができます。
+[`apply`](./Apply.md) タクティクと似ていますが、`apply` と違って「十分条件になっていること」の証明が明らかでないときにも使うことができます。-/
 
-`suffices Q from ...` という形式の場合は、証明を直接構成することが必要です。`suffices Q from by ...` とすると、タクティクによって証明を構成するモードになります。 -/
-import Mathlib.Data.Nat.Order.Lemmas -- 除算 `∣` を使うため
+example (P : Prop) : ¬ ¬ (P ∨ ¬ P) := by
+  intro h
 
-example : 13 ∣ (2 ^ 70 + 3 ^ 70) := by
-  -- 余りがゼロであることを示せば十分
-  suffices goal : (2 ^ 70 + 3 ^ 70) % 13 = 0 from by
-    exact Iff.mpr (Nat.dvd_iff_div_mul_eq (2 ^ 70 + 3 ^ 70) 13) rfl
+  -- `¬ P` を示せば十分である。
+  suffices hyp : ¬ P from by
+    -- 仮定に `¬ P` が追加される。
+    guard_hyp hyp : ¬ P
 
-  rfl
+    -- このとき、特に `P ∨ ¬ P` が成り立つので、示すべきことが言える。
+    have : P ∨ ¬ P := by simp_all
+    contradiction
+
+  -- 無事ゴールを `¬ P` に帰着させることができた。
+  -- 以下 `¬ P` を示す。
+  guard_target =ₛ ¬ P
+
+  intro hP
+  have : P ∨ ¬ P := by simp_all
+  contradiction
+
+/- ## 構文
+`suffices Q from by ...` という構文では、タクティクによって証明を構成するモードになります。`suffices Q from ...` という構文では、証明項を直接構成するモードになります。 -/
+
+example (n : Nat) (h : n ≤ 0) : n = 0 := by
+  -- `n = 0` を示すためには、`n ≤ 0` であることを示せば十分である。
+  suffices hyp : n ≤ 0 from Nat.eq_zero_of_le_zero h
+
+  -- `n ≤ 0` であることを示す。
+  assumption
