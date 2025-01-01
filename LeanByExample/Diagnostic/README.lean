1856946d8375ad4c78c5a1995ec2ebaa836dfd27
Seasawher <tellagoodstory@icloud.com>
2025-01-01 17:52:29 +0900

    宣言的コマンド、対話的コマンドという言葉について補足し、一般的な用語でないことを注意する

--- LeanByExample/Diagnostic/README.lean
+++ LeanByExample/Diagnostic/README.lean
@@ -1,5 +1,7 @@
 /- # 対話的コマンド
 トップレベルコマンドのうち、`#` から始まるものを本書では便宜的に「対話的コマンド」と呼んでいます。これは、そうしたコマンドの多くが「式を評価した値を調べる」「式の型を調べる」など Lean から情報を得るために使用されるからです。
 
-構文的には宣言的コマンドと対話的コマンドは同じ振る舞いをします。
+```admonish warning title="注意"
+対話的コマンド(diagnostic command)という用語は一般的なものではありません。Lean コミュニティで通じない可能性もあります。著者は diagnostic command という用語を [The Hitchhiker's Guide to Logical Verification](https://github.com/blanchette/interactive_theorem_proving_2024) から拝借しましたが、同じものが hash command と呼ばれているのを聞いたこともあります。決まった呼び方がないという印象です。
+```
 -/
