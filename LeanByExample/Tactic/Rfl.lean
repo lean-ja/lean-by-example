4965a4f968c9fc2a26693472d13fc4d066f5f687
Seasawher <tellagoodstory@icloud.com>
2025-01-07 23:01:47 +0900

    `rfl` のコード例で Mathlib を使用しない Fixes #846

--- LeanByExample/Tactic/Rfl.lean
+++ LeanByExample/Tactic/Rfl.lean
@@ -1,9 +1,8 @@
 /- # rfl
 
-`rfl` は、反射的(reflexive)な関係(relation)に対して関係式を示すタクティクです。ここで二項関係 `R : α → α → Prop` が反射的であるとは、任意の `a : α` に対して `R a a` が成り立つことをいいます。
+`rfl` は、反射的(reflexive)な関係に対して関係式を示すタクティクです。ここで二項関係 `R : α → α → Prop` が反射的であるとは、任意の `a : α` に対して `R a a` が成り立つことをいいます。
 
-関係 `R` が反射的であることを `rfl` に利用させるには、`R` の反射性を示した定理に `@[refl]` タグを付けます。`@[refl]` で登録された定理を用いるので、追加でライブラリを import することにより示すことができる命題を増やせます。 -/
-import Mathlib.Tactic -- 大雑把に import する
+関係 `R` が反射的であることを `rfl` に利用させるには、`R` の反射性を示した定理に `@[refl]` タグを付けます。-/
 
 universe u
 
@@ -11,7 +10,7 @@ universe u
 inductive MyEq {α : Type u} : α → α → Prop
   | refl (a : α) : MyEq a a
 
-example (n : ℕ) : MyEq n n := by
+example (n : Nat) : MyEq n n := by
   -- `rfl` で示すことはできない。
   fail_if_success rfl
 
@@ -21,11 +20,7 @@ example (n : ℕ) : MyEq n n := by
 attribute [refl] MyEq.refl
 
 -- `rfl` で示せるようになる
-example (n : ℕ) : MyEq n n := by rfl
-
-/- 適宜ライブラリが必要ですが、不等式 `≤` の反射性も登録されているので `rfl` で示すことができます。-/
-
-example (n : ℕ) : n ≤ n := by rfl
+example (n : Nat) : MyEq n n := by rfl
 
 /- ## 補足
 2項関係 `(· ≡ ·) : A → A → Prop` の反射性とは、任意の `a : A` に対して `a ≡ a` が成り立つという性質です。反射性を使って何かを示すと言っても、`a ≡ a` という形の命題しか示せないのではないかという気がするかもしれません。しかし実際には `rfl` は定義に従って項の簡約を行うため、`a` と `b` が定義上等しいものであれば `a ≡ b` を `rfl` で示すことができます。
