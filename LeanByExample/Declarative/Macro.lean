991418fc4ea1c36bfd806cfb4b2af1202f79a1c6
Seasawher <tellagoodstory@icloud.com>
2025-01-10 01:45:58 +0900

    `macro` のページの校正

--- LeanByExample/Declarative/Macro.lean
+++ LeanByExample/Declarative/Macro.lean
@@ -1,11 +1,10 @@
 /- # macro
 
-`macro` は、その名の通りマクロを定義するための簡便なコマンドです。ただしマクロとは、構文を受け取って新しい構文を返す関数のことです。
+`macro` は、その名の通りマクロを定義するための簡便なコマンドです。ただし[マクロ](#{root}/Type/Macro.md)とは、構文を受け取って新しい構文を返す関数のことです。
 -/
-import Mathlib.Data.Real.Sqrt
-namespace Macro --#
+import Mathlib.Data.Real.Sqrt --#
 
-open Lean Parser
+open Lean Parser in
 
 /-- `s : String` をパースして `Syntax` の項を得る。`cat` は構文カテゴリ。-/
 def parse (cat : Name) (s : String) : MetaM Syntax := do
@@ -14,24 +13,26 @@ def parse (cat : Name) (s : String) : MetaM Syntax := do
 -- 最初は `#greet` が未定義なので、合法的なLeanのコマンドとして認識されない
 /-- error: <input>:1:0: expected command -/
 #guard_msgs in
-  run_meta parse `command "#greet"
+  #eval parse `command "#greet"
 
 -- `#greet` コマンドを定義する
-scoped macro "#greet " : command => `(#eval "Hello World!")
+macro "#greet " : command => `(#eval "Hello World!")
 
 -- `#greet` コマンドが使用可能になった
 #greet
 
-/- 上記の例の `#greet` コマンドは引数を持ちませんが、引数を取るようなものも定義できます。-/
+/- ## 引数を取るマクロの例
+冒頭の例の `#greet` コマンドは引数を持ちませんが、引数を取るようなものも定義できます。-/
 
 -- 引数を取って、引数に対して挨拶するコマンドを定義する
 -- 引数は `$` を付けると展開できる
-scoped macro "#hello " id:term : command => `(#eval s!"Hello, {$id}!")
+macro "#hello " id:term : command => `(#eval s!"Hello, {$id}!")
 
 /-- info: "Hello, Lean!" -/
 #guard_msgs in #hello "Lean"
 
-/- `macro` コマンドを使用すると、コマンドだけでなくタクティクの定義も行うことができます。-/
+/- ## マクロによるタクティク作例
+`macro` コマンドを使用すると、コマンドだけでなくタクティクの定義も行うことができます。-/
 
 -- 平方根の計算
 example : √4 = 2 := by
@@ -53,5 +54,3 @@ macro "norm_sqrt" : tactic => `(tactic| with_reducible
 -- 新しいタクティクにより一発で証明が終わるようになった！
 example : √4 = 2 := by norm_sqrt
 example : √18 = 3 * √ 2 := by norm_sqrt
-
-end Macro --#
