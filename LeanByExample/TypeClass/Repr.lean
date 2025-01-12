207c68545a9eaca8f8ac2a3b232cc926fb58b100
Seasawher <tellagoodstory@icloud.com>
2025-01-13 03:43:21 +0900

    `Repr` のページの説明文の校正

--- LeanByExample/TypeClass/Repr.lean
+++ LeanByExample/TypeClass/Repr.lean
@@ -2,9 +2,8 @@
 # Repr
 `Repr` は、その型の項をどのように表示するかを指示する型クラスです。
 
-たとえば、以下のように新しく[構造体](../Declarative/Structure.md) `Point` を定義したとき、何も指定しなくても `Point` の項を `#eval` で表示することはできますが、裏で使用しているのが `Repr` インスタンスです。
+たとえば、以下のように新しく[構造体](#{root}/Declarative/Structure.md) `Point` を定義したとき、何も指定しなくても `Point` の項を `#eval` で表示することはできますが、実は裏で `Repr` インスタンスを利用しています。
 -/
-namespace Repr --#
 
 -- 平面上の点を表す構造体
 structure Point (α : Type) : Type where
@@ -14,38 +13,39 @@ structure Point (α : Type) : Type where
 -- 原点
 def origin : Point Nat := ⟨0, 0⟩
 
--- Repr インスタンスを暗黙的に生成しない
+-- `origin` の中身を表示することができる
+/-- info: { x := 0, y := 0 } -/
+#guard_msgs in #eval origin
+
+-- Repr インスタンスを暗黙的に生成しないように設定
 set_option eval.derive.repr false
 
+-- 表示できずにエラーになった！
 /--
 error: could not synthesize a 'Repr' or 'ToString' instance for type
   Point Nat
 -/
 #guard_msgs in #eval origin
 
-/- エラーメッセージが示すように、`Point` が型クラス `Repr` または `ToString` のインスタンスではないため `#eval` が実行できずエラーになります。
-
-`Repr` インスタンスの登録方法ですが、通り一遍の表示で構わなければ [`deriving`](../Declarative/Deriving.md) コマンドで Lean に自動生成させることができます。-/
+/- `Repr` インスタンスの登録方法ですが、通り一遍の表示で構わなければ [`deriving`](#{root}/Declarative/Deriving.md) コマンドで Lean に自動生成させることができます。-/
 
 deriving instance Repr for Point
 
 /-- info: { x := 0, y := 0 } -/
-#guard_msgs in
-  #eval origin
+#guard_msgs in #eval origin
 
-/- あるいは、`Point` の定義時に以下のようにしても構いません。-/
-namespace Hidden --#
+/- あるいは、そもそも型を定義する際に [`deriving`](#{root}/Declarative/Deriving.md) 句を用いて生成しても良いでしょう。-/
 
-structure Point (α : Type) : Type where
+structure Point' (α : Type) : Type where
   x : α
   y : α
 deriving Repr
 
-def origin : Point Nat := ⟨0, 0⟩
+def origin' : Point' Nat := ⟨0, 0⟩
 
-#eval origin
+-- 評価できる
+#eval origin'
 
-end Hidden --#
 /- ## deriving を使わない場合
 `Repr` のインスタンスは [`deriving`](../Declarative/Deriving.md) コマンドで生成できますが、手動で作ることもできます。
 -/
@@ -91,5 +91,3 @@ instance [ToString α] : ToString (NestedList α) where
 
 /-- info: [1, [2, 3], 4] -/
 #guard_msgs in #eval sample
-
-end Repr --#
