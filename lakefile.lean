2d853c3b98ff4e9f8e3f88b01e5322a123cb2086
Seasawher <tellagoodstory@icloud.com>
2025-01-17 23:10:10 +0900

    Lean とライブラリのバージョン更新

--- lakefile.lean
+++ lakefile.lean
@@ -24,7 +24,7 @@ require mdgen from git
   "https://github.com/Seasawher/mdgen" @ "main"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 @[default_target]
 lean_lib LeanByExample where
