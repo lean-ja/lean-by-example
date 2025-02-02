61d1aded388be3fd9facb08da3a50152a56a1bbc
Seasawher <tellagoodstory@icloud.com>
2025-02-02 14:45:20 +0900

    strict positivity requirement の説明をより一般的にする Fixes #1440

--- LeanByExample/Declarative/Inductive.lean
+++ LeanByExample/Declarative/Inductive.lean
@@ -9,7 +9,7 @@
 
 帰納型の最も基本的な例は、次のような列挙型です。列挙型とは、固定された値のどれかを取るような型です。
 -/
-
+import Mathlib.SetTheory.Cardinal.Basic --#
 /-- 真か偽のどちらかの値をとる型 -/
 inductive MyBool where
   | true
@@ -301,14 +301,59 @@ opaque A : Type
 unsafe inductive Bad where
   | mk (f : Bad → A)
 
-unsafe def selfApply (b : Bad) : A :=
-  match b with
-  | Bad.mk f => f b
+section
+  /- ## A が空の場合
 
-unsafe def ω : A := selfApply (Bad.mk selfApply)
+  `A` が空なら、`A` は `False` と同じなので矛盾が導かれる。
+  -/
 
--- A は任意の型なのに、Inhabited インスタンスが構築できてしまった！
-unsafe instance : Inhabited A := ⟨ω⟩
+  unsafe def selfApply (b : Bad) : A :=
+    match b with
+    | Bad.mk f => f b
+
+  -- A の項が A の情報を使わずに構成できてしまった
+  unsafe def ω : A := selfApply (Bad.mk selfApply)
+
+  /-- `A` が空なら矛盾が導かれる -/
+  unsafe example [IsEmpty A] : False :=
+    IsEmpty.false ω
+end
+
+section
+  /- ## A に２つ以上の要素があるとき -/
+
+  /-- `Bad` と `Bad → A` の間に全単射がある -/
+  unsafe def equiv : Bad ≃ (Bad → A) where
+    toFun := fun ⟨f⟩ => f
+    invFun := Bad.mk
+    left_inv := by
+      intro ⟨f⟩
+      rfl
+    right_inv := by
+      intro f
+      rfl
+
+  open scoped Cardinal
+
+  unsafe example [Nontrivial A] : False := by
+    -- `Bad` と `Bad → A` は全単射があるので濃度(`#`)が同じ
+    have h : # Bad = # (Bad → A) :=
+      Cardinal.mk_congr equiv
+
+    -- 特に、`# Bad = # A ^ # Bad` が成り立つ
+    replace h : # Bad = #A ^ # Bad := by
+      simpa [Cardinal.mk_pi, Cardinal.prod_const, Cardinal.lift_id] using h
+
+    -- しかしカントール(Cantor)の定理により、`# A ≥ 2` ならば
+    -- `# Bad < # A ^ # Bad` が成り立つ
+    have : # Bad < #A ^ # Bad := by
+      apply Cardinal.cantor' (a := # Bad) (b := # A)
+      rw [Cardinal.one_lt_iff_nontrivial]
+      infer_instance
+
+    -- これは矛盾
+    apply ne_of_lt this h
+end
 
 /- ここで、`ω` の定義が無限ループに陥っていることは指摘する価値があります。 -/
 
