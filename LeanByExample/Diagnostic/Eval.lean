c6c0e2900da9db17aea76880be04c6da22d40155
Seasawher <tellagoodstory@icloud.com>
2025-02-07 01:47:41 +0900

    `#eval` のコード例から `#guard_msgs` を隠す / #eval の方が #guard_msgs より基本的なコマンドであるため

--- LeanByExample/Diagnostic/Eval.lean
+++ LeanByExample/Diagnostic/Eval.lean
@@ -1,42 +1,42 @@
+import Lean.Elab.Command --#
 /- # \#eval
 `#eval` コマンドは、式の値をその場で評価します。
 -/
 
-/-- info: 2 -/
-#guard_msgs in
-  #eval 1 + 1
+/-⋆-//-- info: 2 -/
+#guard_msgs in --#
+#eval 1 + 1
 
 -- 階乗関数
 def fac : Nat → Nat
   | 0 => 1
   | n + 1 => (n + 1) * fac n
 
-/-- info: 120 -/
-#guard_msgs in
-  #eval fac 5
+/-⋆-//-- info: 120 -/
+#guard_msgs in --#
+#eval fac 5
 
 def main : IO Unit :=
   IO.println "Hello, world!"
 
--- `#eval` により一部の IO アクションを実行することもできる
-/-- info: Hello, world! -/
-#guard_msgs in
-  #eval main
+/-⋆-//-- info: Hello, world! -/
+#guard_msgs in --#
+#eval main
 
 /- ## よくあるエラー
 式の評価を行うコマンドであるため、型や関数など、評価のしようがないものを与えるとエラーになります。-/
 
 -- 型は評価できない
-/-- error: cannot evaluate, types are not computationally relevant -/
-#guard_msgs in
-  #eval Nat
+/-⋆-//-- error: cannot evaluate, types are not computationally relevant -/
+#guard_msgs in --#
+#eval Nat
 
 -- 関数そのものも評価できない
-/--
-error: could not synthesize a 'Repr' or 'ToString' instance for type
+/-⋆-//--
+error: could not synthesize a 'ToExpr', 'Repr', or 'ToString' instance for type
   Nat → Nat
 -/
-#guard_msgs in
-  #eval (fun x => x + 1)
+#guard_msgs in --#
+#eval (fun x => x + 1)
 
 /- 一般に、[`Repr`](#{root}/TypeClass/Repr.md) や [`ToString`](#{root}/TypeClass/ToString.md) および `ToExpr` のインスタンスでないような型の項は `#eval` に渡すことができません。-/
