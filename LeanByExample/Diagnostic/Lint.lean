e5d2e7e956de4397dd202d7df08c2be3a74cef92
Seasawher <tellagoodstory@icloud.com>
2025-02-07 20:24:31 +0900

    `#lint` のページの校正

--- LeanByExample/Diagnostic/Lint.lean
+++ LeanByExample/Diagnostic/Lint.lean
@@ -2,26 +2,12 @@
 
 `#lint` コマンドは、リンタ（よくない書き方をされたコードを指摘してくれるツール）を実行します。
 
-たとえば、以下は「ドキュメントコメントのない定理に対して警告するリンタ」を実行する例です。
 -/
 import Batteries.Tactic.Lint
 
 -- ドキュメントコメントのない定理
 theorem hoge : True := by trivial
 
--- エラーにならない
-/--
-info: -- Found 0 errors in 1 declarations
-(plus 0 automatically generated ones) in the current file with 1 linters
-
--- All linting checks passed!
--/
-#guard_msgs (whitespace := lax) in
-  -- 定義にドキュメントコメントがあるか確認するリンタ
-  -- 定理は対象外なのでスルーされる
-  #lint only docBlame
-
--- エラーになっている
 /--
 error: -- Found 1 error in 1 declarations (plus 0 automatically generated ones) in the current file with 1 linters
 
@@ -30,5 +16,20 @@ THEOREMS ARE MISSING DOCUMENTATION STRINGS: -/
 #check hoge /- theorem missing documentation string -/
 -/
 #guard_msgs (error) in
-  -- ドキュメントコメントのない定理に対して警告するリンタを実行している
+  -- ドキュメントコメントのない定理に対して警告するリンタ
   #lint only docBlameThm
+
+-- ドキュメントコメントのない定義
+def fuga : True := by trivial
+
+/--
+error: -- Found 1 error in 2 declarations (plus 0 automatically generated ones) in the current file with 1 linters
+
+/- The `docBlame` linter reports:
+DEFINITIONS ARE MISSING DOCUMENTATION STRINGS: -/
+#check fuga /- definition missing documentation string -/
+-/
+#guard_msgs (whitespace := lax) in
+  -- 定義にドキュメントコメントがあるか確認するリンタ
+  -- 定理は対象外なのでスルーされる
+  #lint only docBlame
