726c23935d3bba986554e2e04eb51f2208c0246a
Seasawher <tellagoodstory@icloud.com>
2025-02-08 13:20:53 +0900

    `syntax` のページの校正

--- LeanByExample/Declarative/Syntax.lean
+++ LeanByExample/Declarative/Syntax.lean
@@ -19,10 +19,11 @@ def parse (cat : Name) (s : String) : MetaM Syntax := do
 -- `#greet` というコマンドのための構文を定義
 syntax "#greet" : command
 
--- `#greet` というコマンドが Lean に認識されるようになった。
+-- まだエラーになるが、少なくとも `#greet` というコマンドが Lean に認識されるようにはなった。
 -- エラーメッセージは、`#greet` コマンドの解釈方法がないと言っている。
-/-- error: elaboration function for '«command#greet»' has not been implemented -/
-#guard_msgs in #greet
+/-⋆-//-- error: elaboration function for '«command#greet»' has not been implemented -/
+#guard_msgs in --#
+#greet
 
 /- Lean に構文を認識させるだけでなく、解釈させるには [`macro_rules`](./MacroRules.md) などの別のコマンドが必要です。-/
 
@@ -30,42 +31,48 @@ syntax "#greet" : command
 macro_rules
   | `(command| #greet) => `(#eval "Hello, Lean!")
 
+/-⋆-//-- info: "Hello, Lean!" -/
+#guard_msgs in --#
+#greet
+
 /- ## パース優先順位
-`syntax` コマンドは Lean に新しい構文解析ルールを追加するので、既存の構文と衝突して意図通りに解釈されないことがあります。
+`syntax` コマンドは Lean に新しい構文解析ルールを追加しますが、導入した構文が意図通りに解釈されないことがあります。
 -/
-section --#
+section
 
-/-- `a = b as T` という構文を定義 -/
-local syntax term " = " term " as " term : term
+  /-- `a = b as T` という構文を定義 -/
+  local syntax term " = " term " as " term : term
 
-/-- `a = b as T` という構文を、型 `T` 上で `a = b` が成り立つと解釈させる -/
-local macro_rules
-  | `(term| $a = $b as $c) => `(@Eq (α := $c) $a $b)
+  /-- `a = b as T` という構文を、型 `T` 上で `a = b` が成り立つと解釈させる -/
+  local macro_rules
+    | `(term| $a = $b as $c) => `(@Eq (α := $c) $a $b)
 
--- メタ変数の番号を表示しない
-set_option pp.mvars false
+  -- メタ変数の番号を表示しない
+  set_option pp.mvars false
 
--- `1 + (1 = 2)` だと認識されてしまっている
-/-- info: 1 + (1 = 2) : ?_ -/
-#guard_msgs (info, drop error) in
-  #check (1 + 1 = 2 as Nat)
+  -- `1 + (1 = 2)` だと認識されてしまっている
+  /-- info: 1 + (1 = 2) : ?_ -/
+  #guard_msgs (info, drop error) in
+    #check (1 + 1 = 2 as Nat)
 
-end --#
+end
 /- **パース優先順位(parsing precedence)** を設定することで、どの構文から順に解釈されるかを指定することができ、問題を修正できることがあります。このあたりは [`notation`](./Notation.md) コマンドと同様です。 -/
-section --#
+section
 
--- 十分低いパース優先順位を設定する
-local syntax:10 term:10 " = " term:10 " as " term:10 : term
+  -- 十分低いパース優先順位を設定する
+  local syntax:10 term:10 " = " term:10 " as " term:10 : term
 
-local macro_rules
-  | `(term| $a = $b as $c) => `(@Eq (α := $c) $a $b)
+  local macro_rules
+    | `(term| $a = $b as $c) => `(@Eq (α := $c) $a $b)
 
--- 意図通りに構文解析が通るようになる
-#guard (1 + 1 = 2 as Nat)
+  -- 意図通りに構文解析が通るようになる
+  #guard (1 + 1 = 2 as Nat)
 
-#guard (3 - 5 + 4 = 2 as Int)
+  #guard (3 - 5 + 4 = 2 as Int)
 
-end --#
+  -- Nat だと 3 - 5 = 0 となるので結果が変わる
+  #guard (3 - 5 + 4 = 4 as Nat)
+end
 /- ## name 構文
 `(name := ...)` という構文により、名前を付けることができます。名前を付けると、その名前で `Lean.ParserDescr` の項が生成されます。
 -/
