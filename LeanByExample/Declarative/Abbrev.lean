03ca737edbd5d994fcda6e13d383a5b706e517ba
Seasawher <tellagoodstory@icloud.com>
2025-02-08 21:46:33 +0900

    `abbrev` のページの校正

--- LeanByExample/Declarative/Abbrev.lean
+++ LeanByExample/Declarative/Abbrev.lean
@@ -8,9 +8,9 @@ namespace Abbrev0 --#
 
 def NaturalNumber : Type := Nat
 
-/- しかし、ここで定義した `Nat` の別名を項に対して使用するとエラーになります。エラーメッセージには以下の通り「`NaturalNumber` の `OfNat` インスタンスが見つかりません」という旨のことが書かれています。これは、Lean が `NaturalNumber` を定義に簡約(reduce)するよりも先に、`42 : NaturalNumber` という表記が定義されているか `OfNat` のインスタンスを探そうとしてエラーになったことを示唆します。-/
+/- しかし、ここで定義した `Nat` の別名を項に対して使用するとエラーになります。エラーメッセージには以下の通り「`NaturalNumber` の [`OfNat`](#{root}/TypeClass/OfNat.md) インスタンスが見つかりません」という旨のことが書かれています。これは、Lean が `NaturalNumber` を定義に展開してそれが実は `Nat` に等しいことを知るよりも先に、`42 : NaturalNumber` という表記が定義されているか [`OfNat`](#{root}/TypeClass/OfNat.md) のインスタンスを探そうとしてエラーになったことを示唆します。-/
 
-/--
+/-⋆-//--
 error: failed to synthesize
   OfNat NaturalNumber 42
 numerals are polymorphic in Lean, but the numeral `42` cannot be used in a context where the expected type is
@@ -18,8 +18,8 @@ numerals are polymorphic in Lean, but the numeral `42` cannot be used in a conte
 due to the absence of the instance above
 Additional diagnostic information may be available using the `set_option diagnostics true` command.
 -/
-#guard_msgs in
-  #check (42 : NaturalNumber)
+#guard_msgs in --#
+#check (42 : NaturalNumber)
 
 end Abbrev0 --#
 /- ここでエラーを修正する方法の一つが、`def` の代わりに `abbrev` を使用することです。-/
@@ -36,8 +36,9 @@ end Abbrev1 --#
 abbrev NaturalNumber : Type := Nat
 
 -- `[reducible]` 属性が付与されている
-/--
+/-⋆-//--
 info: @[reducible] def NaturalNumber : Type :=
 Nat
 -/
-#guard_msgs in #print NaturalNumber
+#guard_msgs in --#
+#print NaturalNumber
