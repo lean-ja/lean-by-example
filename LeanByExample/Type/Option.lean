faa0c6e15b125fbc1abb319ded0b9f96467ac1b3
Seasawher <tellagoodstory@icloud.com>
2025-02-09 19:22:04 +0900

    `Option` の `Functor` インスタンスを紹介する

--- LeanByExample/Type/Option.lean
+++ LeanByExample/Type/Option.lean
@@ -36,3 +36,25 @@ end Hidden --#
 #guard Array.get? #[1, 2, 3] 3 = none
 
 /- 配列やリストなどのコレクション型から要素を取り出す類の操作には、多くの場合返り値が `Option` に包まれた関数が用意されます。より詳しくは [`GetElem`](#{root}/TypeClass/GetElem.md) のページを参照してください。 -/
+
+/- ## Functor インスタンス
+
+`Option` は [`Functor`](#{root}/TypeClass/Functor.md) 型クラスのインスタンスであり、`<$>` が使用できます。実装上は `Option.map` が使用されます。`Option.map f x?` は、`x?` が `some x` ならば `some (f x)` を返し、`x?` が `none` ならば `none` を返します。
+-/
+section
+
+  variable {α β : Type}
+
+  -- `<$>` は `Option.map` で実装されている
+  example (x? : Option α) (f : α → β) : f <$> x? = Option.map f x? := by
+    rfl
+
+  -- `Option.map f` は some x を some (f x) に写す
+  example (x : α) (f : α → β) : Option.map f (some x) = some (f x) := by
+    rfl
+
+  -- `Option.map f` は none を none に写す
+  example (f : α → β) : Option.map f none = none := by
+    rfl
+
+end
