f92e7746f2c5e101cb2ef0eef0591fb66fb79939
Seasawher <tellagoodstory@icloud.com>
2025-02-13 23:20:42 +0900

    `HMul` の例を校正する

--- LeanByExample/TypeClass/HMul.lean
+++ LeanByExample/TypeClass/HMul.lean
@@ -3,16 +3,12 @@
 `HMul` (heterogeneous multiplication)は `*` という二項演算子のための型クラスです。`*` 記号が何を意味するかについての制約はありません。任意の型 `α, β, γ : Type` に対して乗算 `(· * ·) : α → β → γ` を定義することができます。
 -/
 
--- メタ変数の番号を表示しない
-set_option pp.mvars false
-
 -- 最初は `*` 記号が定義されていないのでエラーになる
-#check_failure [1, 2, 3] * [4, 5, 6]
+#check_failure 2 * (· + 1)
 
-/-- `HMul` のインスタンスを定義する。
-`List Nat` 同士の積を、要素ごとの積で定義した。-/
-instance : HMul (List Nat) (List Nat) (List Nat) where
-  hMul xs ys := List.zipWith (· * ·) xs ys
+/-- `HMul` のインスタンスを定義する -/
+instance : HMul Nat (Nat → Nat) (Nat → Nat) where
+  hMul xs ys := fun m => xs * ys m
 
 -- 乗算記号が使えるようになった
-#guard [1, 2, 3] * [4, 5, 6] = [4, 10, 18]
+#check 2 * (· + 1)
