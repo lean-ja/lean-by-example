6ef4d646ea6b330e594f384df74acd576afe54a8
Seasawher <tellagoodstory@icloud.com>
2025-02-21 19:13:34 +0900

    `exact`のページの校正

--- LeanByExample/Tactic/Exact.lean
+++ LeanByExample/Tactic/Exact.lean
@@ -1,25 +1,31 @@
 /- # exact
 
 ゴールが `P` で、ローカルコンテキストに `hP : P` があるときに、`exact hP` はゴールを閉じます。`hP` がゴールの証明になっていないときには、失敗してエラーになります。-/
-variable (P Q : Prop)
 set_option linter.unusedVariables false in --#
 
-example (hP : P) (hQ : Q) : P := by
+example {P Q : Prop}(hP : P)(hQ : Q) : P := by
   -- `hQ : Q` は `P` の証明ではないのでもちろん失敗する
   fail_if_success exact hQ
 
   exact hP
 
 /- `exact` は与えられた証明項をそのまま証明として使うタクティクなので、`by exact` だけで証明が終わるときには、`by exact` を消しても証明が通ります。-/
+section
+  /- ## by exact の省略ができるケース -/
 
-example (hP : P) (hQ : Q) : P ∧ Q := by
-  exact And.intro hP hQ
+  variable {P Q : Prop}
 
-example (hP : P) (hQ : Q) : P ∧ Q := And.intro hP hQ
+  example (hP : P) (hQ : Q) : P ∧ Q := by
+    -- exact を使う証明
+    exact And.intro hP hQ
 
+  example (hP : P) (hQ : Q) : P ∧ Q :=
+    -- by exact を使わない証明
+    And.intro hP hQ
+end
 /- なお `And` は[構造体](#{root}/Declarative/Structure.md)なので[無名コンストラクタ](#{root}/Parser/AnonymousConstructor.md)記法を用いて次のように書くこともできます。-/
 
-example (hP : P) (hQ : Q) : P ∧ Q := ⟨hP, hQ⟩
+example {P Q : Prop} (hP : P) (hQ : Q) : P ∧ Q := ⟨hP, hQ⟩
 
 /- ## assumption との関連
 
