43ed4869d1f23e75940fa59ab9e5df7984f795bb
Seasawher <tellagoodstory@icloud.com>
2025-02-24 21:13:56 +0900

    農民の掛け算（#test)の出典を示す
    resolve #1653

--- LeanByExample/Diagnostic/Test.lean
+++ LeanByExample/Diagnostic/Test.lean
@@ -1,5 +1,5 @@
 /- # \#test
-`#test` コマンドは、Plausible ライブラリで定義されているもので、与えられた命題が成り立つかどうか、具体例をランダムに生成してチェックすることで検証します。[`plausible`](#{root}/Tactic/Plausible.md) タクティクと同様の機能を持ちます。
+`#test` コマンドは、Plausible ライブラリで定義されているもので、与えられた命題が成り立つかどうか、具体例をランダムに生成してチェックすることで検証します。[`plausible`](#{root}/Tactic/Plausible.md) タクティクと同様の機能を持ちます。[^erickson]
 -/
 import Plausible
 
@@ -22,3 +22,4 @@ def peasantMul (x y : Nat) : Nat := Id.run do
 
 -- 正しそう！
 #test ∀ n m, peasantMul n m = n * m
+/- [^erickson]: 以下に紹介する `peasantMul` 関数の例は、Jeff Erickson「Algorithms」の0章2節で紹介されているアルゴリズムを参考にさせていただきました。 -/
