d0aa1151de3589c733595d5e9b44e2c954118a06
Seasawher <tellagoodstory@icloud.com>
2025-03-02 21:38:07 +0900

    plausible を有効活用する例: 最小自由数のテスト
    resolve #1673

--- LeanByExample/Diagnostic/Test.lean
+++ LeanByExample/Diagnostic/Test.lean
@@ -22,4 +22,22 @@ def peasantMul (x y : Nat) : Nat := Id.run do
 
 -- 正しそう！
 #test ∀ n m, peasantMul n m = n * m
+
+/- `#test` コマンドを使う利点としては、期待される仕様をそのまま表現したものがテストとして機能するので、テストを作る労力が軽減されるというのがあります。仕様を述語論理で表現することが容易な関数のテストや、異なる実装をした関数同士が実は等しいといったテストで有用でしょう。 -/
+
+def List.max! {α : Type} [Inhabited α] [Max α] (xs : List α) : α :=
+  xs.max? |>.getD default
+
+/-- 与えられたリストに含まれない最小の要素を求める -/
+def minFree (xs : List Nat) : Nat :=
+  List.range (xs.max! + 2)
+    |>.removeAll xs
+    |>.head!
+
+-- `minFree`の出力は元のリストに含まれない
+#test ∀ xs : List Nat, minFree xs ∉ xs
+
+-- `minFree`の出力は、元のリストに含まれない数の集合に対して下界を与える
+#test ∀ xs : List Nat, ∀ x : Nat, x ∉ xs → minFree xs ≤ x
+
 /- [^erickson]: 以下に紹介する `peasantMul` 関数の例は、Jeff Erickson「Algorithms」の0章2節で紹介されているアルゴリズムを参考にさせていただきました。 -/
