4e110b6dcbe14018fd7a1f15f7c72852f2b44fe4
Seasawher <tellagoodstory@icloud.com>
2025-03-06 16:57:45 +0900

    `#help`のページの校正

--- LeanByExample/Diagnostic/Help.lean
+++ LeanByExample/Diagnostic/Help.lean
@@ -3,13 +3,16 @@
 `#help` は、ドキュメントを確認するためのコマンドです。以下のような機能があります。
 
 * `#help tactic` で全タクティクのリストが見られます。
-* `#help option` で全オプションのリストが見られます。オプションは、[`set_option`](#{root}/Declarative/SetOption.md) を実行することで切り替えられます。たとえば、`set_option warningAsError true` と書くと、warning(警告) がエラーとして扱われるようになります。
-* `#help attr` で全 attribute のリストが見られます。たとえば `simp` という attribute なら、`@[simp] def foo := ...` という風にして登録できます。
-
+* `#help option` で全オプションのリストが見られます。オプションは、[`set_option`](#{root}/Declarative/SetOption.md) を実行することで切り替えることができます。
+* `#help attr` で全[属性(attribute)](#{root}/Declarative/Attribute.md)のリストが見られます。
 * `#help command` で全コマンドのリストが見られます。
 * `#help term` ですべての term syntax の一覧が見られます。
 
 他にも機能がありますが、詳細は[Batteriesのドキュメント](https://leanprover-community.github.io/mathlib4_docs/Batteries/Tactic/HelpCmd.html)をご覧ください。
+
+```admonish info title=""
+[Mathlib4 Help](https://seasawher.github.io/mathlib4-help/) で `#help` コマンドの出力結果を見ることができます。
+```
 -/
 import Batteries.Tactic.HelpCmd
 
