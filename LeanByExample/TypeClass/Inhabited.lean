6b89b28a1d65874bc0522919c7b8129bb8fd2733
Seasawher <tellagoodstory@icloud.com>
2025-03-07 12:57:22 +0900

    `Inhabited`のページの校正

--- LeanByExample/TypeClass/Inhabited.lean
+++ LeanByExample/TypeClass/Inhabited.lean
@@ -4,23 +4,36 @@
 
 `Inhabited` のインスタンスである型は、`default` という項を持ちます。
 -/
+section
+  variable {α : Type} [Inhabited α]
 
-variable {α : Type} [Inhabited α]
-
-#check (default : α)
-
+  #check (default : α)
+end
 /- `Inhabited` の注意すべき使われ方として、クラッシュする関数の返り値として呼ばれるというものがあります。
 
 たとえば `a : Array` に対して `i` 番目の要素を取り出す処理を考えます。`i` 番目の要素が存在するとは限らないので、例外の処理が必要です。一つの方法は、「`i` 番目の要素がなければエラーにする」というものです。
 -/
 
-def get (a : Array α) (i : Nat) : α :=
+def get {α : Type} [Inhabited α] (a : Array α) (i : Nat) : α :=
   if h : i < a.size then
     a[i]'h
   else
     panic! "index out of bounds"
 
-/- 何気ない定義のように見えますが、この定義には `Inhabited α` が必要です。`panic!` でプログラムをクラッシュさせているのですが、このときに `α` が空でないことが要求されています。
+/- 何気ない定義のように見えますが、この定義には `Inhabited α` が必要です。`panic!` でプログラムをクラッシュさせているのですが、このときに `α` が空でないことが要求されています。-/
 
-これは Lean が定理証明支援系としてもプログラミング言語としても使えるようにするための技術的な制約からくる仕様です。もし空の型を返してプログラムがクラッシュすることが許されたとすると、「空の型は `False` に等しい」ので、クラッシュするプログラムを `False` の証明として扱える可能性が生じてしまいます。
+/-⋆-//--
+error: failed to synthesize
+  Inhabited α
+
+Additional diagnostic information may be available using the `set_option diagnostics true` command.
+-/
+#guard_msgs in --#
+def get' {α : Type} (a : Array α) (i : Nat) : α :=
+  if h : i < a.size then
+    a[i]'h
+  else
+    panic! "index out of bounds"
+
+/- これは Lean が定理証明支援系としてもプログラミング言語としても使えるようにするための技術的な制約からくる仕様です。もし空の型を返してプログラムがクラッシュすることが許されたとすると、「空の型は `False` に等しい」ので、クラッシュするプログラムを `False` の証明として扱える可能性が生じてしまいます。
 -/
