d7d048d1e9234bf470c71584183d16b937516cf1
Seasawher <tellagoodstory@icloud.com>
2025-03-11 06:18:09 +0900

    論理式を自作する例（Quote 使用例）
    resolve #1715

--- LeanByExample/TypeClass/Quote.lean
+++ LeanByExample/TypeClass/Quote.lean
@@ -27,7 +27,9 @@ end
 
 /- ## 使用例
 
-これと [`elab`](#{root}/Declarative/Elab.md) コマンドを組み合わせると、たとえば「存在命題を、候補を順に試すことで示すタクティク」を自作することができます。
+### 候補を順に試すタクティク
+
+`Quote` と [`elab`](#{root}/Declarative/Elab.md) コマンドを組み合わせると、たとえば「存在命題を、候補を順に試すことで示すタクティク」を自作することができます。
 -/
 
 open Lean Elab.Tactic Elab.Term in
@@ -54,3 +56,68 @@ example : ∃ n : Nat, (n + 2) / 3 = 5 := by
 
   -- 13 まで試すと見つかる
   try_for 13
+
+/- ### 論理式のための構文を用意する
+
+次のように、Lean のデータとして論理式の AST を定義したとします。
+-/
+
+/-- 論理式 -/
+inductive PropForm where
+  /-- 真 `⊤` -/
+  | tr : PropForm
+  /-- 偽 `⊥` -/
+  | fls : PropForm
+  /-- 命題変数 -/
+  | var : String → PropForm
+  /-- 論理積 `∧` -/
+  | conj : PropForm → PropForm → PropForm
+  /-- 論理和 `∨` -/
+  | disj : PropForm → PropForm → PropForm
+  /-- 含意 `→` -/
+  | impl : PropForm → PropForm → PropForm
+  /-- 否定 `¬` -/
+  | neg : PropForm → PropForm
+  /-- 同値 `↔` -/
+  | biImpl : PropForm → PropForm → PropForm
+deriving Repr, DecidableEq, Inhabited
+
+/- このとき、[`macro_rules`](#{root}/Declarative/MacroRules.md) コマンドと `Quote` を組み合わせることで、論理式のための構文を定義することができます。[^lamr] -/
+
+declare_syntax_cat propform
+
+/-- `PropForm` を見やすく定義するための構文 -/
+syntax "prop!{" propform "}" : term
+
+syntax:max ident : propform
+syntax "⊤" : propform
+syntax "⊥" : propform
+syntax:35 propform:36 " ∧ " propform:35 : propform
+syntax:30 propform:31 " ∨ " propform:30 : propform
+syntax:20 propform:21 " → " propform:20 : propform
+syntax:20 propform:21 " ↔ " propform:20 : propform
+syntax:max "¬ " propform:40 : propform
+syntax:max "(" propform ")" : propform
+
+macro_rules
+  | `(prop!{$p:ident}) => `(PropForm.var $(Lean.quote p.getId.toString))
+  | `(prop!{⊤}) => `(ProfForm.tr)
+  | `(prop!{⊥}) => `(ProfForm.fls)
+  | `(prop!{¬ $p}) => `(PropForm.neg prop!{$p})
+  | `(prop!{$p ∧ $q}) => `(PropForm.conj prop!{$p} prop!{$q})
+  | `(prop!{$p ∨ $q}) => `(PropForm.disj prop!{$p} prop!{$q})
+  | `(prop!{$p → $q}) => `(PropForm.impl prop!{$p} prop!{$q})
+  | `(prop!{$p ↔ $q}) => `(PropForm.biImpl prop!{$p} prop!{$q})
+  | `(prop!{($p:propform)}) => `(prop!{$p})
+
+-- 構文が使用できるようになった
+#check prop!{p ∧ q → (r ∨ ¬ p) → q}
+#check prop!{p ∧ q ∧ r → p}
+
+-- 構文のテスト
+#guard
+  let actual := prop!{p ∧ q}
+  let expected := PropForm.conj (PropForm.var "p") (PropForm.var "q")
+  expected == actual
+
+/- [^lamr]: この例は [Logic and Mechanized Reasoning](https://github.com/avigad/lamr) を参考にしました。 -/
