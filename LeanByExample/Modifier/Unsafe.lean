a58ea8ae8d971fa35153d87027938eb90e873601
Seasawher <tellagoodstory@icloud.com>
2025-03-27 22:12:27 +0900

    FBIP(Functional but inplace)を説明する
    resolve #1409

--- LeanByExample/Modifier/Unsafe.lean
+++ LeanByExample/Modifier/Unsafe.lean
@@ -17,17 +17,15 @@ inductive Bad where
 unsafe inductive Bad where
   | mk : (Bad → Bad) → Bad
 
-/- ## unsafe が付与された関数
+/- ## ptrAddrUnsafe { #ptrAddrUnsafe }
 
-Lean のライブラリには `unsafe` が付与された関数がいくつも存在します。
-
-たとえば `ptrAddrUnsafe` 関数は、値のメモリ上での位置を返す関数ですが、`unsafe` で修飾されています。 -/
+`ptrAddrUnsafe` 関数は、値のメモリ上での位置を返す関数ですが、`unsafe` で修飾されています。 -/
 
 /-⋆-//-- info: unsafe opaque ptrAddrUnsafe.{u} : {α : Type u} → α → USize -/
 #guard_msgs in --#
 #print ptrAddrUnsafe
 
-/- これは、`ptrAddrUnsafe` が **参照透過性(referential transparency)** を壊してしまうからです。参照透過性とは、「引数の値が同じならば関数の値も同じ」という性質で、Lean では全ての関数は参照透過であるべきというルールを課せられています。-/
+/- これは、`ptrAddrUnsafe` が **参照透過性(referential transparency)** を壊してしまうからです。参照透過性とは、「引数の値が同じならば関数の値も同じ」という性質です。`ptrAddrUnsafe` は等しい引数でも異なる値を返すことがあります。 -/
 
 /-⋆-//-- info: true -/
 #guard_msgs in --#
@@ -40,3 +38,46 @@ Lean のライブラリには `unsafe` が付与された関数がいくつも�
 
   -- 返り値が異なる!
   return u != v
+
+/- ## Functional but in-place { #FBIP }
+`unsafe` が付与されていない通常の関数は参照透過性を持ち、したがって特に Lean ではグローバル変数の値を変更することは通常できません。 -/
+
+-- foo という変数を定義する
+def foo := "hello"
+
+-- 再定義しようとするとエラーになる
+/-⋆-//-- error: 'foo' has already been declared -/
+#guard_msgs in --#
+def foo := "hello world"
+
+/-
+しかし、ローカル変数は破壊的に変更することができます。
+-/
+
+/-- フィボナッチ数列を計算する -/
+def fibonacci (n : Nat) : Array Nat := Id.run do
+  -- 可変な配列 `fib` を宣言する
+  let mut fib : Array Nat := Array.mkEmpty n
+  fib := fib.push 0
+  fib := fib.push 1
+  for i in [2:n] do
+    -- 更新前の配列 `fib` のメモリアドレスを取得
+    let old := unsafe ptrAddrUnsafe fib
+
+    -- `fib` を更新
+    fib := fib.push (fib[i-1]! + fib[i-2]!)
+
+    -- 更新後のメモリアドレスを取得
+    let new := unsafe ptrAddrUnsafe fib
+
+    -- メモリアドレスが一致する
+    assert! old = new
+  return fib
+
+-- 値がコピーされていれば panic するはずだが...?
+/-⋆-//-- info: #[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377] -/
+#guard_msgs in --#
+#eval fibonacci 15
+
+/- なぜこのようなことができるのかというと、Lean が値の不要なコピーを行わないからです。具体的には、Lean は **参照カウント** がちょうど１であるような値を更新する際に、コピーして新しい値を生成する代わりに破壊的変更を行います。これを指して、Lean 言語のパラダイムのことを **Functional but in-place** と呼ぶことがあります。
+-/
