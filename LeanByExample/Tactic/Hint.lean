d6c1b44f3d839f6df33fb4c3eee560ef03da78d7
Seasawher <tellagoodstory@icloud.com>
2025-05-11 14:19:31 +0900

    Hint タクティクの更新に追従する

--- LeanByExample/Tactic/Hint.lean
+++ LeanByExample/Tactic/Hint.lean
@@ -3,27 +3,40 @@
 `hint` は複数のタクティクを試し、上手くいったものを報告してくれるタクティクです。-/
 import Mathlib.Tactic -- `hint` は検索を伴うので、おおざっぱに import している
 
-/--
+/-⋆-//--
 info: Try these:
-• exact h p
+• bound
+• group
+• ring_nf
+• norm_num
 -/
-#guard_msgs in
-  example (P Q : Prop) (p : P) (h : P → Q) : Q := by
-    hint
+#guard_msgs in --#
+example (P Q : Prop) (p : P) (h : P → Q) : Q := by
+  hint
 
-/--
+/-⋆-//--
 info: Try these:
-• simp_all only [and_self]
+• bound
+• group
+• ring_nf
+• norm_num
 -/
-#guard_msgs in
-  example (P Q R : Prop) (x : P ∧ Q ∧ R ∧ R) : Q ∧ P ∧ R := by
-    hint
+#guard_msgs in --#
+example (P Q R : Prop) (x : P ∧ Q ∧ R ∧ R) : Q ∧ P ∧ R := by
+  hint
 
 /- ## 登録されているタクティク
-デフォルトでは以下の11個のタクティクを試します。
+デフォルトでは以下の18個のタクティクを試します。
 
+* `group`
+* `noncomm_ring`
 * [`gcongr`](./Gcongr.md)
+* [`ring`](./Ring.md)
 * [`linarith`](./Linarith.md)
+* `norm_num`
+* [`positivity`](./Positivity.md)
+* `bound`
+* `abel`
 * [`omega`](./Omega.md)
 * [`decide`](./Decide.md)
 * [`exact?`](./ExactQuestion.md)
@@ -46,9 +59,16 @@ def getRegisteredTactics : CoreM Unit := do
       | panic! "error: unexpected syntax term"
     IO.println arr[0]!
 
-/--
-info: "gcongr"
+/-⋆-//--
+info: "group"
+"noncomm_ring"
+"gcongr"
+"ring"
 "linarith"
+"norm_num"
+"positivity"
+"bound"
+"abel"
 "omega"
 "decide"
 "exact?"
@@ -59,7 +79,8 @@ info: "gcongr"
 "tauto"
 "trivial"
 -/
-#guard_msgs in #eval getRegisteredTactics
+#guard_msgs in --#
+#eval getRegisteredTactics
 
 /-
 ## タクティクの新規登録
@@ -69,10 +90,10 @@ info: "gcongr"
 
 register_hint nlinarith
 
-/--
+/-⋆-//--
 info: Try these:
 • nlinarith
 -/
-#guard_msgs in
-  example (a b : Nat) (h : a ≤ b) : (a + b) ^ 2 ≤ 4 * b ^ 2 := by
-    hint
+#guard_msgs in --#
+example (a b : Nat) (h : a ≤ b) : (a + b) ^ 2 ≤ 4 * b ^ 2 := by
+  hint
