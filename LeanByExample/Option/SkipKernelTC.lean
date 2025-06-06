1b84f95c4b0b1e31f8b8910f28ef301a36c3b08a
Seasawher <tellagoodstory@icloud.com>
2025-06-07 08:06:40 +0900

    `skipKernelTC` を禁止しても無駄であるという話をする

--- LeanByExample/Option/SkipKernelTC.lean
+++ LeanByExample/Option/SkipKernelTC.lean
@@ -25,3 +25,32 @@ theorem easy_proof (x y z n : Nat) : n > 2 → x ^ n + y ^ n = z ^ n → x * y *
 /-⋆-//-- info: 'easy_proof' does not depend on any axioms -/
 #guard_msgs in --#
 #print axioms easy_proof
+
+/- ## 補足: skipKernelTC を禁止すれば不正な証明を防げるか？
+
+`debug.skipKernelTC` オプションを禁止しても、以下のように、代わりに `addDeclCore` 関数を使うなどして同様のことが実現できてしまいます。 -/
+
+section
+  open Lean
+
+  def myBadTheorem : TheoremVal where
+    name := `bad_proof₂
+    levelParams := []
+    type := Lean.mkConst ``False
+    value := Lean.mkConst ``trivial
+    all := []
+
+  #eval show CoreM Unit from do
+    let currentEnv ← getEnv
+    match currentEnv.addDeclCore (doCheck := false) 0 (.thmDecl myBadTheorem) none with
+    | .ok env => setEnv env
+    | .error _ => throwError "didn't work"
+end
+
+-- Fermat の最終定理が証明できてしまう
+theorem easy_proof₂ (x y z n : Nat) : n > 2 → x ^ n + y ^ n = z ^ n → x * y * z = 0 := by
+  exact bad_proof₂.elim
+
+/-⋆-//-- info: 'easy_proof₂' does not depend on any axioms -/
+#guard_msgs in --#
+#print axioms easy_proof₂
