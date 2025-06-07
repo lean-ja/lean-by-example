43df96041c134055155da9c9c28c64da668a6eb3
Seasawher <tellagoodstory@icloud.com>
2025-06-07 14:00:31 +0900

    `sorry` 使用の痕跡を隠せるという話を追加する

--- LeanByExample/Tactic/Sorry.lean
+++ LeanByExample/Tactic/Sorry.lean
@@ -7,6 +7,33 @@ import Lean --#
 def FermatLastTheorem :=
   ∀ x y z n : Nat, n > 2 ∧ x * y * z ≠ 0 → x ^ n + y ^ n ≠ z ^ n
 
-#guard_msgs (drop warning) in --#
+/-⋆-//-- warning: declaration uses 'sorry' -/
+#guard_msgs (warning) in --#
 theorem flt : FermatLastTheorem :=
   sorry
+
+/- ## 補足: sorry 使用の痕跡は隠すことができる
+
+基本的に、`sorry` タクティクを使用すれば `sorryAx` という公理が使用されて、[`#print axioms`](#{root}/Diagnostic/Print.md#PrintAxioms) コマンドの出力に現れるようになります。
+-/
+
+/-⋆-//-- info: 'flt' depends on axioms: [sorryAx] -/
+#guard_msgs in --#
+#print axioms flt
+
+/- しかし、[`[csimp]`](#{root}/Attribute/Csimp.md) 属性を経由することで `sorryAx` を隠し、`Lean.ofReduceBool` の背後に隠してしまうことができます。 -/
+
+def one := 1
+def two := 2
+
+#guard_msgs (drop warning) in --#
+@[csimp] theorem one_eq_two : one = two := by
+  sorry
+
+theorem false_theorem : 1 = 2 := by
+  rw [show 1 = one from rfl]
+  native_decide
+
+/-⋆-//-- info: 'false_theorem' depends on axioms: [Lean.ofReduceBool] -/
+#guard_msgs in --#
+#print axioms false_theorem
