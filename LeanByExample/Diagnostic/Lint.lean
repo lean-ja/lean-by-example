a780af35e9ff1d35c3438437e00adb97ef88edbb
Seasawher <tellagoodstory@icloud.com>
2025-06-14 19:02:33 +0900

    `#lint`のページの校正

--- LeanByExample/Diagnostic/Lint.lean
+++ LeanByExample/Diagnostic/Lint.lean
@@ -8,28 +8,28 @@ import Batteries.Tactic.Lint
 -- ドキュメントコメントのない定理
 theorem hoge : True := by trivial
 
-/--
+-- ドキュメントコメントのない定理に対して警告するリンタ
+/-⋆-//--
 error: -- Found 1 error in 1 declarations (plus 0 automatically generated ones) in the current file with 1 linters
 
 /- The `docBlameThm` linter reports:
 THEOREMS ARE MISSING DOCUMENTATION STRINGS: -/
 #check hoge /- theorem missing documentation string -/
 -/
-#guard_msgs (error) in
-  -- ドキュメントコメントのない定理に対して警告するリンタ
-  #lint only docBlameThm
+#guard_msgs (error) in --#
+#lint only docBlameThm
 
 -- ドキュメントコメントのない定義
 def fuga : True := by trivial
 
-/--
+-- 定義にドキュメントコメントがあるか確認するリンタ
+-- 定理は対象外なのでスルーされる
+/-⋆-//--
 error: -- Found 1 error in 2 declarations (plus 0 automatically generated ones) in the current file with 1 linters
 
 /- The `docBlame` linter reports:
 DEFINITIONS ARE MISSING DOCUMENTATION STRINGS: -/
 #check fuga /- definition missing documentation string -/
 -/
-#guard_msgs (whitespace := lax) in
-  -- 定義にドキュメントコメントがあるか確認するリンタ
-  -- 定理は対象外なのでスルーされる
-  #lint only docBlame
+#guard_msgs (whitespace := lax) in --#
+#lint only docBlame
