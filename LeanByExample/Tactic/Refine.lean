350ce1b77d740c07421ade6c5102bd0f89f62e1e
Seasawher <tellagoodstory@icloud.com>
2025-06-15 00:04:40 +0900

    refine とメタ変数を利用して、証明の使い回しをする
    resolve #1814

--- LeanByExample/Tactic/Refine.lean
+++ LeanByExample/Tactic/Refine.lean
@@ -1,9 +1,8 @@
 /- # refine
 
-`refine` は [`exact`](./Exact.md) タクティクと同様に機能しますが、プレースホルダを受け入れて新しいゴールを生成するという違いがあります。 -/
-variable (P Q R : Prop)
+`refine` は [`exact`](./Exact.md) タクティクと同様に機能しますが、メタ変数(`?` から始まる変数で、プレースホルダとして機能する)を受け入れて新しいゴールを生成するという違いがあります。 -/
 
-example (hP : P) (hQ : Q) : P ∧ Q := by
+example {P Q : Prop} (hP : P) (hQ : Q) : P ∧ Q := by
   -- 穴埋め形式で証明を作ることができる
   refine ⟨?_, hQ⟩
 
@@ -14,13 +13,13 @@ example (hP : P) (hQ : Q) : P ∧ Q := by
 
 /- ## 用途
 
-`refine` はかなり一般的なタクティクなので、様々な場面で使うことができます。
+`refine` はかなり一般的なタクティクであり、様々な場面で使うことができます。
 
 ### constructor の一般化として
 
 `refine` は [`constructor`](./Constructor.md) の代わりに使うこともできます。実際 `refine` は `constructor` よりも柔軟で、`⊢ P ∧ Q ∧ R` のような形のゴールは `constructor` よりも簡潔に分割できます。-/
 
-example (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
+example {P Q R : Prop} (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
   -- ゴールを３つに分割する
   refine ⟨?_, ?_, ?_⟩
 
@@ -30,7 +29,7 @@ example (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
 
 /- `constructor` を使った場合、一度に２つのゴールに分割することしかできません。 -/
 
-example (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
+example {P Q R : Prop} (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
   constructor
   · exact hP
   · constructor
@@ -41,9 +40,7 @@ example (hP : P) (hQ : Q) (hR : R) : P ∧ Q ∧ R := by
 
 `h : P → Q` という命題があって、ゴールが `⊢ Q` であるとき `refine h ?_` は `apply h` と同様に機能するので、`refine` で [`apply`](./Apply.md) を代用することができます。 -/
 
-variable (P Q : Prop)
-
-example (hPQ : P → Q) (hP : P) : Q := by
+example {P Q : Prop} (hPQ : P → Q) (hP : P) : Q := by
   refine hPQ ?_
 
   -- ゴールが `⊢ P` になる
@@ -56,7 +53,7 @@ example (hPQ : P → Q) (hP : P) : Q := by
 ゴールが `⊢ P ∧ Q` であり、`P` が成り立つことが自明であった場合、いちいち `⊢ P` と `⊢ Q` の２つのサブゴールを作って示すのは面倒に思えます。こうしたとき、`refine` を使うとサブゴールを生成せずにゴールを単に `⊢ Q` に変えることができます。
 -/
 
-example (hP : P) (hQ : ¬ P ∨ Q) : P ∧ Q := by
+example {P Q : Prop} (hP : P) (hQ : ¬ P ∨ Q) : P ∧ Q := by
   -- `P` が成り立つのは自明なので、`Q` だけ示せばよい
   refine ⟨by assumption, ?_⟩
 
@@ -64,3 +61,13 @@ example (hP : P) (hQ : ¬ P ∨ Q) : P ∧ Q := by
   guard_target =ₛ Q
 
   simp_all
+
+/- ### 場合分けの他の枝で示したことを再利用する
+
+メタ変数を使って、場合分けの他の枝で示したことを再利用することができます。
+-/
+
+example {P Q : Prop} (h1 : P → Q) (h2 : P → Q → False) (hP : P) : False := by
+  refine h2 ?a ?_ -- `?a` は `P` の証明を要求するメタ変数
+  · exact hP
+  · exact h1 ?a -- `?a` を `P` の証明の代替として使うことができる
