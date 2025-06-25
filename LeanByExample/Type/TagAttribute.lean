b67c1e955e0918a19de549447a75bc3ece7c7ad0
Seasawher <tellagoodstory@icloud.com>
2025-06-25 17:39:00 +0900

    `TagAttribute`のページにタクティク自作例を載せる

--- LeanByExample/Type/TagAttribute.lean
+++ LeanByExample/Type/TagAttribute.lean
@@ -27,17 +27,48 @@ def foo := "foo"
 attribute [my_tag] foo
 
 /- タグに対する基本的な操作である、「タグが付与されている宣言をすべて集める」という操作をする例も挙げておきましょう。 -/
+section
 
-open Lean
+  open Lean
 
-/-- `[tagAttr]`属性が付与されているものをリストアップする -/
-def listAllTagged (tagAttr : TagAttribute) : MetaM (List Name) := do
-  let env ← getEnv
-  let taggedDecls := tagAttr.ext.getState env
-  return taggedDecls.toList
+  /-- `[tagAttr]`属性が付与されているものをリストアップする -/
+  def listAllTagged (tagAttr : TagAttribute) : MetaM (List Name) := do
+    let env ← getEnv
+    let taggedDecls := tagAttr.ext.getState env
+    return taggedDecls.toList
 
-/-⋆-//-- info: `[my_tag]`が付与されているもの: [hello, foo] -/
-#guard_msgs in --#
-run_meta
-  let tagged ← listAllTagged myTagAttribute
-  logInfo m!"`[my_tag]`が付与されているもの: {tagged}"
+  /-⋆-//-- info: `[my_tag]`が付与されているもの: [hello, foo] -/
+  #guard_msgs in --#
+  run_meta
+    let tagged ← listAllTagged myTagAttribute
+    logInfo m!"`[my_tag]`が付与されているもの: {tagged}"
+
+end
+
+/- さらに応用として、「`[my_tag]` 属性が付与された定理を順に `apply` してゴールを閉じるタクティク」を自作する例も挙げておきます。 -/
+
+@[my_tag]
+theorem testThm (P Q : Prop) : P → Q → P ∧ Q := by
+  simp_all
+
+open Lean Elab Tactic in
+
+/-- `@[my_tag]`タグが付与された定理をひたすら順に`apply`するタクティク -/
+elab "apply_my_tagged" : tactic => do
+  let taggedDecls ← listAllTagged myTagAttribute
+  if taggedDecls.isEmpty then
+    throwError "`[my_tag]`が付与された定理はありません。"
+  for decl in taggedDecls do
+    let declStx : TSyntax `term := mkIdent decl
+    try
+      evalTactic <| ← `(tactic| apply $declStx)
+
+      -- 成功したら終了する
+      return ()
+    catch _ =>
+      -- 失敗したら単に次の候補に進む
+      pure ()
+  throwError "ゴールを閉じることができませんでした。"
+
+example (P Q : Prop) : P → Q → P ∧ Q := by
+  apply_my_tagged
