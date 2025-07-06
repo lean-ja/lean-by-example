7e9b8bf0013f8c4909b1b1c0a1bc4d236e7ae772
Seasawher <tellagoodstory@icloud.com>
2025-07-07 00:49:54 +0900

    `grind` アノテーションの吟味

--- Playground/Grind/MyNat/C04AddCancel.lean
+++ Playground/Grind/MyNat/C04AddCancel.lean
@@ -3,17 +3,17 @@ import Playground.Grind.MyNat.C03CommSemiring
 -- 以降、l m nはすべてMyNat型の項とする
 variable {l m n : MyNat}
 
-@[grind =>]
+@[grind →]
 theorem MyNat.add_one_right_cancel (h : l + 1 = n + 1) : l = n := by
   injection h
 
 /-- 右から足す演算`(· + m)`は単射 -/
-@[grind =>, simp]
+@[grind →, simp]
 theorem MyNat.add_right_cancel (h : l + m = n + m) : l = n := by
   induction m with grind
 
 /-- 左から足す演算`(l + ·)`は単射 -/
-@[grind =>, simp]
+@[grind →, simp]
 theorem MyNat.add_left_cancel (h : l + m = l + n) : m = n := by
   grind
 
@@ -47,18 +47,17 @@ theorem MyNat.self_eq_add_left (m n : MyNat) : m = n + m ↔ n = 0 := by
   grind
 
 /-- `n + 1`はゼロになることはない -/
-@[grind =>]
-theorem MyNat.add_one_eq_zero (n : MyNat) : n + 1 ≠ 0 := by
-  intro h
+@[grind →]
+theorem MyNat.add_one_eq_zero {n : MyNat} (h : n + 1 = 0) : False := by
   injection h
 
 /-- 和がゼロなら両方がゼロ -/
-@[grind =>]
-theorem MyNat.eq_zero_of_add_eq_zero : m + n = 0 → m = 0 ∧ n = 0 := by
+@[grind →]
+theorem MyNat.eq_zero_of_add_eq_zero (h : m + n = 0) : m = 0 ∧ n = 0 := by
   induction n with grind
 
-@[grind =>]
-theorem MyNat.add_eq_zero_of_eq_zero : m = 0 ∧ n = 0 → m + n = 0 := by
+@[grind =>] -- **TODO** なぜ `grind →` は不可なのか？
+theorem MyNat.add_eq_zero_of_eq_zero (h : m = 0 ∧ n = 0) : m + n = 0 := by
   grind
 
 /-- 和がゼロであることと両方がゼロであることは同値 -/
