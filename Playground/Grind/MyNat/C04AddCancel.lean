ce852dd2523625fa4c4c4b112480b6dd89979998
Seasawher <tellagoodstory@icloud.com>
2025-07-07 00:35:52 +0900

    Playground: 証明の簡略化

--- Playground/Grind/MyNat/C04AddCancel.lean
+++ Playground/Grind/MyNat/C04AddCancel.lean
@@ -8,54 +8,54 @@ theorem MyNat.add_one_right_cancel (h : l + 1 = n + 1) : l = n := by
   injection h
 
 /-- 右から足す演算`(· + m)`は単射 -/
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.add_right_cancel (h : l + m = n + m) : l = n := by
   induction m with grind
 
 /-- 左から足す演算`(l + ·)`は単射 -/
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.add_left_cancel (h : l + m = l + n) : m = n := by
   grind
 
 /-- 右からの足し算のキャンセル -/
-@[grind =]
+@[grind =, simp]
 theorem MyNat.add_right_cancel_iff : l + m = n + m ↔ l = n := by
   grind
 
 /-- 左からの足し算のキャンセル -/
-@[grind =]
+@[grind =, simp]
 theorem MyNat.add_left_cancel_iff : l + m = l + n ↔ m = n := by
   grind
 
 instance : Lean.Grind.AddRightCancel MyNat where
   add_right_cancel := by grind
 
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.add_right_eq_self (m n : MyNat) : m + n = m ↔ n = 0 := by
   grind
 
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.add_left_eq_self (m n : MyNat) : n + m = m ↔ n = 0 := by
   grind
 
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.self_eq_add_right (m n : MyNat) : m = m + n ↔ n = 0 := by
   grind
 
-@[grind =>]
+@[grind =>, simp]
 theorem MyNat.self_eq_add_left (m n : MyNat) : m = n + m ↔ n = 0 := by
   grind
+
+/-- `n + 1`はゼロになることはない -/
+@[grind =>]
+theorem MyNat.add_one_eq_zero (n : MyNat) : n + 1 ≠ 0 := by
+  intro h
+  injection h
 
 /-- 和がゼロなら両方がゼロ -/
 @[grind =>]
 theorem MyNat.eq_zero_of_add_eq_zero : m + n = 0 → m = 0 ∧ n = 0 := by
-  intro h
-  induction n with
-  | zero => grind
-  | succ n ih =>
-    exfalso
-    rw [show m + (n + 1) = m + n + 1 from by ac_rfl] at h
-    injection h
+  induction n with grind
 
 @[grind =>]
 theorem MyNat.add_eq_zero_of_eq_zero : m = 0 ∧ n = 0 → m + n = 0 := by
