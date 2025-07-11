b7043be7eb8f2edbe09a4d60e2c9a8109f9a7559
Seasawher <tellagoodstory@icloud.com>
2025-07-12 00:06:00 +0900

    Playground: `MyNat`が`Preorder`であることを示す

---
+++ Playground/MyNat/C05Preorder.lean
@@ -0,0 +1,78 @@
+import Playground.MyNat.C04AddCancel
+
+/- # MyNat が PreOrder であることを示す -/
+
+/-- 自然数上の広義の順序関係 -/
+protected inductive MyNat.le (n : MyNat) : MyNat → Prop
+  /-- ∀ n, n ≤ n -/
+  | refl : MyNat.le n n
+  /-- `n ≤ m`ならば`n ≤ m + 1` -/
+  | step {m : MyNat} : MyNat.le n m → MyNat.le n (m + 1)
+
+/-- `MyNat.le`を`≤`で表せるようにする -/
+instance : LE MyNat where
+  le := MyNat.le
+
+@[induction_eliminator]
+def MyNat.le.recAux {n b : MyNat}
+    {motive : (a : MyNat) → n ≤ a → Prop}
+    (refl : motive n MyNat.le.refl)
+    (step : ∀ {m : MyNat} (a : n ≤ m), motive m a → motive (m + 1) (MyNat.le.step a))
+    (t : n ≤ b) :
+  motive b t := by
+  induction t with
+  | refl => exact refl
+  | @step c h ih =>
+    exact step (a := h) ih
+
+/-- 反射律 -/
+@[refl, simp, grind <=]
+theorem MyNat.le_refl (n : MyNat) : n ≤ n := by
+  exact MyNat.le.refl
+
+-- m, n, k はMyNatの項とする
+variable {m n k : MyNat}
+
+@[grind →]
+theorem MyNat.le_step (h : n ≤ m) : n ≤ m + 1 := by
+  apply MyNat.le.step
+  exact h
+
+/-- 推移律 -/
+@[grind →]
+theorem MyNat.le_trans (hnm : n ≤ m) (hmk : m ≤ k) : n ≤ k := by
+  induction hmk with grind
+
+/-- `MyNat.le`を「推移的な二項関係」として登録する -/
+instance : Trans (· ≤ · : MyNat → MyNat → Prop) (· ≤ ·) (· ≤ ·) where
+  trans := MyNat.le_trans
+
+open Lean Grind
+
+instance : Preorder MyNat where
+  le := (· ≤ ·)
+  le_refl := MyNat.le_refl
+  le_trans := MyNat.le_trans
+
+/-- `a ≤ b`から和の等式を導く -/
+@[grind <=]
+theorem MyNat.le.dest (h : n ≤ m) : ∃ k, n + k = m := by
+  induction h with
+  | refl => exists 0
+  | @step l h ih =>
+    obtain ⟨k, ih⟩ := ih
+    exists k + 1
+    grind
+
+@[grind ←]
+theorem MyNat.le_add_right (n m : MyNat) : n ≤ n + m := by
+  induction m with grind
+
+/-- 和の等式から`a ≤ b`を導く -/
+@[grind =>]
+theorem MyNat.le.intro (h : n + k = m) : n ≤ m := by
+  induction k with grind
+
+/-- 順序関係`n ≤ m`を足し算で書き換える -/
+theorem MyNat.le_iff_add : n ≤ m ↔ ∃ k, n + k = m := by
+  constructor <;> grind
