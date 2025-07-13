868074fb9e86d015ffee6c80d5b579db3f4fbafa
Seasawher <tellagoodstory@icloud.com>
2025-07-13 14:59:51 +0900

    シングル山括弧 `‹›` 記法を紹介する

--- LeanByExample/Tactic/Assumption.lean
+++ LeanByExample/Tactic/Assumption.lean
@@ -11,3 +11,10 @@ example (hP: P) (_: Q) : P := by
 
 example (hP: P) (_: Q) : P := by
   exact hP
+
+/- ## 補足
+
+なお、シングル山括弧 `‹›` を使って `‹P›` と書くと、「命題 `P` の証明を `by assumption` で埋めてください」と Lean に指示したことになります。-/
+
+example (P Q R : Prop) (hp : P) (hq : Q) (h : P → Q → R) : R := by
+  exact h ‹P› ‹Q›
