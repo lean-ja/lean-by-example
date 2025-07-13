e7831b7241eb5b8b80cf25759d128b9837c8e354
Seasawher <tellagoodstory@icloud.com>
2025-07-13 15:07:43 +0900

    Playground: コード例の修正

--- Playground/Grind/AddCommMonoid.lean
+++ Playground/Grind/AddCommMonoid.lean
@@ -1,11 +1,15 @@
 
 open Lean Grind
 
-variable {M : Type} [AddCommMonoid M] [Zero M]
+variable {M : Type} [AddCommMonoid M]
 
-set_option warn.sorry false in --#
+example (l m n : M) : l + m + n = (l + m) + n := by
+  -- これは示すことができる
+  grind
 
-example (l m n : M) : l + m + n + 0 = m + (l + n) := by
+set_option warn.sorry false in
+
+example (l m n : M) : l + m = m + l := by
   -- `grind`は本来このゴールを閉じることができるべきだと思うが、現状はできない。
   fail_if_success grind
   sorry
