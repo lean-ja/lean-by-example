857bf8e6384e454f333a805b13983e11826b8b62
Seasawher <tellagoodstory@icloud.com>
2025-07-15 14:33:28 +0900

    `autoImplicit` を非推奨にしない
    resolve #1897

--- LeanByExample/Option/RelaxedAutoImplicit.lean
+++ LeanByExample/Option/RelaxedAutoImplicit.lean
@@ -27,29 +27,3 @@ section
     | [] => false
     | _ :: _ => true
 end
-
-/- ## 問題点
-
-```admonish error title="非推奨"
-この機能には以下の問題点が指摘されており使用は推奨されません。
-* タイポを見過ごしやすくなってしまう
-* 自動束縛の結果どのような型になるか指定できないため、意図しない型に束縛されてバグを引き起こす可能性がある
-```
-
-デフォルトではこのオプションは有効になっているため、`lakefile` の設定で無効にすることをお勧めします。
--/
-
-open Lean Elab Command in
-
-/-- オプションのデフォルト値を取得する関数 -/
-def getDefaultValue (opt : Name) : CommandElabM Unit := do
-  let decls ← getOptionDecls
-  match decls.find? opt with
-  | some decl =>
-    logInfo m!"default value of {opt} is {decl.defValue}"
-  | none =>
-    logInfo m!"{opt} is unknown"
-
-/-⋆-//-- info: default value of relaxedAutoImplicit is true -/
-#guard_msgs in --#
-#eval getDefaultValue `relaxedAutoImplicit
