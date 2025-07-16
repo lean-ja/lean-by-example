7db97eb9df4927a8488e808e8fa5a3eb3825d64f
Seasawher <tellagoodstory@icloud.com>
2025-07-16 19:47:36 +0900

    `∀`を紹介する

---
+++ LeanByExample/Parser/Forall.lean
@@ -0,0 +1,24 @@
+/- # ∀
+
+`∀` 記号は基本的に、全称量化を表します。つまり述語 `P : α → Prop` に対して、`∀ x : α, P x` は「すべての `x : α` に対して `P x` が成り立つ」という意味になります。
+
+しかし、`∀` は述語以外のものに対しても実は使うことができます。
+-/
+
+#check ∀ n : Nat, Vector Nat n
+
+/- この場合何を意味するかというと、`∀ a : A, B` は `(a : A) → B` と同じ意味になります。-/
+
+set_option pp.foralls false
+
+/-⋆-//-- info: (n : Nat) → Vector Nat n : Type -/
+#guard_msgs in --#
+#check ∀ n : Nat, Vector Nat n
+
+/- 実際のところ両者は常に同じです。単に型が `Prop` になるときだけ、見やすいので `∀` 記号を使う慣習になっているというだけです。 -/
+
+universe u
+
+variable {A : Type} {B : A → Sort u}
+
+example : ((a : A) → B a) = (∀ a : A, B a) := by rfl
