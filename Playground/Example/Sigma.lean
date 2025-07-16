2d84f2c8de413d42a5a737a65c077eedf16ba7d9
Seasawher <tellagoodstory@icloud.com>
2025-07-17 07:43:58 +0900

    不要になったファイルを削除

--- Playground/Example/Sigma.lean
+++
@@ -1,3 +0,0 @@
-#check Sigma
-
-#check ([⟨Nat, 1⟩, ⟨String, "hello"⟩, ⟨Bool, true⟩] : List ((α : Type) × α))
--- Playground/Example/TypeList.lean
+++
@@ -1,6 +0,0 @@
-/-
-`[(Nat, 1), (String, "hello"), (Bool, true)]` のようなリストに対する適切な型はなんだろう？
--/
-
-def sampleList : List ((α : Type) × α) :=
-  [⟨Nat, 1⟩, ⟨String, "hello"⟩, ⟨Bool, true⟩]
