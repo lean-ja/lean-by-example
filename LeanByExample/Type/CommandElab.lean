1c797f00c6fd92ddd7e9e292cfb8cec79a9667fe
Seasawher <tellagoodstory@icloud.com>
2025-07-16 19:28:12 +0900

    `CommandElab`の例を追記する

--- LeanByExample/Type/CommandElab.lean
+++ LeanByExample/Type/CommandElab.lean
@@ -6,9 +6,9 @@
 -/
 import Lean
 
-open Lean Elab Command in
+open Lean in
 
-example : CommandElab = (Syntax → CommandElabM Unit) := rfl
+example : Lean.Elab.Command.CommandElab = (Syntax → Lean.Elab.Command.CommandElabM Unit) := rfl
 
 /- ## CommandElab 型の関数からコマンドへ
 
@@ -28,3 +28,28 @@ def evalHello : CommandElab := fun _stx => do
 /-⋆-//-- info: Hello, Lean! -/
 #guard_msgs in --#
 #hello
+
+/- ### ドキュメントコメントを取得するコマンド
+
+識別子のドキュメントコメントを取得して表示するコマンドを定義する例です。
+-/
+
+syntax (name := docCmdStx) "#doc " ident : command
+
+open Lean Elab Command in
+
+/-- ドキュメントコメントを取得して表示するコマンド -/
+@[command_elab docCmdStx]
+def evalDocCmd : CommandElab := fun stx =>
+  match stx with
+  | `(command| #doc $x:ident) => do
+    let name ← liftCoreM do realizeGlobalConstNoOverload x
+    if let some s ← findDocString? (← getEnv) name then
+      logInfo m!"{s}"
+  | _ => throwUnsupportedSyntax
+
+/-⋆-//--
+info: Characters are Unicode [scalar values](http://www.unicode.org/glossary/#unicode_scalar_value).
+-/
+#guard_msgs in --#
+#doc Char
