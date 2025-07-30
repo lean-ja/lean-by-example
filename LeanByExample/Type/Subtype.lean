2a379444dca294dac489876b181088df898b8a89
Seasawher <tellagoodstory@icloud.com>
2025-07-30 21:42:43 +0900

    `Subtype`の実行時の性質を追記する

--- LeanByExample/Type/Subtype.lean
+++ LeanByExample/Type/Subtype.lean
@@ -12,6 +12,19 @@ import Lean --#
 -- 正の数を表す subtype
 #check { n : Nat // n > 0 }
 
+/- ## 実行時の性質
+
+型 `A` の `Subtype` は実行時には `A` の項であるかのように振る舞います。言い換えれば、`{ x : A // p x}` の項は計算の実行時には `A` の項と同一視されます。これは、`Subtype` でラップしてもメモリアドレスが変わらないことから確認できます。
+-/
+
+unsafe def checkSubtype : IO Unit := do
+  let x : Nat := 42
+  let pos : { n : Nat // n > 0} := ⟨x, by omega⟩
+  if ptrAddrUnsafe x != ptrAddrUnsafe pos then
+    throw <| .userError "メモリアドレスが異なります。"
+
+#eval checkSubtype
+
 /- ## 用途
 
 `Subtype` を使うと、ある型 `A` が `U` という型の一部を切り取ったものだということをコードで表現することができます。たとえば、自然数の型 `Nat` に対して、正の数だけを抜き出して正の整数の型 `Pos` を定義することを考えてみます。
