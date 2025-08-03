adb292e6f765e7f07f5ceedac87f764978d739e4
Seasawher <tellagoodstory@icloud.com>
2025-08-03 11:25:13 +0900

    リンク切れの修正

--- LeanByExample/Attribute/README.lean
+++ LeanByExample/Attribute/README.lean
@@ -2,5 +2,5 @@
 
 属性は、[`attribute`](#{root}/Declarative/Attribute.md) コマンドや `@[...]` という構文で扱うことができるものです。
 
-本書では属性の完全なリストは示しません。Mathlib において利用可能な属性の一覧は [attribute list](https://seasawher.github.io/mathlib4-help/attributes/) で確認できます。
+本書では属性の完全なリストは示しません。Mathlib において利用可能な属性の一覧は [attribute list](https://seasawher.github.io/mathlib4-help/attributes.html) で確認できます。
 -/
