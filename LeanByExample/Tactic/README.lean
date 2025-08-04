61456dea080486ce92e9da50f680e9c750a51c92
Seasawher <tellagoodstory@icloud.com>
2025-08-04 22:00:58 +0900

    リンクの修正

--- LeanByExample/Tactic/README.lean
+++ LeanByExample/Tactic/README.lean
@@ -1,5 +1,5 @@
 /- # タクティク
 タクティクは、Lean において証明を対話的に行ったり自動化したりすることを可能にするものです。
 
-なおこのリストは全部のタクティクを網羅していません。Mathlib の全タクティクのリストが必要であれば [mathlib4 tactics](https://seasawher.github.io/mathlib4-help/tactics.html) を参照してください。
+なおこのリストは全部のタクティクを網羅していません。Mathlib の全タクティクのリストが必要であれば [Mathlib4 Help](https://seasawher.github.io/mathlib4-help/) を参照してください。
 -/
