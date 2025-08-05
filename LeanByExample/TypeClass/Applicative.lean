e538d0d80bbff7354e114da31e1ae1822fd6ff90
Seasawher <tellagoodstory@icloud.com>
2025-08-05 20:41:33 +0900

    `Applicative` 関手を紹介する

--- LeanByExample/TypeClass/Applicative.lean
+++ LeanByExample/TypeClass/Applicative.lean
@@ -8,3 +8,31 @@
 
 {{#include ./Applicative/Def.md}}
 -/
+
+/- ## Functor との違い
+
+`Functor.map` メソッドは `(α → β) → F α → F β` という型を持ちます。これは、`F = Id` の場合を考えてみると分かるように、１引数の関数適用を一般化したものだと考えることができます。では２引数、３引数の時はどうなるでしょうか？
+
+単純に拡張すると、２引数の時は `(α → β → γ) → F α → F β → F γ` という型になり、３引数の時は `(α → β → γ → δ) → F α → F β → F γ → F δ` という型になります。これらを `Functor.map` を使って表現するのは困難です。
+
+しかし、`F` が `Applicative` 型クラスのインスタンスになっていれば、n 引数の場合でも表現することができます。[^PIH]
+-/
+
+variable {α β γ δ : Type}
+variable {F : Type → Type} [Applicative F]
+
+/-- 1引数の場合 -/
+example : (α → β) → F α → F β := fun f x =>
+  pure f <*> x
+
+/-- 2引数の場合 -/
+example : (α → β → γ) → F α → F β → F γ := fun f x y =>
+  pure f <*> x <*> y
+
+/-- 3引数の場合 -/
+example : (α → β → γ → δ) → F α → F β → F γ → F δ := fun f x y z =>
+  pure f <*> x <*> y <*> z
+
+/-
+[^PIH]: ここでの説明は 「プログラミングHaskell 第2版」(Graham Hutton 著、山本和彦訳)を参考にしました。
+-/
