40b87e12ac7c2711f1c9776ac4479e310377a662
Seasawher <tellagoodstory@icloud.com>
2025-08-28 20:03:13 +0900

    Mathlib の更新に伴う `field_simp` のページの修正

--- LeanByExample/Tactic/FieldSimp.lean
+++ LeanByExample/Tactic/FieldSimp.lean
@@ -15,7 +15,8 @@ example (n m : Rat) : n * m = ((n + m) ^ 2 - n ^ 2 - m ^ 2 ) / 2 := by
 
 example {x y z : Rat} (hy : y = z ^ 2 + 1) : (x + 2 * y) / y = x / y + 2 := by
   -- 最初 `field_simp` は動作しない
-  fail_if_success field_simp
+  fail_if_success solve
+  | field_simp
 
   -- 分母がゼロでないことを示す
   have ypos : y ≠ 0 := by
@@ -34,10 +35,7 @@ example (n m : Nat) : n * m = ((n + m) ^ 2 - n ^ 2 - m ^ 2 ) / 2 := by
 
   -- 分母を払う
   suffices goal : 2 * (n * m) = (n + m) ^ 2 - n ^ 2 - m ^ 2 from by
-    rw [← goal]
-
-    -- この部分は(自然数の話だが) `field_simp` で示せる
-    field_simp
+    simp [← goal]
 
   -- `(n + m) ^ 2` を展開する
   rw [show (n + m) ^ 2 = n ^ 2 + 2 * n * m + m ^ 2 from by ring]
@@ -51,10 +49,12 @@ variable (x y : Rat)
 
 example : x * y ≤ (x ^ 2 + y ^ 2) / 2 := by
   -- 何も起こらない
-  fail_if_success field_simp
+  fail_if_success solve
+  | field_simp
 
   suffices x ^ 2 - 2 * x * y + y ^ 2 ≥ 0 from by
     linarith
+
   calc
     x ^ 2 - 2 * x * y + y ^ 2 = (x - y) ^ 2 := by ring
     _ ≥ 0 := by apply sq_nonneg
