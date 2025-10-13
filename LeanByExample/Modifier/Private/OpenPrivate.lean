82f2014c111b8ac4feebf00116f8adb38714a0c3
Seasawher <tellagoodstory@icloud.com>
2025-10-13 21:59:57 +0900

    校正

--- LeanByExample/Modifier/Private/OpenPrivate.lean
+++ LeanByExample/Modifier/Private/OpenPrivate.lean
@@ -1,13 +1,11 @@
 import LeanByExample.Modifier.Private.Lib
-import Batteries.Tactic.OpenPrivate 
+import Batteries.Tactic.OpenPrivate
 
-section
-  -- 最初はアクセスできない
-  #check_failure Point.private_sub
+-- 最初はアクセスできない
+#check_failure Point.private_sub
 
-  -- `open private` コマンドを使用する
-  open private Point.private_sub from LeanByExample.Modifier.Private.Lib
+-- `open private` コマンドを使用する
+open private Point.private_sub from LeanByExample.Modifier.Private.Lib
 
-  -- アクセスできるようになった
-  #check Point.private_sub
-end
+-- アクセスできるようになった
+#check Point.private_sub
