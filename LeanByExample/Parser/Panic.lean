9469ca2ba394b6a4a639f0e74e029aad0085cb0c
Seasawher <tellagoodstory@icloud.com>
2025-11-02 12:04:15 +0900

    `panic!` のページの校正

--- LeanByExample/Parser/Panic.lean
+++ LeanByExample/Parser/Panic.lean
@@ -7,16 +7,25 @@
 def safeDiv (x y : Nat) : Nat :=
   if y = 0 then panic! "0 で割ることはできません！" else x / y
 
+/-⋆-//--
+info: PANIC at safeDiv LeanByExample.Parser.Panic:8:16: 0 で割ることはできません！
+---
+info: 0
+-/
+#guard_msgs in --#
 #eval safeDiv 10 0
 
-/- 注意点として、`panic!` を使ったところで**処理は中断しませんし、例外にもなりません**。その部分で期待されている型の [`Inhabited`](#{root}/TypeClass/Inhabited.md) インスタンスが使用されて、値を返してしまいます。上記の例でいうと、`Nat` の `Inhabited` インスタンスが使用されて `0` が返ります。
+/- ## panic! で処理は中断しない
+
+注意点として、`panic!` を使っても処理は中断しません。その部分で期待されている型の [`Inhabited`](#{root}/TypeClass/Inhabited.md) インスタンスが使用されて、値を返します。上記の例でいうと、`Nat` の `Inhabited` インスタンスが使用されて `0` が返ります。
 -/
 
--- 例外になっておらず、処理は中断されていない
+-- エラーメッセージが出るだけで処理は中断されていない
 #guard safeDiv 10 0 = 0
 
-/- 処理を本当に中断したい場合、たとえば [`IO`](#{root}/Type/IO.md) モナドで返り値を包んだうえで、例外を `throw` すればよいです。(Lean は純粋関数型言語なので、返り値の型が例外を許していなければ、関数が値を返さずに途中で中断することは許されません) -/
+/- 処理を本当に中断したい場合、たとえば [`IO`](#{root}/Type/IO.md) モナドで返り値を包んだうえで、例外を `throw` すればよいでしょう。-/
 
+/-- ゼロ除算に対して例外を投げるバージョンの割り算 -/
 def divIO (x y : Nat) : IO Nat :=
   if y = 0 then
     throw (IO.userError "0 で割ることはできません！")
@@ -26,3 +35,20 @@ def divIO (x y : Nat) : IO Nat :=
 /-⋆-//-- error: 0 で割ることはできません！ -/
 #guard_msgs in --#
 #eval divIO 10 0
+
+/- ## panic! は例外を投げない
+
+`panic!` は例外を投げているわけではなく、`try .. catch` ブロックで補足することはできません。例外ハンドリングが必要な場合は `panic!` を使用しない方が良いでしょう。
+-/
+
+/-⋆-//--
+info: PANIC at safeDiv LeanByExample.Parser.Panic:8:16: 0 で割ることはできません！
+Result: 0
+-/
+#guard_msgs in --#
+#eval show IO Unit from do
+  try
+    let result := safeDiv 10 0
+    IO.println s!"Result: {result}"
+  catch e =>
+    IO.println s!"Caught an error: {e}"
