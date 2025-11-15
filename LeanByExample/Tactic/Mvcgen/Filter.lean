7456e07327da7386135cda2d8b0712338eeb2e88
Seasawher <tellagoodstory@icloud.com>
2025-11-15 09:17:32 +0900

    コード例の校正

--- LeanByExample/Tactic/Mvcgen/Filter.lean
+++ LeanByExample/Tactic/Mvcgen/Filter.lean
@@ -2,28 +2,24 @@ import Lean
 
 variable {α : Type}
 
-/-- `continue` 文を使って実装した `Array.filter` -/
-def Array.filterDo (p : α → Bool) (l : Array α) : Array α := Id.run do
-  let mut res : Array α := #[]
+/-- `continue` 文を使って実装した `List.filter` -/
+def List.filterDo (p : α → Bool) (l : List α) : List α := Id.run do
+  let mut res : List α := []
   for x in l do
     if ! p x then
       continue
-    res := res.push x
+    res := res ++ [x]
   return res
-
--- 証明のための補題を用意する
-attribute [grind =] Array.toList_inj
-attribute [grind _=_] Array.toList_filter
 
 open Std.Do
 
 set_option mvcgen.warning false
 
-theorem Array.filterDo_spec (p : α → Bool) (l : Array α) :
+theorem List.filterDo_spec (p : α → Bool) (l : List α) :
     l.filterDo p = l.filter p := by
   generalize h : l.filterDo p = r
   apply Id.of_wp_run_eq h
 
   mvcgen invariants
-  · ⇓⟨cursor, res⟩ => ⌜res.toList = cursor.prefix.filter p⌝
+  · ⇓⟨cursor, res⟩ => ⌜res = cursor.prefix.filter p⌝
   with grind
