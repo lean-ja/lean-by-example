462330eba21c8e42569e5c4549d0142aa72f679a
Seasawher <tellagoodstory@icloud.com>
2025-11-23 19:12:34 +0900

    変数名の修正

--- LeanByExample/Tactic/Mvcgen/NoDup.lean
+++ LeanByExample/Tactic/Mvcgen/NoDup.lean
@@ -23,9 +23,9 @@ theorem nodup_correct (l : List α) : nodupDo l ↔ l.Nodup := by
   mvcgen invariants
   · Invariant.withEarlyReturn
       -- forループの１ステップが平常終了したとき。
-      -- プログラム中の可変変数`seen`は、現在までに見た要素の集合`xs.prefix`に等しく、
-      -- かつ`xs.prefix`は重複がない。
-      (onContinue := fun xs seen => ⌜(∀ x, x ∈ seen ↔ x ∈ xs.prefix) ∧ xs.prefix.Nodup⌝)
+      -- プログラム中の可変変数`seen`は、現在までに見た要素の集合`cursor.prefix`に等しく、
+      -- かつ`cursor.prefix`は重複がない。
+      (onContinue := fun cursor seen => ⌜(∀ x, x ∈ seen ↔ x ∈ cursor.prefix) ∧ cursor.prefix.Nodup⌝)
       -- 早期リターンしたとき。
       -- 返り値は`false`であり、かつ`l`に重複がある
       (onReturn := fun ret seen => ⌜ret = false ∧ ¬l.Nodup⌝)
