9c7bf7166d8f0c3b1612cd6a2b7744b1e7bc0b29
Seasawher <tellagoodstory@icloud.com>
2025-11-24 13:34:16 +0900

    コードの校正

--- LeanByExample/Tactic/Mvcgen/PairsSumToZero.lean
+++ LeanByExample/Tactic/Mvcgen/PairsSumToZero.lean
@@ -5,27 +5,24 @@ open Std Do
 
 variable {α : Type}
 
+/-- リストに、与えられた条件を満たすペアが存在する -/
 def List.Any₂ (P : α → α → Prop) (l : List α) : Prop := ¬ l.Pairwise (fun x y => ¬P x y)
 
-theorem List.any₂_iff_not_pairwise {P : α → α → Prop} {l : List α} :
-    l.Any₂ P ↔ ¬l.Pairwise (fun x y => ¬P x y) := by
-  grind [List.Any₂]
-
 @[simp, grind ·]
-theorem not_any₂_nil {P : α → α → Prop} : ¬List.Any₂ P [] := by
-  simp [List.any₂_iff_not_pairwise]
+theorem List.not_any₂_nil {P : α → α → Prop} : ¬List.Any₂ P [] := by
+  simp [List.Any₂]
 
 @[simp, grind =]
 theorem List.any₂_cons {P : α → α → Prop} {x : α} {xs : List α} :
     List.Any₂ P (x :: xs) ↔ (∃ y ∈ xs, P x y) ∨ List.Any₂ P xs := by
-  grind [List.any₂_iff_not_pairwise, pairwise_cons]
+  grind [List.Any₂, pairwise_cons]
 
 @[simp, grind =]
 theorem List.any₂_append {P : α → α → Prop} {xs ys : List α} :
     List.Any₂ P (xs ++ ys) ↔ List.Any₂ P xs ∨ List.Any₂ P ys ∨ (∃ x ∈ xs, ∃ y ∈ ys, P x y) := by
-  grind [List.any₂_iff_not_pairwise]
+  grind [List.Any₂]
 
-
+/-- 与えられた整数のリストの中に、足してゼロになるペアがあるか判定する -/
 def pairsSumToZero (l : List Int) : Bool := Id.run do
   let mut seen : HashSet Int := ∅
   for x in l do
@@ -43,7 +40,7 @@ theorem pairsSumToZero_spec (l : List Int) :
 
   mvcgen invariants
   · Invariant.withEarlyReturn
+    (onContinue := fun cursor seen =>
+      ⌜(∀ x, x ∈ seen ↔ x ∈ cursor.prefix) ∧ ¬cursor.prefix.Any₂ (fun a b => a + b = 0)⌝)
     (onReturn := fun r b => ⌜r = true ∧ l.Any₂ (fun a b => a + b = 0)⌝)
-    (onContinue := fun traversalState seen =>
-      ⌜(∀ x, x ∈ seen ↔ x ∈ traversalState.prefix) ∧ ¬traversalState.prefix.Any₂ (fun a b => a + b = 0)⌝)
   with simp_all <;> grind
