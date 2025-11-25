7d5be802c2e28b56fd71aead6f9e097fba5a2a71
Seasawher <tellagoodstory@icloud.com>
2025-11-25 21:54:41 +0900

    `<;>` の挙動について注釈を追加する

--- LeanByExample/Tactic/SeqFocus.lean
+++ LeanByExample/Tactic/SeqFocus.lean
@@ -7,8 +7,9 @@ variable (P Q : Prop)
 example (hP : P) (hQ : Q) : P ∧ Q := by
   constructor <;> assumption
 
-/- これは [`all_goals`](./AllGoals.md) とよく似た挙動です。
+/- ## all_goals との違い
 
+`<;>` は [`all_goals`](./AllGoals.md) とよく似た挙動をします。
 しかし、`<;>` と `all_goals` は完全に同じではありません。
 `<;>` が「直前のタクティクによって生成された全てのサブゴール」に対して後続のタクティクを実行するのに対して、`all_goals` は「すべての未解決のゴール」に対して後続のタクティクまたはタクティクブロックを実行します。
 
@@ -37,5 +38,23 @@ example (hP : P) (hQ : Q) (hR : R) : (P ∧ Q) ∧ R := by
 
   -- 証明完了
   done
+
+/- ## ゴールが途中で閉じられたとき
+
+`tac1 <;> tac2` の実行において、`tac1` の実行でゴールが閉じられた場合、`tac2` は実行されずスキップされます。これは `tac1; tac2` とは異なる挙動であることに注意してください。
+-/
+
+example (hP : P) : P := by
+  -- `assumption`の時点でゴールは閉じているが、エラーにならない
+  -- `simp`の実行は単にスキップされる
+  assumption <;> simp
+
+example (hP : P) : P := by
+  -- `tac1; tac2` では、`tac2` を実行しようとする
+  -- そのため `tac2` が実行できないとエラーになることがある
+  fail_if_success
+    assumption; simp
+
+  assumption
 
 /- [^name]: `<;>` の正式な呼び名はわかりません。`linter.unnecessarySeqFocus` というリンタが存在するので、ここでは `seqFocus` と仮に呼んでいます。-/
