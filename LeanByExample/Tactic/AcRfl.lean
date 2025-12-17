a6eb72cf76650c224784c61d461148bb998fb617
Seasawher <tellagoodstory@icloud.com>
2025-12-17 22:28:31 +0900

    `grind` は `ac_rfl` の完全上位互換であると明記する

--- LeanByExample/Tactic/AcRfl.lean
+++ LeanByExample/Tactic/AcRfl.lean
@@ -121,3 +121,36 @@ namespace Color
     ac_rfl
 
 end Color
+/-
+## grind と ac_rfl
+
+なお、[`grind`](#{root}/Tactic/Grind.md) タクティクでも `ac_rfl` と同様のことができます。`grind` タクティクは `ac_rfl` の完全上位互換かもしれません。
+-/
+
+/-- 自然数をイメージした何か -/
+opaque MyNat : Type
+
+variable [Add MyNat]
+
+/-- 加法の結合法則 -/
+axiom MyNat.add_assoc (a b c : MyNat) : (a + b) + c = a + (b + c)
+
+/-- 結合法則が成り立つことを型クラス経由で登録する -/
+instance : @Std.Associative MyNat (· + ·) :=
+  ⟨MyNat.add_assoc⟩
+
+example (a b c d : MyNat) : (a + b) + (c + d) = a + (b + c) + d := by
+  -- grind で証明することができる
+  grind only
+
+
+/-- 加法の交換法則 -/
+axiom MyNat.add_comm (a b : MyNat) : a + b = b + a
+
+/-- 交換法則が成り立つことを型クラス経由で登録する -/
+instance : @Std.Commutative MyNat (· + ·) :=
+  ⟨MyNat.add_comm⟩
+
+example (a b c : MyNat) : a + b + c = c + b + a := by
+  -- grind で証明することができる
+  grind only
