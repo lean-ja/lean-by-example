17e4a6aac38ea2c7cd8aa20d23908f52ae678ef9
Seasawher <tellagoodstory@icloud.com>
2026-01-12 22:22:12 +0900

    variable で指定すると再帰がうまくいかない
    resolve #2257

--- LeanByExample/Declarative/Variable.lean
+++ LeanByExample/Declarative/Variable.lean
@@ -29,3 +29,22 @@ def isNil : Bool :=
 theorem nng_list_length : l.length ≥ 0 := by simp
 
 end variable1 --#
+/- ## 再帰と variable
+
+`variable` コマンドで宣言された引数は、再帰呼び出しの中でも同じ値が使用されます。
+したがって、再帰関数の再帰呼び出しで変化する引数を `variable` で共通化することはできません。
+-/
+
+variable {α : Type} (l : List α)
+
+set_option warn.sorry false in --#
+def List.myLength : Nat :=
+  match l with
+  | [] => 0
+  | _ :: xs => by
+    -- `List.myLength` の型が `List α → Nat` ではなくて、
+    -- `Nat` になってしまっている。
+    -- 引数の `List α` の部分には固定された値が入ってしまっているため
+    guard_hyp List.myLength : Nat
+
+    sorry
