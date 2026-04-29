a1e4ab9a1db251e65699a6618a41a1f09d839095
Seasawher <tellagoodstory@icloud.com>
2026-04-29 14:35:52 +0900

    `lake test`の修正

--- LeanByExample/Declarative/Syntax/Environment.lean
+++ LeanByExample/Declarative/Syntax/Environment.lean
@@ -9,4 +9,5 @@ private def fileName : Name := `LeanByExample.Declarative.Syntax.Syntax
 /-- `Arith`のための構文とマクロの定義が終わった直後の状態の`Environment` -/
 initialize env_of_arith_stx : Environment ← do
   initSearchPath (← findSysroot) -- コンパイル時にLeanにoleanファイルを見つけさせるために必要
+  unsafe Lean.enableInitializersExecution -- importModulesの前で初期化を許可する必要がある
   importModules #[{module := fileName}] {} (loadExts := true)
