d47d114c934500208c80907d75333415284821c1
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-05-23 17:11:26 +0900

    `panic!` のコード例として、意図の不明なコードがあったので削除する

--- LeanByExample/Syntax/Panic/CheckAbort.lean
+++ LeanByExample/Syntax/Panic/CheckAbort.lean
@@ -3,16 +3,18 @@ Syntax/Panic/Abort.lean の abort 挙動を確認するコード
 -/
 import Std
 
-open IO
+open IO Std
 
 /-- `LEAN_ABORT_ON_PANIC` に値を設定して `Abort.lean` を実行し、
 `panic!` で処理が中断されるかを判定する。 -/
 def checkAbort : IO Bool := do
-  Std.Internal.IO.Async.System.setEnvVar "LEAN_ABORT_ON_PANIC" "1"
+  -- 一回消しておく
+  Internal.IO.Async.System.unsetEnvVar "LEAN_ABORT_ON_PANIC"
   try
     let out ← IO.Process.output {
       cmd := "lean"
       args := #["--run", "LeanByExample/Syntax/Panic/Abort.lean"]
+      env := #[("LEAN_ABORT_ON_PANIC", "1")]
     }
 
     if out.stdout.trimAscii.endsWith "hello world!" then
