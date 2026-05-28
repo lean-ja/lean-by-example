1c98ad05cf281e8434352c6e62b430f641ba7d8d
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-05-28 23:53:15 +0900

    根本的に書き直し

--- LeanByExample/Declarative/GrindPattern.lean
+++ LeanByExample/Declarative/GrindPattern.lean
@@ -17,31 +17,20 @@ example (x y z : Nat) (h₁ : R x y) (h₂ : R y z) : R x z := by
   -- grind で証明できる
   grind
 
-/- パターンの中では、通常の項と同じように `_` でワイルドカードを指定できます。
-ただし、定理をインスタンス化するために必要なパラメータは、どこかのパターンでカバーされていなければなりません。
-次の例では、`S x y` が `x` と `y` をカバーし、`S _ z` が `z` をカバーしています。-/
-
-namespace WildcardPattern --#
-
-/-- 何らかの二項関係 -/
-opaque S : Nat → Nat → Prop
-
-/-- S は推移的 -/
-axiom Strans {x y z : Nat} : S x y → S y z → S x z
-
-grind_pattern Strans => S x y, S _ z
-
-example (x y z : Nat) (h₁ : S x y) (h₂ : S y z) : S x z := by
-  grind
-
-end WildcardPattern --#
-
 /-
 ## `[grind]` 属性では登録できない例
 
 多くの場合、わざわざ `grind_pattern` コマンドでパターンを手動指定しなくても `[grind]` 属性にパターンを自動で推測させることができますが、自動推測に失敗することがあります。このような場合は `grind_pattern` コマンドでパターンを明示する必要があります。
 
 {{#include ./GrindPattern/AttributeFailure.md}}
+-/
+
+/-
+## ワイルドカード
+
+パターンの中でワイルドカード `_` を使用することができます。この機能は、条件を厳しすぎず緩すぎない、ちょうどよい強さに調整するのに役立ちます。
+
+{{#include ./GrindPattern/WildCard.md}}
 -/
 
 /-
