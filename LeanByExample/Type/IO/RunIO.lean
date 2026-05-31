5260d79041953be83be9ed964de7ea9f9567f1f6
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-05-31 13:07:10 +0900

    `IO` から脱出する方法を紹介する

---
+++ LeanByExample/Type/IO/RunIO.lean
@@ -0,0 +1,25 @@
+import Lean
+
+open Lean Meta Elab Term
+
+def toExprIO {α : Type} [ToExpr α] (x : IO α) : IO Expr :=
+  toExpr <$> x
+
+elab tk:"run_io " t:doSeq : term <= expectedType => withRef t do
+  let expectedType := mkApp (mkConst ``IO) expectedType
+  let v ← elabTermEnsuringType (← `(do $t)) expectedType
+  synthesizeSyntheticMVarsNoPostponing
+  let v ← instantiateMVars v
+  if (← logUnassignedUsingErrorInfos (← getMVars v)) then
+    throwAbortTerm
+  let v ← mkAppM ``toExprIO #[v]
+  let io ← unsafe evalExpr (IO Expr) (mkApp (mkConst ``IO) (mkConst ``Expr)) v
+  let (out, x) ← IO.FS.withIsolatedStreams io.toBaseIO
+  unless out.isEmpty do
+    logInfoAt tk out
+  match x with
+  | .ok x => return x
+  | .error e => throwErrorAt tk e.toString
+
+-- 使用例。ランダムな数を選んでいるのに、型を `Nat` にすることができる
+def number : Nat := run_io IO.rand 0 200
