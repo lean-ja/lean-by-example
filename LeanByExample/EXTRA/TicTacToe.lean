439688828b2c0276cfef32720e3187e8b2b5d8a2
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-03 20:55:00 +0900

    コードの校正 / 依存型を使わない

--- LeanByExample/EXTRA/TicTacToe.lean
+++ LeanByExample/EXTRA/TicTacToe.lean
@@ -14,11 +14,11 @@ protected def Cell.toString (c : Cell) : String :=
 instance : ToString Cell where
   toString := Cell.toString
 
-/-- ゲームが持っている、盤面の状態 -/
-abbrev Board := Vector Cell 9
+/-- ゲームの盤面 -/
+abbrev Board := Array Cell
 
 def Board.empty : Board :=
-  Vector.replicate 9 Cell.empty
+  Array.replicate 9 Cell.empty
 
 /-- ユーザー入力を受け取る -/
 def getUserRawInput : IO String := do
@@ -27,60 +27,57 @@ def getUserRawInput : IO String := do
   let input ← stdin.getLine
   return input.trimAscii.copy
 
-/-- 盤面上の位置。0 以上 9 未満の番号で表す -/
-abbrev Position := Fin 9
-
-def parsePosition (input : String) : Option Position :=
+def parsePosition (input : String) : Option Nat :=
   let num? := input.toNat?
   match num? with
   | none => none
   | some num =>
-    if h : num < 9 then
-      some ⟨num, h⟩
+    if num < 9 then
+      num
     else
       none
 
 def grayText (s : String) : String :=
   s!"\x1b[90m{s}\x1b[0m"
 
-def Cell.display (c : Cell) (pos : Position) : String :=
+def Cell.display (c : Cell) (pos : Nat) : String :=
   match c with
-  | .empty => grayText <| toString pos.val
+  | .empty => grayText <| toString pos
   | _ => toString c
 
 /-- ゲームの盤面の状態を表示する -/
 def Board.display (board : Board) (indentSize : Nat := 5) : IO Unit := do
   let indent := " ".pushn ' ' indentSize
   IO.println s!"{indent}+--+--+--+"
-  for pos in Vector.finRange 9 do
-    if pos.val % 3 == 0 then
+  for pos in [0:9] do
+    if pos % 3 == 0 then
       IO.print indent
-    let displayText := board[pos].display pos
+    let displayText := board[pos]!.display pos
     IO.print s!"| {displayText}"
-    if pos.val % 3 == 2 then
+    if pos % 3 == 2 then
       IO.println "|"
       IO.println s!"{indent}+--+--+--+"
 
 /-- 未着手のセルを列挙する -/
-def Board.unused (board : Board) : Array Position :=
-  (Vector.finRange 9).toArray.filter (fun pos => board[pos] == .empty)
+def Board.unused (board : Board) : Array Nat :=
+  List.range 9 |>.toArray |>.filter (fun pos => board[pos]! == .empty)
 
 /-- ゲームの盤面の状態を更新して、新しい盤面を返す
 既に着手済みの場所に置こうとすると失敗し、`panic!` する -/
-def Board.update! (board : Board) (pos : Position) (c : Cell) : Board :=
-  if board[pos] != .empty then
+def Board.update (board : Board) (pos : Nat) (c : Cell) : Board :=
+  if board[pos]! != .empty then
     panic! "[Board.update] the position is already used"
   else
-    board.set pos c
+    board.set! pos c
 
-def Array.getRandom! {α : Type} [Inhabited α] (xs : Array α) : IO α := do
+def Array.getRandom {α : Type} [Inhabited α] (xs : Array α) : IO α := do
   let idx ← IO.rand 0 (xs.size - 1)
   return xs[idx]!
 
 /-- 未着手の場所をランダムに選ぶ -/
-def getRandomPos (board : Board) : IO Position := do
+def getRandomPos (board : Board) : IO Nat := do
   let unused := board.unused
-  let pos ← unused.getRandom!
+  let pos ← unused.getRandom
   return pos
 
 inductive Result where
@@ -94,21 +91,21 @@ inductive Result where
   | draw
 deriving BEq
 
-abbrev Line := Vector Position 3
+abbrev Line := Array Nat
 
 def allLines : Array Line :=
-  let row0 : Line := #v[0, 1, 2]
-  let row1 : Line := #v[3, 4, 5]
-  let row2 : Line := #v[6, 7, 8]
-  let col0 : Line := #v[0, 3, 6]
-  let col1 : Line := #v[1, 4, 7]
-  let col2 : Line := #v[2, 5, 8]
-  let diag1 : Line := #v[0, 4, 8]
-  let diag2 : Line := #v[2, 4, 6]
+  let row0 : Line := #[0, 1, 2]
+  let row1 : Line := #[3, 4, 5]
+  let row2 : Line := #[6, 7, 8]
+  let col0 : Line := #[0, 3, 6]
+  let col1 : Line := #[1, 4, 7]
+  let col2 : Line := #[2, 5, 8]
+  let diag1 : Line := #[0, 4, 8]
+  let diag2 : Line := #[2, 4, 6]
   #[row0, row1, row2, col0, col1, col2, diag1, diag2]
 
 def Line.check (line : Line) (board : Board) (P : Cell → Bool) : Bool :=
-  line.all (fun pos => P (board[pos]))
+  line.all (fun pos => P (board[pos]!))
 
 def Board.checkForLines (board : Board) (P : Cell → Bool) : Bool :=
   allLines.any (fun line => line.check board P)
@@ -126,14 +123,14 @@ def Board.result (board : Board) : Result :=
   else
     .progress
 
-partial def getUserHand (board : Board) : IO Position := do
+partial def getUserHand (board : Board) : IO Nat := do
   let input ← getUserRawInput
   let some pos := parsePosition input |
     IO.println "0から8の数字を入力してください"
     board.display
     getUserHand board
 
-  if board[pos] != .empty then
+  if board[pos]! != .empty then
     IO.println "その場所には着手できません"
     board.display
     getUserHand board
@@ -148,14 +145,14 @@ def main : IO Unit := do
     board.display
 
     let yourPos ← getUserHand board
-    board := board.update! yourPos Cell.x
+    board := board.update yourPos Cell.x
 
     result := board.result
     if result != .progress then
       break
 
     let cpuPos ← getRandomPos board
-    board := board.update! cpuPos Cell.o
+    board := board.update cpuPos Cell.o
 
     result := board.result
     if result != .progress then
