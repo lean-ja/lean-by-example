f9e539293577b8dfcf896f60357fdf31e1933989
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-10 00:39:29 +0900

    余計な `import` 文を削除する

--- LeanByExample/Attribute/Irreducible.lean
+++ LeanByExample/Attribute/Irreducible.lean
@@ -1,7 +1,6 @@
-import Lean --#
 /- # irreducible
 
-`[irreducible]` 属性を付与すると、定義に展開できなくなり、[`rfl`](#{root}/Tactic/Rfl.md) タクティクや [`dsimp`](#{root}/Tactic/Dsimp.md) タクティクが通らなくなります。
+`[irreducible]` 属性を付与すると、その名前が定義に等しいということが隠されて、[`rfl`](#{root}/Tactic/Rfl.md) タクティクや [`dsimp`](#{root}/Tactic/Dsimp.md) タクティクが通らなくなります。
 -/
 
 /-- 階乗関数 -/
@@ -33,7 +32,7 @@ example : factorial 5 = 120 := by
 
 /- ## reducibility を確認する
 
-Lean が暗黙の裡に `[irreducible]` 属性や `[reducible]` 属性を付与していることがあります。`Lean.getReducibilityStatus` 関数を使うと確認することができます。
+Lean が暗黙の裡に `[irreducible]` 属性や `[reducible]` 属性を付与していることがあります。[`#print`](#{root}/Diagnostic/Print.md) コマンドを使うと確認することができます。
 -/
 
 def searchF {α : Type} (f : Nat → Option α) (start : Nat) : Option Nat :=
@@ -43,6 +42,15 @@ def searchF {α : Type} (f : Nat → Option α) (start : Nat) : Option Nat :=
 partial_fixpoint
 
 -- `partial_fixpoint` を使ったので irreducible になっている
-/-- info: Lean.ReducibilityStatus.irreducible -/
+/--
+info: @[irreducible] def searchF : {α : Type} → (Nat → Option α) → Nat → Option Nat :=
+fun {α} f =>
+  Lean.Order.fix
+    (fun f_1 start =>
+      match f start with
+      | some val => some start
+      | none => f_1 (start + 1))
+    ⋯
+-/
 #guard_msgs in --#
-#eval Lean.getReducibilityStatus `searchF
+#print searchF
