0142373c41478a9cadf5db4ad993733e1de3c780
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-09 22:17:47 +0900

    コード例の修正

--- LeanByExample/Type/IO/Greet.lean
+++ LeanByExample/Type/IO/Greet.lean
@@ -1,10 +1,10 @@
 /-- 標準入力から入力された名前に対して挨拶をする -/
 def main : IO Unit := do
   let stdin ← IO.getStdin
-  let stdout ← IO.getStdout
 
-  stdout.putStrLn "誰に挨拶しますか？"
+  IO.println "誰に挨拶しますか？"
+  IO.print "> "
   let input ← stdin.getLine
-  let name := input.trimAscii -- これをしないと余計な改行が入る
+  let name := input.trimAscii.copy -- これをしないと余計な改行が入る
 
-  stdout.putStrLn s!"Hello, {name}!"
+  IO.println s!"Hello, {name}!"
