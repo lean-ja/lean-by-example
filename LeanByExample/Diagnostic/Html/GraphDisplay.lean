1ad126ab5f4b61d9f07e5e68c87a9786144ec287
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-07-04 13:55:50 +0900

    mathlib バージョンの更新

--- LeanByExample/Diagnostic/Html/GraphDisplay.lean
+++ LeanByExample/Diagnostic/Html/GraphDisplay.lean
@@ -1,6 +1,6 @@
 import ProofWidgets
 
-open ProofWidgets Jsx GraphDisplay
+open ProofWidgets Jsx ForceGraphDisplay
 
 /-- `Edge` を作る -/
 def mkEdge (st : String × String) : Edge := {source := st.1, target := st.2}
@@ -10,7 +10,7 @@ def mkVertex (id : String) : Vertex := {id := id}
 
 -- 有向グラフを表示する
 #html
-  <GraphDisplay
+  <ForceGraphDisplay
     vertices={#["a", "b", "c", "d", "e"].map mkVertex}
     edges={#[("a", "b"), ("b", "c"), ("c", "d"), ("d", "e"), ("e", "a")].map mkEdge}
   />
