0a4e8e70c5e689a8f747e1c5e4371ba15c8ed87d
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-07-20 19:19:04 +0900

    `HAppend` の校正

--- LeanByExample/TypeClass/HAppend.lean
+++ LeanByExample/TypeClass/HAppend.lean
@@ -1,24 +1,18 @@
 /- # HAppend
 
 `HAppend` (heterogeneous append) は、`++` という二項演算子のための型クラスです。
-`++` 記法が何を意味するかについての制約はありません。
 任意の型 `α, β, γ : Type` に対して連結 `(· ++ ·) : α → β → γ` を定義することができます。
 -/
 
 -- 最初は `++` 記号が定義されていないのでエラーになる
 #check_failure ([1, 2] ++ (3 : Nat))
 
-/-- HAppend 型クラスのインスタンスを実装する -/
+/-- HAppend 型クラスのインスタンスを実装する
+注意: これは例示のためのインスタンスで、あまり良いインスタンスではない。 -/
 instance : HAppend (List Nat) Nat (List Nat) where
-  hAppend xs n := xs.concat n
+  hAppend xs n := xs.map (· + n)
 
 -- 連結記号が使えるようになった
-#guard [1, 2] ++ (3 : Nat) = [1, 2, 3]
-
-/- 標準ライブラリには、`Array α` に `List α` を連結して `Array α` を得る
-`HAppend (Array α) (List α) (Array α)` インスタンスが定義されています。-/
-
--- Array に List を連結して Array を得ることができる
-#guard #[1, 2] ++ [3, 4] = (#[1, 2, 3, 4] : Array Nat)
+#guard [1, 2] ++ (3 : Nat) = [4, 5]
 
 /- 連結 `(· ++ ·) : α → β → γ` が一つの型の中で閉じているとき、つまり `α = β = γ` のときは [`Append`](#{root}/TypeClass/Append.md) が使用できます。 -/
