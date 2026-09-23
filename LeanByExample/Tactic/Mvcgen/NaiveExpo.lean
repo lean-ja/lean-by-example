edb375f81809cc6e03ae9f2d370ca8bbedf6cd60
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-09-23 23:05:52 +0900

    `NaiveExpo` の修正

--- LeanByExample/Tactic/Mvcgen/NaiveExpo.lean
+++ LeanByExample/Tactic/Mvcgen/NaiveExpo.lean
@@ -1,8 +1,7 @@
 import Std.Tactic.Do
+import Std.WP
 
-set_option mvcgen.warning false
-
-open Std.Do
+open Std.WP
 
 /-- 素朴に実装されたべき乗関数 -/
 def naiveExpo (x n : Nat) : Nat := Id.run do
@@ -11,14 +10,17 @@ def naiveExpo (x n : Nat) : Nat := Id.run do
     result := result * x
   return result
 
+-- `vcgen` が実験的機能であることを明示する
+set_option experimental.vcgen true in
+
 theorem naiveExpo_correct (x n : Nat) : naiveExpo x n = x ^ n := by
   generalize h : naiveExpo x n = r
-  apply Id.of_wp_run_eq h
+  apply Id.of_run_eq_wp h
 
-  mvcgen invariants
+  vcgen invariants
   -- 不変条件を指定する
-  -- ここでは`cursor`がループの進捗を表していて、
-  -- `cursor.prefix.length`はこれまでにループが回った回数を表す
-  -- `result`はループ内で更新される変数の値を表す
-  · ⇓⟨cursor, result⟩ => ⌜result = x ^ cursor.prefix.length⌝
-  with simp_all <;> grind -- すべてのゴールに対して `mleave` に加えて `simp_all <;> grind` を適用してみる
+  -- `pref` は処理済みの要素のリストで、
+  -- `pref.length` はこれまでにループが回った回数を表す
+  -- `result` はループ内で更新される変数の値を表す
+  · fun pref _ result => result = x ^ pref.length
+  all_goals simp_all +zetaDelta [Nat.pow_succ]
